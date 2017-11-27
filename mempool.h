24bd724f259e45401ea4f9014c61d0bce1870f39
Joel K. Pettersson <joelkpettersson@gmail.com>
2017-11-27 03:26:19 +0100

    2014-10-12: Test lexer branch. (Revert previous.)
    Add quick test version of a lexer module, used
    in place of the parser. (Normal output is disabled.)
    Experimental work towards loose plans for
    a new scripting language and a fuller redesign.
    
    Beginning of new symbol table implementation.
    Implemented string pool/string interning with a simple
    hash table. Used by the test lexer but not integrated
    with the old code.
    
    Added memory pool module (mempool.c), used for string
    storage in the new symbol table code. It is meant for
    large numbers of small allocations. Internally they are
    stored in arrays sized after the system's memory page size.
    Upon destruction, everything stored is freed.
    
    Fix build on OpenBSD (and NetBSD) using conditional
    compilation in audiodev_oss.c and an LFLAGS addition.
    (Note: This broke building on DragonFly and FreeBSD,
    fixed in newer versions.)
    
    (The .gitignore file is replaced with that used for
    all old snapshots.)
    
    Change log:
     * 2014-09-18: First quick test version of lexer module.
     * 2014-09-20: Add new symtab code. Add mempool module.
                   Second quick test version of lexer module.
                   Replace normal output with lexer testing.
                   Implemented string interning for symtab.
     * 2014-10-11: Portability fix for OSS on OpenBSD and NetBSD.
     * 2014-10-12: Mempool module debugged.
                   Test hash collisions in symtab module.
                   Third quick test version of lexer module.
                   Add GNUmakefile with debug build options.

---
+++ mempool.h
@@ -0,0 +1,26 @@
+/* sgensys memory pool module.
+ * Copyright (c) 2014 Joel K. Pettersson <joelkpettersson@gmail.com>
+ *
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version; WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * View the file COPYING for details, or if missing, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SGS_mempool_h
+#define __SGS_mempool_h
+
+#include <stddef.h>
+
+struct SGSMemPool;
+typedef struct SGSMemPool SGSMemPool;
+
+SGSMemPool *SGS_create_mempool(size_t block_size);
+void SGS_destroy_mempool(SGSMemPool *o);
+
+void *SGS_mempool_alloc(SGSMemPool *o, size_t size);
+
+#endif /* EOF */
--- program.h
+++ program.h
@@ -1,5 +1,5 @@
 /* sgensys parsing data to program data translator module.
- * Copyright (c) 2011-2013 Joel K. Pettersson <joelkpettersson@gmail.com>
+ * Copyright (c) 2011-2012 Joel K. Pettersson <joelkpettersson@gmail.com>
  *
  * This file and the software of which it is part is distributed under the
  * terms of the GNU Lesser General Public License, either version 3 or (at
@@ -11,109 +11,113 @@
  */
 
 enum {
-	/* voice parameters */
-	SGS_OPLIST = 1<<0,
-	SGS_PANNING = 1<<1,
-	SGS_VALITPANNING = 1<<2,
-	SGS_VOATTR = 1<<3,
-	/* operator parameters */
-	SGS_UNUSED = 1<<4,
-	SGS_WAVE = 1<<5,
-	SGS_TIME = 1<<6,
-	SGS_SILENCE = 1<<7,
-	SGS_FREQ = 1<<8,
-	SGS_VALITFREQ = 1<<9,
-	SGS_DYNFREQ = 1<<10,
-	SGS_PHASE = 1<<11,
-	SGS_AMP = 1<<12,
-	SGS_VALITAMP = 1<<13,
-	SGS_DYNAMP = 1<<14,
-	SGS_OPATTR = 1<<15
+  /* voice parameters */
+  SGS_GRAPH = 1<<0,
+  SGS_PANNING = 1<<1,
+  SGS_VALITPANNING = 1<<2,
+  SGS_VOATTR = 1<<3,
+  /* operator parameters */
+  SGS_ADJCS = 1<<4,
+  SGS_WAVE = 1<<5,
+  SGS_TIME = 1<<6,
+  SGS_SILENCE = 1<<7,
+  SGS_FREQ = 1<<8,
+  SGS_VALITFREQ = 1<<9,
+  SGS_DYNFREQ = 1<<10,
+  SGS_PHASE = 1<<11,
+  SGS_AMP = 1<<12,
+  SGS_VALITAMP = 1<<13,
+  SGS_DYNAMP = 1<<14,
+  SGS_OPATTR = 1<<15
 };
 
 #define SGS_VOICE_PARAMS(flags) \
-	((flags) & (SGS_OPLIST|SGS_PANNING|SGS_VALITPANNING|SGS_VOATTR))
+  ((flags) & (SGS_GRAPH|SGS_PANNING|SGS_VALITPANNING|SGS_VOATTR))
 
 #define SGS_OPERATOR_PARAMS(flags) \
-	((flags) & (SGS_UNUSED|SGS_WAVE|SGS_SILENCE|SGS_FREQ|SGS_VALITFREQ| \
-	            SGS_DYNFREQ|SGS_PHASE|SGS_AMP|SGS_VALITAMP|SGS_DYNAMP| \
-	            SGS_OPATTR))
+  ((flags) & (SGS_ADJCS|SGS_WAVE|SGS_SILENCE|SGS_FREQ|SGS_VALITFREQ| \
+              SGS_DYNFREQ|SGS_PHASE|SGS_AMP|SGS_VALITAMP|SGS_DYNAMP| \
+              SGS_OPATTR))
 
 /* operator wave types */
 enum {
-	SGS_WAVE_SIN = 0,
-	SGS_WAVE_SRS,
-	SGS_WAVE_TRI,
-	SGS_WAVE_SQR,
-	SGS_WAVE_SAW
+  SGS_WAVE_SIN = 0,
+  SGS_WAVE_SRS,
+  SGS_WAVE_TRI,
+  SGS_WAVE_SQR,
+  SGS_WAVE_SAW
 };
 
 /* special operator timing values */
 enum {
-	SGS_TIME_INF = -1 /* used for nested operators */
+  SGS_TIME_INF = -1 /* used for nested operators */
 };
 
 /* operator atttributes */
 enum {
-	SGS_ATTR_WAVEENV = 1<<0,
-	SGS_ATTR_FREQRATIO = 1<<1,
-	SGS_ATTR_DYNFREQRATIO = 1<<2,
-	SGS_ATTR_VALITFREQ = 1<<3,
-	SGS_ATTR_VALITFREQRATIO = 1<<4,
-	SGS_ATTR_VALITAMP = 1<<5,
-	SGS_ATTR_VALITPANNING = 1<<6
+  SGS_ATTR_FREQRATIO = 1<<1,
+  SGS_ATTR_DYNFREQRATIO = 1<<2,
+  SGS_ATTR_VALITFREQ = 1<<3,
+  SGS_ATTR_VALITFREQRATIO = 1<<4,
+  SGS_ATTR_VALITAMP = 1<<5,
+  SGS_ATTR_VALITPANNING = 1<<6
 };
 
 /* value iteration types */
 enum {
-	SGS_VALIT_NONE = 0, /* when none given */
-	SGS_VALIT_LIN,
-	SGS_VALIT_EXP,
-	SGS_VALIT_LOG
+  SGS_VALIT_NONE = 0, /* when none given */
+  SGS_VALIT_LIN,
+  SGS_VALIT_EXP,
+  SGS_VALIT_LOG
 };
 
+typedef struct SGSProgramGraph {
+  uchar opc;
+  int ops[1]; /* sized to opc */
+} SGSProgramGraph;
+
+typedef struct SGSProgramGraphAdjcs {
+  uchar fmodc;
+  uchar pmodc;
+  uchar amodc;
+  uchar level;  /* index for buffer used to store result to use if node
+                   revisited when traversing the graph. */
+  int adjcs[1]; /* sized to total number */
+} SGSProgramGraphAdjcs;
+
 typedef struct SGSProgramValit {
-	int time_ms, pos_ms;
-	float goal;
-	uchar type;
+  int time_ms, pos_ms;
+  float goal;
+  uchar type;
 } SGSProgramValit;
 
 typedef struct SGSProgramVoiceData {
-	uint voice_id;
-	uint input_block_id;
-	int panning_block_id; /* -1 if none */
-	const int *operator_list;
-	uint operator_c;
-	uchar attr;
-	float panning;
-	SGSProgramValit valitpanning;
+  const SGSProgramGraph *graph;
+  uchar attr;
+  float panning;
+  SGSProgramValit valitpanning;
 } SGSProgramVoiceData;
 
 typedef struct SGSProgramOperatorData {
-	uint operator_id;
-	uint output_block_id;
-	int freq_block_id, /* -1 if none */
-	    freq_mod_block_id,
-	    phase_mod_block_id,
-	    amp_block_id,
-	    amp_mod_block_id;
-	uchar attr, wave;
-	int time_ms, silence_ms;
-	float freq, dynfreq, phase, amp, dynamp;
-	SGSProgramValit valitfreq, valitamp;
+  const SGSProgramGraphAdjcs *adjcs;
+  uint operator_id;
+  uchar attr, wave;
+  int time_ms, silence_ms;
+  float freq, dynfreq, phase, amp, dynamp;
+  SGSProgramValit valitfreq, valitamp;
 } SGSProgramOperatorData;
 
 typedef struct SGSProgramEvent {
-	int wait_ms;
-	uint params;
-	const SGSProgramVoiceData *voice;
-	const SGSProgramOperatorData *operator;
+  int wait_ms;
+  uint params;
+  uint voice_id; /* needed for both voice and operator data */
+  const SGSProgramVoiceData *voice;
+  const SGSProgramOperatorData *operator;
 } SGSProgramEvent;
 
 struct SGSProgram {
-	const SGSProgramEvent *events;
-	uint eventc,
-	     blockc,
-	     voicec,
-	     operatorc;
+  const SGSProgramEvent *events;
+  uint eventc;
+  uint operatorc,
+       voicec;
 };
