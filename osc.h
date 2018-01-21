1bad251145c8079adf7baac5ea92208be39bd701
Joel K. Pettersson <joelkpettersson@gmail.com>
2018-01-21 19:43:21 +0100

    Snapshot, 2011-03-04: Support AM and real FM. Syntax changes.
    Change syntax for modulator lists. Now enclosed within "{}"
    instead of "<>". "a!{}" for AM, "f!{}" or "r!{}" for FM,
    "m{}" for PM.
    
    Turn 'r' into an actual ratio parameter, adding the necessary
    divisions.
    
    Oscillator: store no more than phase, taking frequency and
    amplitude as parameters. Add "envelope output" version.

--- osc.h
+++ osc.h
@@ -14,27 +14,22 @@ extern MGSOscTab MGSOsc_sin,
                  MGSOsc_saw;
 
 typedef struct MGSOsc {
-  float freq;
   uint phase;
-  float amp;
 } MGSOsc;
 
 #define MGSOsc_COEFF(sr) \
   (4294967296.0/(sr))
 
+#define MGSOsc_PHASE(o) \
+  ((uint)((o)->phase))
+
 #define MGSOsc_SET_PHASE(o, p) \
   ((void)((o)->phase = (p)))
 
-#define MGSOsc_SET_FREQ(o, fq) \
-  ((void)((o)->freq = (fq)))
-
-#define MGSOsc_SET_AMP(o, r) \
-  ((void)((o)->amp = (r)))
-
-#define MGSOsc_RUN(o, osctab, coeff, out) do{ \
+#define MGSOsc_RUN(o, osctab, coeff, freq, amp, out) do{ \
   int MGSOsc__s; \
   uint MGSOsc__i; \
-  SET_I2F(MGSOsc__i, (coeff)*(o)->freq); \
+  SET_I2F(MGSOsc__i, (coeff)*(freq)); \
   (o)->phase += MGSOsc__i; \
   MGSOsc__i = (o)->phase >> (32-MGSOsc_TABINDEXBITS); \
   MGSOsc__s = (osctab)[MGSOsc__i]; \
@@ -44,14 +39,14 @@ typedef struct MGSOsc {
      ((float)(((osctab)[MGSOsc__i + 1] - MGSOsc__s))) * \
      ((float)((o)->phase & MGSOsc_TABINDEXMASK)) * \
      (1.f / (1 << (32-MGSOsc_TABINDEXBITS)))) * \
-    (o)->amp \
+    (amp) \
   ); \
 }while(0)
 
-#define MGSOsc_RUN_FM(o, osctab, coeff, fm, out) do{ \
+#define MGSOsc_RUN_FM(o, osctab, coeff, freq, fm, amp, out) do{ \
   int MGSOsc__s; \
   uint MGSOsc__i; \
-  SET_I2F(MGSOsc__i, (coeff)*(o)->freq); \
+  SET_I2F(MGSOsc__i, (coeff)*(freq)); \
   (o)->phase += MGSOsc__i + ((fm) * ((MGSOsc__i >> 11) - (MGSOsc__i >> 14) + (MGSOsc__i >> 18))); \
   MGSOsc__i = (o)->phase >> (32-MGSOsc_TABINDEXBITS); \
   MGSOsc__s = (osctab)[MGSOsc__i]; \
@@ -61,14 +56,14 @@ typedef struct MGSOsc {
      ((float)(((osctab)[MGSOsc__i + 1] - MGSOsc__s))) * \
      ((float)((o)->phase & MGSOsc_TABINDEXMASK)) * \
      (1.f / (1 << (32-MGSOsc_TABINDEXBITS)))) * \
-    (o)->amp \
+    (amp) \
   ); \
 }while(0)
 
-#define MGSOsc_RUN_PM(o, osctab, coeff, pm, out) do{ \
+#define MGSOsc_RUN_PM(o, osctab, coeff, freq, pm, amp, out) do{ \
   int MGSOsc__s; \
   uint MGSOsc__i, MGSOsc__p; \
-  SET_I2F(MGSOsc__i, (coeff)*(o)->freq); \
+  SET_I2F(MGSOsc__i, (coeff)*(freq)); \
   (o)->phase += MGSOsc__i; \
   MGSOsc__p = (o)->phase + ((pm) << 16); \
   MGSOsc__i = MGSOsc__p >> (32-MGSOsc_TABINDEXBITS); \
@@ -79,13 +74,31 @@ typedef struct MGSOsc {
      ((float)(((osctab)[MGSOsc__i + 1] - MGSOsc__s))) * \
      ((float)(MGSOsc__p & MGSOsc_TABINDEXMASK)) * \
      (1.f / (1 << (32-MGSOsc_TABINDEXBITS)))) * \
-    (o)->amp \
+    (amp) \
   ); \
 }while(0)
 
-#define MGSOsc_WAVE_OFFS(o, coeff, timepos, out) do{ \
+/* Gives output in 0.0 to 1.0 range for AM use as envelope */
+#define MGSOsc_RUN_PM_ENVO(o, osctab, coeff, freq, pm, out) do{ \
+  int MGSOsc__s; \
+  uint MGSOsc__i, MGSOsc__p; \
+  SET_I2F(MGSOsc__i, (coeff)*(freq)); \
+  (o)->phase += MGSOsc__i; \
+  MGSOsc__p = (o)->phase + ((pm) << 16); \
+  MGSOsc__i = MGSOsc__p >> (32-MGSOsc_TABINDEXBITS); \
+  MGSOsc__s = (osctab)[MGSOsc__i]; \
+  /* write lerp'd & scaled result */ \
+  (out) = (((float)MGSOsc__s) + \
+           ((float)(((osctab)[MGSOsc__i + 1] - MGSOsc__s))) * \
+           ((float)(MGSOsc__p & MGSOsc_TABINDEXMASK)) * \
+           (1.f / (1 << (32-MGSOsc_TABINDEXBITS)))) * \
+          (1.f / (1 << 16)) + \
+          .5f; \
+}while(0)
+
+#define MGSOsc_WAVE_OFFS(o, coeff, freq, timepos, out) do{ \
   uint MGSOsc__i; \
-  SET_I2F(MGSOsc__i, (coeff)*(o)->freq); \
+  SET_I2F(MGSOsc__i, (coeff)*(freq)); \
   uint MGSOsc__p = MGSOsc__i * (uint)(timepos); \
   int MGSOsc__o = MGSOsc__p - (MGSOsc_TABINDEXMASK+1); \
   (out) = (MGSOsc__o / MGSOsc__i); \
