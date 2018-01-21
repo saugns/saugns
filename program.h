30a790073aeb290d72b3e1583b4686767ae4066e
Joel K. Pettersson <joelkpettersson@gmail.com>
2018-01-21 19:34:43 +0100

    2011-02-13: Add label definition/referencing.
    Defining with "'", referencing with ":".
    
    Add symbol table, using a simple linked list until something
    better is needed.
    
    Sound generator changes also include click reduction by
    increasing time to match the end of a wave cycle.

--- program.h
+++ program.h
@@ -1,4 +1,10 @@
 enum {
+  MGS_TYPE_SETAMP = 1<<0,
+  MGS_TYPE_SETTIME = 1<<1,
+  MGS_TYPE_SETFREQ = 1<<2
+};
+
+enum {
   MGS_WAVE_SIN = 0,
   MGS_WAVE_SQR,
   MGS_WAVE_TRI,
@@ -12,13 +18,13 @@ enum {
 };
 
 typedef struct MGSProgramNode {
-  struct MGSProgramNode *next;
+  struct MGSProgramNode *next, *ref;
   uchar type, wave, mode;
   float amp, delay, time, freq;
+  uint id;
 } MGSProgramNode;
 
 struct MGSProgram {
   MGSProgramNode *steps;
   uint stepc;
-  uint componentc;
 };
