3aec91db01b086be78920f0282a182827890ba2b
Joel K. Pettersson <joelkpettersson@gmail.com>
2018-01-22 03:25:47 +0100

    Snapshot, 2012-02-01: Fixes, part 2. Move, fix, test scripts.
    Parser: Fixes for timing.
    
    Sound generator: Corrected outdated adjacency indexing.
    
    Script files in main dir moved to "tests/".
    
    Update remaining scripts with outdated syntax.

--- parser.h
+++ parser.h
@@ -28,9 +28,10 @@ enum {
   ON_MULTIPLE_OPERATORS = 1<<1,
   ON_FIRST_IN_SCOPE = 1<<2,
   ON_LAST_IN_SCOPE = 1<<3,
-  ON_LABEL_ALLOC = 1<<4,
-  ON_OPERATOR_NESTED = 1<<5,
-  ON_SILENCE_ADDED = 1<<6,
+  ON_OPERATOR_NESTED = 1<<4,
+  ON_LABEL_ALLOC = 1<<5,
+  ON_TIME_DEFAULT = 1<<6,
+  ON_SILENCE_ADDED = 1<<7,
 };
 
 typedef struct SGSOperatorNode {
@@ -62,7 +63,6 @@ typedef struct SGSEventNode {
   struct SGSEventNode *groupfrom;
   struct SGSEventNode *composite;
   int wait_ms;
-  int duration_ms; /* filled in and used later */
   SGSNodeList operators; /* operator linkage graph */
   uint scopeid;
   uint en_flags;
