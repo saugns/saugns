367c9202eb382f104c3128e1dd721ed7406edbdc
Joel K. Pettersson <joelkpettersson@gmail.com>
2018-02-09 16:16:45 +0100

    Add 3 wave types. Basic syntax changes. Reorganize scripts.
    Script syntax changes:
     * Scope nesting syntax: Change from "<...>" to "[...]".
       ('<' and '>' are now unused. "[...]" is now used both for
       lists of objects, and arguments with several sub-values.)
     * PM modulator list prefix: Change from "p!" to "p*".
       (I.e., a list uses "p![]". Since semantics differ for PM
       lists vs. AM and FM lists, make this clearer in the syntax.
       For PM lists, modulator amplitudes are summed,
       while for AM and FM lists, they are multiplied.)
     * Numerical expressions:
       Fix really old bug which prevented evaluation
       of "Y" in an expression of the form "((X)Y)".
    
    Split osc module into osc and wave. Wave type changes:
     * Add 'sha' (half-frequency absolute sine).
     * Add 'szh' (half-zero'd sine).
     * Add 'shh' (first quarter of sine).
     * Rename 'srs' (square root of sine) to 'ssr'.
    
    Move wave type names from parser to wave module.
    
    Add -v version number, initially at v0.2.3.
    
    Reorganize example and test scripts. Move some old and useful
    scripts from devtests/ to examples/tests/. Rename scripts,
    add descriptions to some of them, modify a few, remove a few
    which are useless, and add more wave type testing.
    
    Change log:
     * 2018-04-18: Move wave type names to osc module, add new
           'sab' wave type, rename 'srs' to 'ssr'.
           Reorganize and update example and test scripts.
     * 2018-06-02: Add 'shw' and 'shr' wave types, and
           further LUT init code changes.
     * 2018-06-30: Split osc module into wave and osc.
     * 2018-09-12: Rename wave types
           ("sab"->"sah", "shw"->"szh", "shr"->"szhhr").
     * 2018-09-27: Rename wave type 'sah'->'sha',
           add 'shh', comment out 'szhhr'.
     * 2018-09-29: Rename debug #define switches in sgensys.h.
     * 2018-10-16: Update headers, heading comments, for added code.
           Complete branch merging.
     * 2018-10-19: Change PM list syntax from "p!<>" to "p*<>".
     * 2018-12-21: Fix two outdated scripts in devtests/.
           Fix really old bug in parsing of numerical
           expressions with nested parentheses, which
           prevented "Y" in an expression of the form
           ((X)Y) from being evaluated.
     * 2019-06-05: Change scope nesting syntax
           from "<...>" to "[...]".
           Add -v version, initially at v0.3.0.
     * 2019-06-13: Set version to v0.2.3.

--- osc.h
+++ osc.h
@@ -12,32 +12,8 @@
  */
 
 #pragma once
+#include "wave.h"
 #include "math.h"
-
-#define SGSOsc_LUT_INDEXBITS 11
-#define SGSOsc_LUT_INDEXMASK (SGSOsc_LUT_LEN - 1)
-#define SGSOsc_LUT_LEN       (1<<SGSOsc_LUT_INDEXBITS) /* 2048 */
-
-#define SGSOsc_LUT_MAXVAL ((1<<15) - 1)
-#define SGSOsc_LUT_MINVAL (-SGSOsc_LUT_MAXVAL)
-
-#define SGSOsc_PHASE_LERPSCALE (1<<(32-SGSOsc_LUT_INDEXBITS))
-#define SGSOsc_PHASE_LERPMASK  (SGSOsc_PHASE_LERPSCALE - 1)
-
-/**
- * Oscillator wave types.
- */
-enum {
-	SGS_WAVE_SIN = 0,
-	SGS_WAVE_SRS,
-	SGS_WAVE_TRI,
-	SGS_WAVE_SQR,
-	SGS_WAVE_SAW,
-	SGS_WAVE_TYPES
-};
-
-/** Oscillator LUTs, indexed by wave type value. */
-extern int16_t SGSOsc_luts[SGS_WAVE_TYPES][SGSOsc_LUT_LEN];
 
 /**
  * Oscillator data. Only includes phase, as most parameters are
@@ -58,14 +34,14 @@ typedef struct SGSOsc {
 #define SGSOsc_SET_PHASE(o, p) ((void) ((o)->phase = (p)))
 
 /**
- * Convert floating point (0.0 to 1.0) phase value to 32-bit unsigned int,
- * as used by oscillator.
+ * Convert floating point phase value (0.0 = 0 deg., 1.0 = 360 deg.)
+ * to 32-bit unsigned int, as used by oscillator.
  */
 #define SGSOsc_PHASE(p) ((uint32_t) lrint((p) * 4294967296.0))
 
 /**
  * Calculate the sample rate-dependent coefficent multiplied
- * by the frequency to give the phase increment.
+ * by the frequency to give the per-sample phase increment.
  */
 #define SGSOsc_SRATE_COEFF(srate) ((double) 4294967296.0/(srate))
 
@@ -94,15 +70,15 @@ typedef struct SGSOsc {
  */
 #define SGSOsc_RUN_S16(o, lut, coeff, freq, pm_s16, amp, s16_out) do{ \
 	uint32_t SGSOsc__phs = (o)->phase + ((pm_s16) << 16); \
-	uint32_t SGSOsc__ind = SGSOsc__phs >> (32-SGSOsc_LUT_INDEXBITS); \
-	int32_t SGSOsc__s16 = (lut)[SGSOsc__ind]; \
+	uint32_t SGSOsc__ind = SGSWave_INDEX(SGSOsc__phs); \
+	int_fast16_t SGSOsc__s16 = (lut)[SGSOsc__ind]; \
 	/* write lerp'd & scaled result */ \
 	SGSOsc__s16 = lrintf( \
 		(((float)SGSOsc__s16) + \
-		 ((float)((lut)[(SGSOsc__ind + 1) & SGSOsc_LUT_INDEXMASK] - \
+		 ((float)((lut)[(SGSOsc__ind + 1) & SGSWave_LENMASK] - \
 		          SGSOsc__s16)) * \
-		 (((float)(SGSOsc__phs & SGSOsc_PHASE_LERPMASK)) * \
-		  (1.f / SGSOsc_PHASE_LERPSCALE))) * \
+		 (((float)(SGSOsc__phs & SGSWave_SCALEMASK)) * \
+		  (1.f / SGSWave_SCALE))) * \
 		(amp)); \
 	(s16_out) = SGSOsc__s16; \
 	/* update phase */ \
@@ -115,19 +91,17 @@ typedef struct SGSOsc {
  */
 #define SGSOsc_RUN_SF(o, lut, coeff, freq, pm_s16, sf_out) do{ \
 	uint32_t SGSOsc__phs = (o)->phase + ((pm_s16) << 16); \
-	uint32_t SGSOsc__ind = SGSOsc__phs >> (32-SGSOsc_LUT_INDEXBITS); \
-	int32_t SGSOsc__s16 = (lut)[SGSOsc__ind]; \
+	uint32_t SGSOsc__ind = SGSWave_INDEX(SGSOsc__phs); \
+	int_fast16_t SGSOsc__s16 = (lut)[SGSOsc__ind]; \
 	/* write lerp'd & scaled result */ \
 	(sf_out) = ((float)(SGSOsc__s16) + \
-		    ((float)((lut)[(SGSOsc__ind + 1) & SGSOsc_LUT_INDEXMASK] - \
+		    ((float)((lut)[(SGSOsc__ind + 1) & SGSWave_LENMASK] - \
 		             SGSOsc__s16)) * \
-		    (((float)(SGSOsc__phs & SGSOsc_PHASE_LERPMASK)) * \
-		     (1.f / SGSOsc_PHASE_LERPSCALE))) * \
-	           (1.f / ((float) SGSOsc_LUT_MAXVAL * 2)) + \
+		    (((float)(SGSOsc__phs & SGSWave_SCALEMASK)) * \
+		     (1.f / SGSWave_SCALE))) * \
+	           (1.f / ((float) SGSWave_MAXVAL * 2)) + \
 	           .5f; \
 	/* update phase */ \
 	uint32_t SGSOsc__inc = lrint((coeff)*(freq)); \
 	(o)->phase += SGSOsc__inc; \
 }while(0)
-
-extern void SGSOsc_global_init(void);
