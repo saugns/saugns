324589a24b03e9404d262b672b45b1311266efb3
Joel K. Pettersson <joelkpettersson@gmail.com>
2018-02-09 16:16:45 +0100

    Add 3 wave types. Basic syntax changes. Reorganize scripts.
    Script syntax changes:
     * Scope nesting syntax: Change from "<...>" to "[...]".
       ('<' and '>' are now unused. "[...]" is now used both for
       lists of objects, and arguments with several sub-values.)
     * PM modulator list prefix: Change from "p!" to "p*".
       (I.e., a list uses "p*[]". Since semantics differ for PM
       lists vs. AM and FM lists, make this clearer in the syntax.
       For PM lists, modulator amplitudes are summed,
       while for AM and FM lists, they are multiplied.)
     * Numerical expressions:
       Fix really old bug which prevented evaluation
       of "Y" in an expression of the form "((X)Y)".
    
    Split osc module into osc and wave. Wave type changes:
     * Add 'sha' (half-frequency absolute sine).
     * Add 'szh' (half-zero'd sine).
     * Add 'shh' (first quarter of sine).
     * Rename 'srs' (square root of sine) to 'ssr'.
    
    Move wave type names from parser to wave module.
    
    Add -v version number, initially at v0.2.3.
    
    Reorganize example and test scripts. Move some old and useful
    scripts from devtests/ to examples/tests/. Rename scripts,
    add descriptions to some of them, modify a few, remove a few
    which are useless, and add more wave type testing.
    
    Change log:
     * 2018-04-18: Move wave type names to osc module, add new
           'sab' wave type, rename 'srs' to 'ssr'.
           Reorganize and update example and test scripts.
     * 2018-06-02: Add 'shw' and 'shr' wave types, and
           further LUT init code changes.
     * 2018-06-30: Split osc module into wave and osc.
     * 2018-09-12: Rename wave types
           ("sab"->"sah", "shw"->"szh", "shr"->"szhhr").
     * 2018-09-27: Rename wave type 'sah'->'sha',
           add 'shh', comment out 'szhhr'.
     * 2018-09-29: Rename debug #define switches in sgensys.h.
     * 2018-10-16: Update headers, heading comments, for added code.
           Complete branch merging.
     * 2018-10-19: Change PM list syntax from "p!<>" to "p*<>".
     * 2018-12-21: Fix two outdated scripts in devtests/.
           Fix really old bug in parsing of numerical
           expressions with nested parentheses, which
           prevented "Y" in an expression of the form
           ((X)Y) from being evaluated.
     * 2019-06-05: Change scope nesting syntax
           from "<...>" to "[...]".
           Add -v version, initially at v0.3.0.
     * 2019-06-13: Set version to v0.2.3.
     * 2019-06-30: Update debug printout details
           to match June syntax changes.

--- program.c
+++ program.c
@@ -412,9 +412,9 @@ static SGSProgram* build_program(SGSParserResult *pr) {
       else
         printf("\n\top %d \tt=%d \tf=%.f", oop->operator_id, oop->time_ms, oop->freq);
       if (ga != NULL) {
-        print_linked("\n\t    f!<", ">", ga->fmodc, ga->adjcs);
-        print_linked("\n\t    p!<", ">", ga->pmodc, &ga->adjcs[ga->fmodc]);
-        print_linked("\n\t    a!<", ">", ga->amodc, &ga->adjcs[ga->fmodc +
+        print_linked("\n\t    f![", "]", ga->fmodc, ga->adjcs);
+        print_linked("\n\t    p*[", "]", ga->pmodc, &ga->adjcs[ga->fmodc]);
+        print_linked("\n\t    a![", "]", ga->amodc, &ga->adjcs[ga->fmodc +
                                                                ga->pmodc]);
       }
     }
@@ -424,7 +424,7 @@ static SGSProgram* build_program(SGSParserResult *pr) {
   return prg;
 }
 
-#if USE_LEXER
+#if TEST_LEXER
 #include "lexer.h"
 #endif
 /**
@@ -433,7 +433,7 @@ static SGSProgram* build_program(SGSParserResult *pr) {
  * Return SGSProgram if successful, NULL on error.
  */
 SGSProgram* SGS_open_program(const char *filename) {
-#if USE_LEXER
+#if TEST_LEXER
 	SGSSymtab *symtab = SGS_create_symtab();
 	SGSLexer *lexer = SGS_create_lexer(filename, symtab);
 	if (!lexer) return NULL;
