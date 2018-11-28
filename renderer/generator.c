933dc8a7b5f579445b14ee513e5bcb04a9a618f0
Joel K. Pettersson <joelkpettersson@gmail.com>
2018-11-28 23:03:38 +0100

    Use restrict keyword. Update test code & split out as program.
    Use restrict keyword for pointer function parameters
    throughout code. For now, this is the only use.
    Makes for a minor size and speed improvement.
    The design has been checked for safety with this,
    though something could have been missed.
    
    Various smaller updates for test lexer and scanner code.
    
    Split out usage of the test lexer and test scanner
    into the separate 'test-builder' program,
    built with 'make test'.
    
    Also add install/uninstall targets to Makefile.
    
    Change log:
     * 2018-11-18: Reduce lexer char switch cases to values 0-127,
           use default for the identical rest.
           Reduce scanner char handler array to 128 values,
           use entry for 0 when char > 127.
     * 2018-11-19: Move test lexer use to
           separate 'test-builder' program,
           instead of conditionally compiling it into sgensys.
           Add (unused) SGS_Scanner_handle_char1comments()
           for first-character-only line comment openers.
     * 2018-11-22: Move test scanner use to
           'test-builder' program.
           Complete branch merging.
     * 2018-11-23: Add SGS_Scanner_close().
     * 2018-11-28: Use restrict keyword.
           For now, only add to function argument pointers.
           Reduces size of the binary somewhat,
           and produces very marginal speed-up.
     * 2019-05-06: Move back to after new v0.4.2.
           Increase version to v0.4.3.
           Change test scanner file handling,
           write new SGS_Scanner_open().
           Copy scanner file handling to lexer.
           Update test-builder for new code.
     * 2019-05-10: Tweak usage notice.
           Fix missing part of scanner.c merge.
     * 2019-06-13: Set version to v0.2.8. Re-merge tidying.
     * 2019-06-22: Makefile: add install/uninstall targets.
     * 2019-09-12: Merge diff for renaming slope module to ramp.
     * 2020-07-13: Keep version at v0.2-beta.

--- renderer/generator.c
+++ renderer/generator.c
@@ -91,7 +91,8 @@ struct SGS_Generator {
 // maximum number of buffers needed for op nesting depth
 #define COUNT_BUFS(op_nest_depth) ((1 + (op_nest_depth)) * 4)
 
-static bool alloc_for_program(SGS_Generator *o, SGS_Program *prg) {
+static bool alloc_for_program(SGS_Generator *restrict o,
+                              SGS_Program *restrict prg) {
   size_t i;
 
   i = prg->ev_count;
@@ -124,8 +125,8 @@ ERROR:
   return false;
 }
 
-static bool convert_program(SGS_Generator *o, SGS_Program *prg,
-                            uint32_t srate) {
+static bool convert_program(SGS_Generator *restrict o,
+                            SGS_Program *restrict prg, uint32_t srate) {
   if (!alloc_for_program(o, prg))
     return false;
 
@@ -168,7 +169,8 @@ static bool convert_program(SGS_Generator *o, SGS_Program *prg,
 /**
  * Create instance for program \p prg and sample rate \p srate.
  */
-SGS_Generator* SGS_create_Generator(SGS_Program *prg, uint32_t srate) {
+SGS_Generator* SGS_create_Generator(SGS_Program *restrict prg,
+                                    uint32_t srate) {
   SGS_MemPool *mem = SGS_create_MemPool(0);
   if (!mem)
     return NULL;
@@ -189,7 +191,7 @@ SGS_Generator* SGS_create_Generator(SGS_Program *prg, uint32_t srate) {
 /**
  * Destroy instance.
  */
-void SGS_destroy_Generator(SGS_Generator *o) {
+void SGS_destroy_Generator(SGS_Generator *restrict o) {
   if (!o)
     return;
   free(o->bufs);
@@ -199,7 +201,8 @@ void SGS_destroy_Generator(SGS_Generator *o) {
 /*
  * Set voice duration according to the current list of operators.
  */
-static void set_voice_duration(SGS_Generator *o, VoiceNode *vn) {
+static void set_voice_duration(SGS_Generator *restrict o,
+                               VoiceNode *restrict vn) {
   uint32_t time = 0;
   for (uint32_t i = 0; i < vn->op_count; ++i) {
     const SGS_ProgramOpRef *or = &vn->graph[i];
@@ -214,7 +217,7 @@ static void set_voice_duration(SGS_Generator *o, VoiceNode *vn) {
 /*
  * Process one event; to be called for the event when its time comes.
  */
-static void handle_event(SGS_Generator *o, EventNode *e) {
+static void handle_event(SGS_Generator *restrict o, EventNode *restrict e) {
   if (1) /* more types to be added in the future */ {
     /*
      * Set state of operator and/or voice. Voice updates must be done last,
@@ -301,9 +304,10 @@ static void handle_event(SGS_Generator *o, EventNode *e) {
  * If not NULL, \p modbuf will be used as a sequence of value
  * multipliers. This is used to get actual values from ratios.
  */
-static bool run_param(SGS_Generator *o,
-                      float *buf, uint32_t buf_len, SGS_Ramp *ramp,
-                      float *state, const float *modbuf) {
+static bool run_param(SGS_Generator *restrict o,
+                      float *restrict buf, uint32_t buf_len,
+                      SGS_Ramp *restrict ramp, float *restrict state,
+                      const float *restrict modbuf) {
   uint32_t i;
   if (!ramp) {
     float s0 = *state;
@@ -336,8 +340,10 @@ static bool run_param(SGS_Generator *o,
  *
  * Returns number of samples generated for the node.
  */
-static uint32_t run_block(SGS_Generator *o, Buf *bufs, uint32_t buf_len,
-                          OperatorNode *n, float *parent_freq,
+static uint32_t run_block(SGS_Generator *restrict o,
+                          Buf *restrict bufs, uint32_t buf_len,
+                          OperatorNode *restrict n,
+                          float *restrict parent_freq,
                           bool wave_env, uint32_t acc_ind) {
   uint32_t i, len;
   int32_t *sbuf = bufs->i, *pm;
@@ -510,8 +516,8 @@ static uint32_t run_block(SGS_Generator *o, Buf *bufs, uint32_t buf_len,
  * The second generator buffer is used for panning if dynamic panning
  * is used.
  */
-static void mix_output(SGS_Generator *o, VoiceNode *vn,
-                       int16_t **st_out, uint32_t len) {
+static void mix_output(SGS_Generator *restrict o, VoiceNode *restrict vn,
+                       int16_t **restrict st_out, uint32_t len) {
   int32_t *s_buf = o->bufs[0].i;
   float scale = o->amp_scale;
   if (vn->attr & SGS_PVOA_RAMP_PAN) {
@@ -542,8 +548,8 @@ static void mix_output(SGS_Generator *o, VoiceNode *vn,
  *
  * Returns number of samples generated for the voice.
  */
-static uint32_t run_voice(SGS_Generator *o, VoiceNode *vn,
-                          int16_t *out, uint32_t buf_len) {
+static uint32_t run_voice(SGS_Generator *restrict o, VoiceNode *restrict vn,
+                          int16_t *restrict out, uint32_t buf_len) {
   uint32_t out_len = 0;
   const SGS_ProgramOpRef *ops = vn->graph;
   uint32_t opc = vn->op_count;
@@ -584,7 +590,7 @@ RETURN:
 /*
  * Any error checking following audio generation goes here.
  */
-static void check_final_state(SGS_Generator *o) {
+static void check_final_state(SGS_Generator *restrict o) {
   for (uint16_t i = 0; i < o->vo_count; ++i) {
     VoiceNode *vn = &o->voices[i];
     if (!(vn->flags & VN_INIT)) {
@@ -605,8 +611,9 @@ static void check_final_state(SGS_Generator *o) {
  * Return true as long as there are more samples to generate, false
  * when the end of the signal has been reached.
  */
-bool SGS_Generator_run(SGS_Generator *o, int16_t *buf, size_t buf_len,
-                       size_t *out_len) {
+bool SGS_Generator_run(SGS_Generator *restrict o,
+                       int16_t *restrict buf, size_t buf_len,
+                       size_t *restrict out_len) {
   int16_t *sp = buf;
   uint32_t i, len = buf_len;
   for (i = len; i--; sp += 2) {
