4d7306677367b4b6c787af3d43fb9f353596a84a
Joel K. Pettersson <joelkpettersson@gmail.com>
2018-11-28 23:03:38 +0100

    Use restrict keyword. Update test code & split out as program.
    Use restrict keyword for pointer function parameters
    throughout code. For now, this is the only use.
    Makes for a minor size and speed improvement.
    The design has been checked for safety with this,
    though something could have been missed.
    
    Various smaller updates for test lexer and scanner code.
    
    Split out usage of the test lexer and test scanner
    into the separate 'test-builder' program,
    built with 'make test'.
    
    Also add install/uninstall targets to Makefile.
    
    Change log:
     * 2018-11-18: Reduce lexer char switch cases to values 0-127,
           use default for the identical rest.
           Reduce scanner char handler array to 128 values,
           use entry for 0 when char > 127.
     * 2018-11-19: Move test lexer use to
           separate 'test-builder' program,
           instead of conditionally compiling it into sgensys.
           Add (unused) SGS_Scanner_handle_char1comments()
           for first-character-only line comment openers.
     * 2018-11-22: Move test scanner use to
           'test-builder' program.
           Complete branch merging.
     * 2018-11-23: Add SGS_Scanner_close().
     * 2018-11-28: Use restrict keyword.
           For now, only add to function argument pointers.
           Reduces size of the binary somewhat,
           and produces very marginal speed-up.
     * 2019-05-06: Move back to after new v0.4.2.
           Increase version to v0.4.3.
           Change test scanner file handling,
           write new SGS_Scanner_open().
           Copy scanner file handling to lexer.
           Update test-builder for new code.
     * 2019-05-10: Tweak usage notice.
           Fix missing part of scanner.c merge.
     * 2019-06-13: Set version to v0.2.8. Re-merge tidying.
     * 2019-06-22: Makefile: add install/uninstall targets.
     * 2019-09-12: Merge diff for renaming slope module to ramp.
     * 2020-07-13: Keep version at v0.2-beta.

--- builder.c
+++ builder.c
@@ -13,11 +13,6 @@
 
 #include "sgensys.h"
 #include "script.h"
-#if SGS_TEST_SCANNER
-# include "builder/scanner.h"
-#elif SGS_TEST_LEXER
-# include "builder/lexer.h"
-#endif
 #include "builder/file.h"
 #include <string.h>
 #include <stdlib.h>
@@ -28,11 +23,11 @@
  *
  * \return instance or NULL on error
  */
-static SGS_File *open_file(const char *script_arg, bool is_path) {
+static SGS_File *open_file(const char *restrict script_arg, bool is_path) {
 	SGS_File *f = SGS_create_File();
 	if (!f) return NULL;
 	if (!is_path) {
-		SGS_File_stropenrb(f, "-e ...", script_arg);
+		SGS_File_stropenrb(f, "<string>", script_arg);
 		return f;
 	}
 	if (!SGS_File_fopenrb(f, script_arg)) {
@@ -49,50 +44,15 @@ static SGS_File *open_file(const char *script_arg, bool is_path) {
  *
  * \return instance or NULL on error
  */
-SGS_Program* SGS_build(const char *script_arg, bool is_path) {
+SGS_Program* SGS_build(const char *restrict script_arg, bool is_path) {
 	SGS_File *f = open_file(script_arg, is_path);
 	if (!f) return NULL;
 
 	SGS_Program *o = NULL;
-#if SGS_TEST_SCANNER
-	SGS_SymTab *symtab = SGS_create_SymTab();
-	SGS_Scanner *scanner = SGS_create_Scanner(f, symtab);
-	if (!scanner) {
-		SGS_destroy_SymTab(symtab);
-		goto CLOSE;
-	}
-	for (;;) {
-		char c = SGS_Scanner_getc(scanner);
-		if (!c) {
-			putchar('\n');
-			break;
-		}
-		putchar(c);
-	}
-	SGS_destroy_Scanner(scanner);
-	SGS_destroy_SymTab(symtab);
-	o = (SGS_Program*) calloc(1, sizeof(SGS_Program)); // placeholder
-#elif SGS_TEST_LEXER
-	SGS_SymTab *symtab = SGS_create_SymTab();
-	SGS_Lexer *lexer = SGS_create_Lexer(f, symtab);
-	if (!lexer) {
-		SGS_destroy_SymTab(symtab);
-		goto CLOSE;
-	}
-	for (;;) {
-		SGS_ScriptToken token;
-		if (!SGS_Lexer_get(lexer, &token)) break;
-	}
-	SGS_destroy_Lexer(lexer);
-	SGS_destroy_SymTab(symtab);
-	o = (SGS_Program*) calloc(1, sizeof(SGS_Program)); // placeholder
-#else // OLD PARSER
 	SGS_Script *sd = SGS_load_Script(f);
 	if (!sd) goto CLOSE;
-
 	o = SGS_build_Program(sd);
 	SGS_discard_Script(sd);
-#endif
 CLOSE:
 	SGS_destroy_File(f);
 	return o;
