b5894ca84dc748d599e66f5e10f4b96e16aaa43a
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-04-30 00:17:43 +0200

    Add -e string evaluation option. Add test scanner.
    Support direct evaluation of a string argument with
    a new -e option. E.g., ./sgensys -e "Osin f643 t1"
    will run (play) a 643 Hz oscillator for 1 second.
    
    Add experimental test scanner, a reworking of
    the test lexer; a draft for future redesign,
    as in breaking up the tasks of the lexer
    and/or an intermediate parser redesign.
    
    Add file module string open/read/close code,
    and change sgensys, builder, parser, lexer to use it.
    Also add support to file for creating nested instances,
    in preparation for future nested file usage.
    
    Updates to the test lexer.
    
    Change log:
     * 2019-04-24: Add file module string open/read/close code.
     * 2019-04-25: Name changes in file.
     * 2019-04-29: Move back to after new v0.4.0.
     * 2019-04-30: Add -e string evaluation option.
           Also change file module to support nested (linked)
           instances, in preparation for future addition
           of nested file usage (prepend/predefine arg,
           file includes from file, etc.).
           Increase version to v0.4.1.
           Merge test lexer changes,
           with some final tweaks.
       Old branch:
        - 2018-11-04: Import updated test lexer code.
              (Drop new test scanner for now.)
        - 2018-11-09: Silence warnings in test lexer.
        - 2018-11-15: Use SGS_File_skipline() in test lexer.
        - 2018-11-16: Debug test lexer, also a few clean-ups
              and performance tweaks.
        - 2018-11-18: Minor fixes and tweaks for test lexer.
        - 2018-11-19: Minor clean-up in test lexer.
        - 2019-01-14: Use SGS_File_trynewline() in test lexer.
     * 2019-05-04: Add test scanner module,
           merging tidied up basic version.
       Old branch:
        - 2018-03-25: Add scanner module (incomplete)
              for use in parser (next) and lexer (later).
        - 2018-04-07: Some debugging and improvements
              for test scanner.
        - 2018-04-13: Improve test scanner design and
              add C-style comment support.
        - 2018-11-05: Complete branch merging.
              (Add the postponed test scanner.)
              Minor clean-up in test scanner.
        - 2018-11-09: Silence warnings in test scanner.
        - 2018-11-15: Use SGS_File_skipspace(),
              SGS_File_skipline(), in test scanner.
        - 2018-11-18: Properly check scanner EOF status for error.
        - 2018-11-19: Renaming and minor changes in test scanner.
              Use symtab in scanner symbol string reading.
        - 2019-01-14: Use SGS_File_trynewline() in test scanner.
     * 2019-05-08: Tweak usage notice.
     * 2019-05-10: Add -e quick test to README.
           Fix parser string matching when item
           ends with end of file marker
           (as happens with -e "Osin").
           Fix missing part of scanner.c merge.
     * 2019-06-13: Set version to v0.2.7.
           Merge minor code tidying for lexer.c, scanner.c.
     * 2020-02-09: Simplify usage notice.
     * 2020-07-13: Keep version at v0.2-beta.

--- builder/lexer.h
+++ builder/lexer.h
@@ -1,5 +1,5 @@
 /* sgensys: Script lexer module.
- * Copyright (c) 2014, 2017-2018 Joel K. Pettersson
+ * Copyright (c) 2014, 2017-2019 Joel K. Pettersson
  * <joelkpettersson@gmail.com>.
  *
  * This file and the software of which it is part is distributed under the
@@ -14,72 +14,38 @@
 #pragma once
 #include "symtab.h"
 
-/**
- * For those 1-character (special character) tokens that are defined, passing
- * the character will yield the token number.
- */
-#define SGS_T_1CT(c) (0x100 + (c))
-
 /*
  * Token enumerations.
  */
 enum {
-	SGS_T_ERROR = -1,
-	SGS_T_EOF = 0,
-	SGS_T_INVALID = 1,
+	SGS_T_INVALID = 0,
 	SGS_T_ID_STR,
-	SGS_T_INT_NUM,
-	SGS_T_REAL_NUM,
-	/* individual special characters */
-	SGS_T_BANG           = SGS_T_1CT('!'),
-	SGS_T_QUOTATIONMARK  = SGS_T_1CT('"'),
-	SGS_T_NUMBERSIGN     = SGS_T_1CT('#'),
-	SGS_T_DOLLARSIGN     = SGS_T_1CT('$'),
-	SGS_T_PERCENTSIGN    = SGS_T_1CT('%'),
-	SGS_T_AMPERSAND      = SGS_T_1CT('&'),
-	SGS_T_APOSTROPHE     = SGS_T_1CT('\''),
-	SGS_T_LPARENTHESIS   = SGS_T_1CT('('),
-	SGS_T_RPARENTHESIS   = SGS_T_1CT(')'),
-	SGS_T_ASTERISK       = SGS_T_1CT('*'),
-	SGS_T_PLUS           = SGS_T_1CT('+'),
-	SGS_T_COMMA          = SGS_T_1CT(','),
-	SGS_T_MINUS          = SGS_T_1CT('-'),
-	SGS_T_DOT            = SGS_T_1CT('.'),
-	SGS_T_SLASH          = SGS_T_1CT('/'),
-	SGS_T_COLON          = SGS_T_1CT(':'),
-	SGS_T_SEMICOLON      = SGS_T_1CT(';'),
-	SGS_T_LESSTHAN       = SGS_T_1CT('<'),
-	SGS_T_EQUALSSIGN     = SGS_T_1CT('='),
-	SGS_T_GREATERTHAN    = SGS_T_1CT('>'),
-	SGS_T_QUESTIONMARK   = SGS_T_1CT('?'),
-	SGS_T_ATSIGN         = SGS_T_1CT('@'),
-	SGS_T_LSQUAREBRACKET = SGS_T_1CT('['),
-	SGS_T_BACKSLASH      = SGS_T_1CT('\\'),
-	SGS_T_RSQUAREBRACKET = SGS_T_1CT(']'),
-	SGS_T_CARET          = SGS_T_1CT('^'),
-	SGS_T_UNDERSCORE     = SGS_T_1CT('_'),
-	SGS_T_BACKTICK       = SGS_T_1CT('`'),
-	SGS_T_LCURLYBRACKET  = SGS_T_1CT('{'),
-	SGS_T_PIPE           = SGS_T_1CT('|'),
-	SGS_T_RCURLYBRACKET  = SGS_T_1CT('}'),
-	SGS_T_TILDE          = SGS_T_1CT('~'),
+	SGS_T_VAL_INT,
+	SGS_T_VAL_REAL,
+	SGS_T_SPECIAL,
 };
 
 typedef struct SGS_ScriptToken {
-	int32_t type;
+	uint32_t type;
 	union {
 		const char *id;
+		int32_t i;
+		float f;
+		uint8_t b;
+		char c;
 	} data;
 } SGS_ScriptToken;
 
 struct SGS_Lexer;
 typedef struct SGS_Lexer SGS_Lexer;
 
-SGS_Lexer *SGS_create_Lexer(const char *fname, SGS_SymTab *symtab)
+struct SGS_File;
+SGS_Lexer *SGS_create_Lexer(struct SGS_File *f, SGS_SymTab *symtab)
 	SGS__malloclike;
 void SGS_destroy_Lexer(SGS_Lexer *o);
 
-SGS_ScriptToken *SGS_Lexer_get_token(SGS_Lexer *o);
+bool SGS_Lexer_get(SGS_Lexer *o, SGS_ScriptToken *t);
+bool SGS_Lexer_get_special(SGS_Lexer *o, SGS_ScriptToken *t);
 
 void SGS_Lexer_warning(SGS_Lexer *o, const char *fmt, ...);
 void SGS_Lexer_error(SGS_Lexer *o, const char *fmt, ...);
