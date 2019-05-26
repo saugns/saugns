51aba02b210fdcfb330ace4a15241a20f6ddc61c
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-05-26 06:34:45 +0000

    Add 'hold' ramp curve. Refactor scanning & ramp params.
    Recognize 'hold' ramp curve in scripts.
    Use new SGS_RampParam type for ramp parameters
    in script.h and program.h types.
    
    Break out PScanner struct from SGS_Parser.
    Refactoring without functional change,
    in preparation for merge with test scanner.
     * Move line num changes into basic scan_* functions.
     * Rearrange code, placing PScanner above Parser.
     * Rename scan_char() to scan_getc(),
       add scan_stashc() for next_c logic.
     * Add ScanLookup struct, stored in parser and pointed
       to by its scanner; preparation for future redesign,
       where the parser needs to associate additional data
       with the scanner.
     * Replace parser string matching
       using new SGS_SymTab_pool_stra()
       and string pool pointer comparison.
    
    Redesign test scanner. A start for bridging the design
    gap for the parser:
     * Separate getc and nospace version of getc
       (both types will be needed by the parser).
     * Break out update_cframe() and set_prevc(),
       use in get functions.
     * Renaming and minor tweaks.
    
    Simplify the file module (and change SGS_File_AFTER_EOF()
    to work regardless of whether pos was masked).
    No functional change for uses.
    
    Change log:
     * 2018-11-09: Rename slope-using params and their flags.
     * 2018-11-14: Add builder/script/param.h with
           type SGS_TimedParam, use in script.h types.
           Add slope change type 'hold'
           (by enabling and renaming 'state').
     * 2018-11-22: Move builder/script/param.h to program/param.h.
           Use SGS_TimedParam in program.h types, handling
           conversion directly in generator (removing
           the small added function from parseconv).
           Remove obsolete *_SLOPE parameter flags,
           keeping attribute *_SLOPE flags for now.
           Complete branch merging.
     * 2018-11-23: Refactor scanning code in parser,
           in preparation for merge with test scanner.
     * 2019-05-10: Merge changes.
           Adjust parser refactoring for new file code
           (slightly simplifying it).
           Move out param.h from builder/.
     * 2019-05-13: Place before new v0.5.0.
           Increase version to v0.4.4.
           Merge further parser refactoring (and polish it),
           along with some test scanner changes.
       Old branch:
        - 2018-12-04: Replace parser string matching
              using new SGS_SymTab_pool_stra()
              and string pool pointer comparison.
              Complete branch merging.
        - 2018-12-06: scanner: Rename old SGS_Scanner_getc() to
              SGS_Scanner_getc_nospace(), adding a more
              space-preserving function with the old name.
              Some further renaming and tweaks.
        - 2018-12-13: scanner: Break out update_cframe() and
              set_prevc(), use in get functions,
              and further changes.
     * 2019-05-15: Merge param.h into program.h.
           Rename SGS_TimedParam to SGS_SlopeParam.
     * 2019-05-26: Merge file module clean-up back.
           Update comments, refactor a bit more.
       Old branch:
        - 2019-05-24: Simplify file module.
     * 2019-05-27: Avoid future problems by
           ensuring SGS_File_AFTER_EOF()
           works after mask applied to pos.
     * 2019-06-05: Merge parser diff after v0.2.3 syntax change.
     * 2019-06-13: Set version to v0.2.9.
     * 2019-06-16: Adjust parser for new v0.2.4 syntax changes.
     * 2019-06-18: Place after new v0.2.9, set version to v0.2.10.
     * 2019-09-12: Merge diff for renaming slope module to ramp.
           Further rename SGS_SlopeParam to SGS_RampParam.
     * 2019-12-21: Merge 'S a' multiplier apply-to-ramp fix.
     * 2019-12-24: Adapt v0.2.5 changes to
           use 'S t' as default time placeholder for ramps.
     * 2020-07-13: Keep version at v0.2-beta.

--- script.h
+++ script.h
@@ -12,8 +12,8 @@
  */
 
 #pragma once
-#include "program.h"
 #include "ptrlist.h"
+#include "program.h"
 
 /**
  * Script data operator flags.
@@ -32,19 +32,19 @@ enum {
  */
 typedef struct SGS_ScriptOpData {
 	struct SGS_ScriptEvData *event;
-	SGS_PtrList on_next; /* all immediate forward refs for op(s) */
 	struct SGS_ScriptOpData *on_prev; /* preceding for same op(s) */
+	SGS_PtrList on_next; /* all immediate forward refs for op(s) */
 	struct SGS_ScriptOpData *next_bound;
-	uint32_t op_flags;
 	const char *label;
+	uint32_t op_flags;
 	/* operator parameters */
 	uint32_t op_id; /* not used by parser; for program module */
 	uint32_t op_params;
+	uint32_t time_ms, silence_ms;
 	uint8_t attr;
 	uint8_t wave;
-	uint32_t time_ms, silence_ms;
-	float freq, dynfreq, phase, amp, dynamp;
-	SGS_Ramp ramp_freq, ramp_amp;
+	SGS_RampParam freq, amp;
+	float phase, dynfreq, dynamp;
 	/* node adjacents in operator linkage graph */
 	SGS_PtrList fmods, pmods, amods;
 } SGS_ScriptOpData;
@@ -76,8 +76,7 @@ typedef struct SGS_ScriptEvData {
 	uint32_t vo_params;
 	struct SGS_ScriptEvData *voice_prev; /* preceding event for voice */
 	uint8_t vo_attr;
-	float pan;
-	SGS_Ramp ramp_pan;
+	SGS_RampParam pan;
 	SGS_PtrList op_graph;
 } SGS_ScriptEvData;
 
