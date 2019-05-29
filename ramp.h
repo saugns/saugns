87bf304ade9102617811116d1a24f7c8e3fd1e93
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-05-29 14:32:56 +0200

    Rework audio mixing. Refactor ramp params. Clean-up.
    Use floating-point processing in generator
    internally, until writing 16-bit output,
    instead of mixing float and int buffers.
    For PM phase offsets, get 32-bit int values
    from float buffer, instead of 16-bit values.
    Rework mixing, add clipping for audio output.
    Also move oscillator block running to osc.c
    (split from generator.c).
    
    Further refactor ramp parameters and their uses:
     * Remove the old SGS_Ramp type,
       renaming the SGS_RampParam type to SGS_Ramp.
     * Separate the ramp fill functions from the
       type and its uses. (E.g. future envelope support
       could use the fill functions with a different
       data structure and surrounding logic.)
     * New SGS_Ramp_run() function fully replaces
       parameter running function in generator.c.
     * Remove voice & operator attr fields and flags;
       these are now fully replaced by the SGS_Ramp
       flags and code.
     * Cleaner code for handling ramp data in parser
       and generator.
    
    Todo: Since earlier, dynfreq/dynamp parameter
    ratio flags are ignored; to correct handling.
    For now, they are simply removed.
    
    Improve -p printout, extending info for 'f' parameter
    and adding it for 'a' on each main per-operator line.
    
    Also:
     * Refactor mempool using arrtype, and initialize
       allocated memory to zero bytes.
       Split SGS_MemPool_memdup() from SGS_MemPool_alloc().
     * Refactor symtab, without functional change.
     * Rename arrtype SGS_UInt8Arr to SGS_ByteArr.
     * Minor refactoring in renderer.c.
    
    Increase version to v0.2.11.
    
    Change log:
     * 2018-11-08: Refactor mempool using arrtype.
           Move wave module to program/.
           Split SGS_MemPool_memdup()
           from SGS_MemPool_alloc().
           Fix Makefile.
     * 2018-11-09: Complete changing generator to use float
           internally. Rework mixing, clip output.
           Move script.h and ptrlist to builder/.
           Complete branch merging.
     * 2018-11-11: Minor refactoring in generator.
           Minor comment clean-up.
     * 2019-01-14: Complete branch merging.
           Minor clean-up.
           Set version to v0.5.0.
     * 2019-05-10: Merge changes.
           Undo file moves.
     * 2019-05-13: Place after new v0.4.4.
     * 2019-05-17: Merge refactoring of slope parameter handling,
           and accompanying changes, with adjustments.
           Merge param module into slope module, and
           re-rename SGS_TimedParam type to SGS_Slope.
           Also refactor symtab, without functional change.
       Old branch:
        - 2018-11-27: Refactor timed param handling,
              replacing the SGS_Slope type with use of
              SGS_TimedParam, and more fully moving out
              code for parameter "running" from generator.
              Remove "attr" fields and flags.
              Improve -p printout (more 'f', added 'a', info).
        - 2018-12-01: Add SGS_TimedParam_ENABLED(),
              refactor parser and generator handling of flags.
     * 2019-05-28: Merge refactoring.
       Old branch:
        - 2019-02-07: Move osc block running to osc.c
              (split from generator.c).
        - 2019-02-10: Rename SAU_UInt8Arr to SAU_ByteArr.
     * 2019-06-05: Merge diff after v0.2.3 syntax change.
     * 2019-06-13: Set version to v0.3.0.
     * 2019-06-16: Merge back bugfix for generator timing.
           Adjust parser for new v0.2.4 syntax changes.
     * 2019-06-18: Split out v0.2.9, place after v0.2.10.
     * 2019-06-22: Comment clean-up in mempool.c.
           Minor refactoring in renderer.c.
           Set version to v0.2.11.
     * 2019-08-07: Merge Makefile -ffast-math portability fix.
     * 2019-09-12: Move down renaming of slope module to ramp.
     * 2019-12-21: Merge 'S a' multiplier apply-to-ramp fix.
     * 2019-12-24: Merge clean-up changes for v0.2.5.
           Adapt v0.2.5 changes to use 'S t' as
           default time placeholder for ramps.
     * 2019-12-25: Fix partial ramp update (using the new flag),
           after renaming it (flip to _SET instead of _DEFAULT).

--- ramp.h
+++ ramp.h
@@ -15,31 +15,75 @@
 #include "common.h"
 
 /**
- * Ramp types.
+ * Ramp curves.
  */
 enum {
-	SGS_RAMP_HOLD = 0,
-	SGS_RAMP_LIN,
-	SGS_RAMP_EXP,
-	SGS_RAMP_LOG,
-	SGS_RAMP_TYPES
+	SGS_RAC_HOLD = 0,
+	SGS_RAC_LIN,
+	SGS_RAC_EXP,
+	SGS_RAC_LOG,
+	SGS_RAC_TYPES
 };
 
-/** Names of ramp types, with an extra NULL pointer at the end. */
-extern const char *const SGS_Ramp_names[SGS_RAMP_TYPES + 1];
+/** Names of ramp curve types, with an extra NULL pointer at the end. */
+extern const char *const SGS_RampCurve_names[SGS_RAC_TYPES + 1];
+
+typedef void (*SGS_RampCurve_f)(float *restrict buf, uint32_t len,
+		float v0, float vt, uint32_t pos, uint32_t time);
+
+/** Functions for ramp curve types. */
+extern const SGS_RampCurve_f SGS_RampCurve_funcs[SGS_RAC_TYPES];
+
+void SGS_RampCurve_hold(float *restrict buf, uint32_t len,
+		float v0, float vt,
+		uint32_t pos, uint32_t time);
+void SGS_RampCurve_lin(float *restrict buf, uint32_t len,
+		float v0, float vt,
+		uint32_t pos, uint32_t time);
+void SGS_RampCurve_exp(float *restrict buf, uint32_t len,
+		float v0, float vt,
+		uint32_t pos, uint32_t time);
+void SGS_RampCurve_log(float *restrict buf, uint32_t len,
+		float v0, float vt,
+		uint32_t pos, uint32_t time);
 
 /**
- * Ramp, used for gradual value change.
+ * Ramp parameter flags.
+ */
+enum {
+	SGS_RAMP_STATE       = 1<<0, // v0 set
+	SGS_RAMP_STATE_RATIO = 1<<1,
+	SGS_RAMP_CURVE       = 1<<2, // vt and time_ms set
+	SGS_RAMP_CURVE_RATIO = 1<<3,
+	SGS_RAMP_TIME_SET    = 1<<4, // manually used for tracking changes
+};
+
+/**
+ * Ramp parameter type.
  *
- * The \a pos field keeps track of position in samples;
- * reset to 0 when running for a new duration.
+ * Holds data for parameters with support for gradual change,
+ * both during script processing and audio rendering.
  */
 typedef struct SGS_Ramp {
+	float v0, vt;
 	uint32_t time_ms;
-	uint32_t pos;
-	float goal;
-	uint8_t type;
+	uint8_t curve;
+	uint8_t flags;
 } SGS_Ramp;
 
-bool SGS_Ramp_run(SGS_Ramp *restrict o, uint32_t srate,
-		float *restrict buf, uint32_t buf_len, float s0);
+/**
+ * Get the main flags showing whether state and/or curve are enabled.
+ * Zero implies that the instance is unused.
+ *
+ * \return flag values
+ */
+#define SGS_Ramp_ENABLED(o) \
+	((o)->flags & (SGS_RAMP_STATE | SGS_RAMP_CURVE))
+
+void SGS_Ramp_reset(SGS_Ramp *restrict o);
+void SGS_Ramp_copy(SGS_Ramp *restrict o,
+		const SGS_Ramp *restrict src);
+
+bool SGS_Ramp_run(SGS_Ramp *restrict o, float *restrict buf,
+		uint32_t buf_len, uint32_t srate,
+		uint32_t *restrict pos, const float *restrict mulbuf);
