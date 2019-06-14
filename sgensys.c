11f0b5c2bbc606b7a98fbf703c615854fca3de67
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-06-14 06:20:45 +0200

    Support multiple script args. Use larger output buffer.
    Support multiple script files or strings per command-line
    invocation. Also make corresponding changes to the
    'test-builder' program. (Use SGS_PtrList in main module,
    builder, and renderer for lists. Combine the old behavior
    with running through a batch of things at each stage.)
    
    Use 256 ms of audio output buffering in renderer.
    Fixes stuttering on NetBSD 8 (which requires >= 128 ms
    of buffering for consistently smooth playback).
    The extra 128 ms is for the sake of portability.
    (All non-NetBSD systems I personally use have however
    worked fine with the old 1024 sample buffering.)
    
    While at it, also increase the generator's internal
    buffer length from the ancient value of 256 samples
    to 1024. (This is simply a performance vs. memory
    use trade-off. The number of buffers could potentially
    grow into the hundreds or thousands for extreme input.
    This increases memory use fairly conservatively.)
    
    Const-ify pointers to non-modified SGS_Program instances.
    
    Increase version to v0.2.9.
    
    Change log:
     * 2019-06-04: Move multiple script args support back
           to v0.5.0, use the old version number.
       Old branch:
        - 2019-02-10: Support multiple script paths per
              command-line invocation. Also make corresponding
              changes to the 'test-builder' program.
              Move ptrlist to main dir (out of builder/).
              Increase version to v0.6.1.
              Const-ify non-modified SAU_Program pointers.
        - 2019-05-28: Merge support for multiple script args,
              and refactoring. Combine with support
              for -e <string> in the simplest way.
              Set version to v0.5.2. Update README.
        - 2019-05-29: Tweak usage notice.
     * 2019-06-18: Merge audio buffering changes.
           Move it all back to after v0.2.8,
           set version to v0.2.9.
       Old branch:
        - 2019-06-13: Increase generator buffer length
              from 256 samples to 1024 samples.
        - 2019-06-14: Change renderer output buffer
              sizing to remove stuttering on NetBSD.
              (Use 256 ms instead of 1024 samples.)
              Refactor the code in the process.
              Increase version to v0.3.2.

--- sgensys.c
+++ sgensys.c
@@ -23,10 +23,10 @@
  */
 static void print_usage(bool by_arg) {
 	fputs(
-"Usage: sgensys [-a|-m] [-r srate] [-p] [-o wavfile] script\n"
-"       sgensys [-a|-m] [-r srate] [-p] [-o wavfile] -e string\n"
-"       sgensys [-c] [-p] script\n"
-"       sgensys [-c] [-p] -e string\n"
+"Usage: sgensys [-a|-m] [-r <srate>] [-p] [-o <wavfile>] <script>...\n"
+"       sgensys [-a|-m] [-r <srate>] [-p] [-o <wavfile>] -e <string>...\n"
+"       sgensys [-c] [-p] <script>...\n"
+"       sgensys [-c] [-p] -e <string>...\n"
 "\n"
 "By default, audio device output is enabled.\n"
 "\n"
@@ -36,9 +36,9 @@ static void print_usage(bool by_arg) {
 "     \tif unsupported for audio device, warns and prints rate used instead.\n"
 "  -o \tWrite a 16-bit PCM WAV file, always using the sample rate requested;\n"
 "     \tdisables audio device output by default.\n"
-"  -e \tEvaluate string instead of file.\n"
-"  -c \tCheck script only, reporting any errors or requested info.\n"
-"  -p \tPrint info for script after loading.\n"
+"  -e \tEvaluate strings instead of files.\n"
+"  -c \tCheck scripts only, reporting any errors or requested info.\n"
+"  -p \tPrint info for scripts after loading.\n"
 "  -h \tPrint this message.\n"
 "  -v \tPrint version.\n",
 	(by_arg) ? stdout : stderr);
@@ -85,21 +85,22 @@ enum {
  * \return true if args valid and script path set
  */
 static bool parse_args(int argc, char **restrict argv,
-		uint32_t *restrict flags, const char **restrict script_arg,
-		uint32_t *restrict srate, const char **restrict wav_path) {
+		uint32_t *restrict flags,
+		SGS_PtrList *restrict script_args,
+		const char **restrict wav_path,
+		uint32_t *restrict srate) {
 	int i;
 	for (;;) {
 		const char *arg;
 		--argc;
 		++argv;
 		if (argc < 1) {
-			if (!*script_arg) goto INVALID;
+			if (!script_args->count) goto INVALID;
 			break;
 		}
 		arg = *argv;
 		if (*arg != '-') {
-			if (*script_arg) goto INVALID;
-			*script_arg = arg;
+			SGS_PtrList_add(script_args, arg);
 			continue;
 		}
 NEXT_C:
@@ -123,7 +124,7 @@ NEXT_C:
 		case 'h':
 			if (*flags != 0) goto INVALID;
 			print_usage(true);
-			return false;
+			goto CLEAR;
 		case 'm':
 			if ((*flags & (ARG_ENABLE_AUDIO_DEV |
 					ARG_ONLY_COMPILE)) != 0)
@@ -160,72 +161,90 @@ NEXT_C:
 			continue;
 		case 'v':
 			print_version();
-			return false;
+			goto CLEAR;
 		default:
 			goto INVALID;
 		}
 		goto NEXT_C;
 	}
-	return (*script_arg != NULL);
-
+	return (script_args->count != 0);
 INVALID:
 	print_usage(false);
+CLEAR:
+	SGS_PtrList_clear(script_args);
 	return false;
 }
 
 /*
- * Process the given script file.
- *
- * \return true unless error occurred
+ * Discard the programs in the list, ignoring NULL entries,
+ * and clearing the list.
  */
-static bool build(const char *restrict script_arg,
-		SGS_Program **restrict prg_out,
-		uint32_t options) {
-	SGS_Program *prg;
-	bool is_path = !(options & ARG_EVAL_STRING);
-	if (!(prg = SGS_build(script_arg, is_path)))
-		return false;
-	if ((options & ARG_PRINT_INFO) != 0)
-		SGS_Program_print_info(prg);
-	if ((options & ARG_ONLY_COMPILE) != 0) {
-		SGS_discard_Program(prg);
-		*prg_out = NULL;
-		return true;
+static void discard_programs(SGS_PtrList *restrict prg_objs) {
+	SGS_Program **prgs = (SGS_Program**) SGS_PtrList_ITEMS(prg_objs);
+	for (size_t i = 0; i < prg_objs->count; ++i) {
+		SGS_discard_Program(prgs[i]);
 	}
+	SGS_PtrList_clear(prg_objs);
+}
 
-	*prg_out = prg;
+/*
+ * Process the listed scripts.
+ *
+ * \return true if at least one script succesfully built
+ */
+static bool build(const SGS_PtrList *restrict script_args,
+		SGS_PtrList *restrict prg_objs,
+		uint32_t options) {
+	bool are_paths = !(options & ARG_EVAL_STRING);
+	if (!SGS_build(script_args, are_paths, prg_objs))
+		return false;
+	if ((options & ARG_PRINT_INFO) != 0) {
+		const SGS_Program **prgs =
+			(const SGS_Program**) SGS_PtrList_ITEMS(prg_objs);
+		for (size_t i = 0; i < prg_objs->count; ++i) {
+			const SGS_Program *prg = prgs[i];
+			if (prg != NULL) SGS_Program_print_info(prg);
+		}
+	}
+	if ((options & ARG_ONLY_COMPILE) != 0) {
+		discard_programs(prg_objs);
+	}
 	return true;
 }
 
 /*
- * Produce results from the given program.
+ * Produce results from the list of programs, ignoring NULL entries.
  *
  * \return true unless error occurred
  */
-static bool render(SGS_Program *restrict prg, uint32_t srate,
-		uint32_t options, const char *restrict wav_path) {
+static bool render(const SGS_PtrList *restrict prg_objs,
+		uint32_t srate, uint32_t options,
+		const char *restrict wav_path) {
 	bool use_audiodev = (wav_path != NULL) ?
-			((options & ARG_ENABLE_AUDIO_DEV) != 0) :
-			((options & ARG_DISABLE_AUDIO_DEV) == 0);
-	return SGS_render(prg, srate, use_audiodev, wav_path);
+		((options & ARG_ENABLE_AUDIO_DEV) != 0) :
+		((options & ARG_DISABLE_AUDIO_DEV) == 0);
+	return SGS_render(prg_objs, srate, use_audiodev, wav_path);
 }
 
 /**
  * Main function.
  */
 int main(int argc, char **restrict argv) {
-	const char *script_arg = NULL, *wav_path = NULL;
+	SGS_PtrList script_args = (SGS_PtrList){0};
+	SGS_PtrList prg_objs = (SGS_PtrList){0};
+	const char *wav_path = NULL;
 	uint32_t options = 0;
-	SGS_Program *prg;
 	uint32_t srate = DEFAULT_SRATE;
-
-	if (!parse_args(argc, argv, &options, &script_arg, &srate, &wav_path))
+	if (!parse_args(argc, argv, &options, &script_args, &wav_path,
+			&srate))
 		return 0;
-	if (!build(script_arg, &prg, options))
+	bool error = !build(&script_args, &prg_objs, options);
+	SGS_PtrList_clear(&script_args);
+	if (error)
 		return 1;
-	if (prg != NULL) {
-		bool error = !render(prg, srate, options, wav_path);
-		SGS_discard_Program(prg);
+	if (prg_objs.count > 0) {
+		error = !render(&prg_objs, srate, options, wav_path);
+		discard_programs(&prg_objs);
 		if (error)
 			return 1;
 	}
