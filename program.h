d069bed2a6b6bb86a1e790e9dcdaedf37f4632ce
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-06-14 06:20:45 +0200

    Support running multiple scripts. Use larger output buffer.
    Support multiple script files or strings per command-line
    invocation. Also make corresponding changes to the
    'test-builder' program. (Use SGS_PtrList in main module,
    builder, and renderer for lists. Combine the old behavior
    with running through a batch of things at each stage.)
    
    Use 256 ms of audio output buffering in renderer.
    Fixes stuttering on NetBSD 8 (which requires >= 128 ms
    of buffering for consistently smooth playback).
    The extra 128 ms is for the sake of portability.
    (All non-NetBSD systems I personally use have however
    worked fine with the old 1024 sample buffering.)
    
    While at it, also increase the generator's internal
    buffer length from the ancient value of 256 samples
    to 1024. (This is simply a performance vs. memory
    use trade-off. The number of buffers could potentially
    grow into the hundreds or thousands for extreme input.
    This increases memory use fairly conservatively.)
    
    Const-ify pointers to non-modified SGS_Program instances.
    
    Change log:
     * 2019-06-04: Move multiple script args support back
           to v0.5.0, use the old version number.
       Old branch:
        - 2019-02-10: Support multiple script paths per
              command-line invocation. Also make corresponding
              changes to the 'test-builder' program.
              Move ptrlist to main dir (out of builder/).
              Increase version to v0.6.1.
              Const-ify non-modified SAU_Program pointers.
        - 2019-05-28: Merge support for multiple script args,
              and refactoring. Combine with support
              for -e <string> in the simplest way.
              Set version to v0.5.2. Update README.
        - 2019-05-29: Tweak usage notice.
     * 2019-06-18: Merge audio buffering changes.
           Move it all back to after v0.2.8,
           set version to v0.2.9.
       Old branch:
        - 2019-06-13: Increase generator buffer length
              from 256 samples to 1024 samples.
        - 2019-06-14: Change renderer output buffer
              sizing to remove stuttering on NetBSD.
              (Use 256 ms instead of 1024 samples.)
              Refactor the code in the process.
              Increase version to v0.3.2.
     * 2019-02-09: Merge changes for renderer.c,
           fixing crash on opening several files when
           only some failed to open.
           Simplify usage notice.
       Old branch:
        - 2019-06-22: Minor refactoring in renderer.c.
        - 2020-02-03: Add missing NULL checks in renderer.c;
              check for NULL programs, as is supposed to be done.
     * 2020-07-13: Keep version at v0.2-beta.

--- program.h
+++ program.h
@@ -178,4 +178,4 @@ struct SGS_Script;
 SGS_Program* SGS_build_Program(struct SGS_Script *restrict sd) SGS__malloclike;
 void SGS_discard_Program(SGS_Program *restrict o);
 
-void SGS_Program_print_info(SGS_Program *restrict o);
+void SGS_Program_print_info(const SGS_Program *restrict o);
