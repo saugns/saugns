d2126cccebdca452e7e955902a2229ff5b2a1713
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-06-19 06:07:23 +0200

    Rename language 'SAU', program 'saugns', split out sau/.
    Name the scripting language SAU (Scriptable AUdio),
    split out a 'sau' static library; rename the program from
    'sgensys' to 'saugns' (Scriptable AUdio GeNeration System).
    In files under 'sau/', change the SGS-prefix to SAU.
    Rename all scripts from *.sgs to *.sau.
    
    Update descriptions in README.
    
    Set saugns version to v0.3-dev, keep v0.2-dev in sau.
    
    Change log:
     * 2018-12-05: Rename to 'saugns'. Change SGS prefix to SAG.
     * 2019-01-21: Update for newer code. Change prefix to AGS.
     * 2019-01-22: Bump copyright years. Change prefix to SAU.
           Rename scripts from *.sgs to *.sau, polish README.
     * 2019-07-04: Update for newer code. Tag "v0.3.0".
     * 2019-08-18: Increase version suffix to 'b'.
     * 2019-12-25: Update for newer code.
     * 2020-01-03: Increase version suffix to 'c'.
     * 2020-02-09: Update for newer code. Tag "v0.3.6e-2".
     * 2020-07-26: Update for newer code. Set version to v0.3-dev.
     * 2022-12-31: Update to latest code. Split out 'sau' library.
     * 2024-07-01: Update for last big changes rebase (redesigns).
     * 2025-08-31: Last rebase update (a minor fix, `make tests`).

--- player/audiodev/oss.c
+++ player/audiodev/oss.c
@@ -1,4 +1,4 @@
-/* sgensys: OSS audio output support.
+/* saugns: OSS audio output support.
  * Copyright (c) 2011-2014, 2017-2021 Joel K. Pettersson
  * <joelkpettersson@gmail.com>.
  *
@@ -49,7 +49,7 @@ static inline bool open_oss(SGS_AudioDev *restrict o,
 		goto ERROR;
 	}
 	if (tmp != AFMT_S16_NE) {
-		SGS_error("OSS", "16-bit signed integer native endian format unsupported");
+		SAU_error("OSS", "16-bit signed integer native endian format unsupported");
 		goto ERROR;
 	}
 
@@ -59,7 +59,7 @@ static inline bool open_oss(SGS_AudioDev *restrict o,
 		goto ERROR;
 	}
 	if (tmp != o->channels) {
-		SGS_error("OSS", "%d channels unsupported",
+		SAU_error("OSS", "%d channels unsupported",
 			o->channels);
 		goto ERROR;
 	}
@@ -70,7 +70,7 @@ static inline bool open_oss(SGS_AudioDev *restrict o,
 		goto ERROR;
 	}
 	if ((uint32_t) tmp != o->srate) {
-		SGS_warning("OSS", "sample rate %d unsupported, using %d",
+		SAU_warning("OSS", "sample rate %d unsupported, using %d",
 			o->srate, tmp);
 		o->srate = tmp;
 	}
@@ -81,10 +81,10 @@ static inline bool open_oss(SGS_AudioDev *restrict o,
 	return true;
 ERROR:
 	if (err_name)
-		SGS_error("OSS", "%s: %s", err_name, strerror(errno));
+		SAU_error("OSS", "%s: %s", err_name, strerror(errno));
 	if (fd != -1)
 		close(fd);
-	SGS_error("OSS", "configuration for device \"%s\" failed", dev_name);
+	SAU_error("OSS", "configuration for device \"%s\" failed", dev_name);
 	return false;
 }
 
---
+++ sau/arrtype.c
@@ -0,0 +1,171 @@
+/* SAU library: Generic array module.
+ * Copyright (c) 2018-2024 Joel K. Pettersson
+ * <joelkpettersson@gmail.com>.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <sau/arrtype.h>
+#include <sau/mempool.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Add an item to the given array. Its memory is initialized
+ * to zero bytes only if allocating a new portion of memory.
+ *
+ * The address of the item in the array is returned, and can
+ * be used to initialize it. (If allocation fails, the array
+ * will remain unaltered and NULL be returned.) This address
+ * should however be expected to change with array resizing.
+ *
+ * (Generic version of the function, to be used through wrapper.)
+ *
+ * \return item in array, or NULL if allocation failed
+ */
+void *SAU_ArrType_add(void *restrict _o, size_t item_size) {
+	SAU_ByteArr *restrict o = _o;
+	if (!SAU_ArrType_upsize(o, o->count + 1, item_size))
+		return NULL;
+	size_t offs = o->count * item_size;
+	void *mem = o->a + offs;
+	++o->count;
+	return mem;
+}
+
+/**
+ * Add an item to the given array. It is always initialized,
+ * with a copy of \p item if not NULL, otherwise zero bytes.
+ *
+ * The address of the item in the array is returned, and can
+ * be used to initialize it. (If allocation fails, the array
+ * will remain unaltered and NULL be returned.) This address
+ * should however be expected to change with array resizing.
+ *
+ * (Generic version of the function, to be used through wrapper.)
+ *
+ * \return item in array, or NULL if allocation failed
+ */
+void *SAU_ArrType_push(void *restrict _o,
+		const void *restrict item, size_t item_size) {
+	SAU_ByteArr *restrict o = _o;
+	if (!SAU_ArrType_upsize(o, o->count + 1, item_size))
+		return NULL;
+	size_t offs = o->count * item_size;
+	void *mem = o->a + offs;
+	if (item != NULL)
+		memcpy(mem, item, item_size);
+	else
+		memset(mem, 0, item_size);
+	++o->count;
+	return mem;
+}
+
+/**
+ * Resize the given array if \p count is greater than the current
+ * allocation. Initializes a new part of the array to zero bytes.
+ *
+ * (Generic version of the function, to be used through wrapper.)
+ *
+ * \return true unless allocation failed
+ */
+bool SAU_ArrType_upsize(void *restrict _o,
+		size_t count, size_t item_size) {
+	SAU_ByteArr *restrict o = _o;
+	size_t asize = o->asize;
+	size_t min_asize = count * item_size;
+	if (!o->a || asize < min_asize) {
+		if (!asize) asize = item_size;
+		while (asize < min_asize) asize <<= 1;
+		void *a = realloc(o->a, asize);
+		if (!a)
+			return false;
+		if (!o->a)
+			memset(a, 0, asize);
+		else
+			memset(a + o->asize, 0, asize - o->asize);
+		o->a = a;
+		o->asize = asize;
+	}
+	return true;
+}
+
+/**
+ * Clear the given array.
+ *
+ * (Generic version of the function, to be used through wrapper.)
+ */
+void SAU_ArrType_clear(void *restrict _o) {
+	SAU_ByteArr *restrict o = _o;
+	free(o->a);
+	o->a = NULL;
+	o->count = 0;
+	o->asize = 0;
+}
+
+/**
+ * Memdup function for the contents of the given array.
+ *
+ * \p dst will be set to point to the new allocation
+ * (or to NULL if the array was empty). If the array was
+ * non-empty and allocation failed, \p will remain
+ * unaltered.
+ *
+ * (Generic version of the function, to be used through wrapper.)
+ *
+ * \return true unless allocation failed
+ */
+bool SAU_ArrType_memdup(void *restrict _o,
+		void **restrict dst, size_t item_size) {
+	SAU_ByteArr *restrict o = _o;
+	if (!o->count) {
+		*dst = NULL;
+		return true;
+	}
+	size_t size = o->count * item_size;
+	void *a = malloc(size);
+	if (!a)
+		return false;
+	memcpy(a, o->a, size);
+	*dst = a;
+	return true;
+}
+
+/**
+ * Mempool-using variant of the
+ * memdup function for the contents of the given array.
+ *
+ * \p dst will be set to point to the new allocation
+ * (or to NULL if the array was empty). If the array was
+ * non-empty and allocation failed, \p will remain
+ * unaltered.
+ *
+ * (Generic version of the function, to be used through wrapper.)
+ *
+ * \return true unless allocation failed
+ */
+bool SAU_ArrType_mpmemdup(void *restrict _o,
+		void **restrict dst, size_t item_size,
+		SAU_Mempool *restrict mempool) {
+	SAU_ByteArr *restrict o = _o;
+	if (!o->count) {
+		*dst = NULL;
+		return true;
+	}
+	size_t size = o->count * item_size;
+	void *a = SAU_mpmemdup(mempool, o->a, size);
+	if (!a)
+		return false;
+	*dst = a;
+	return true;
+}
