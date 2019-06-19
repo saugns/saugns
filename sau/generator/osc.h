e2a7dfa30f5264be0eeff3d8cd86c0e50557d30f
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-06-19 06:07:23 +0200

    Rename language 'SAU', program 'saugns', split out sau/.
    Name the scripting language SAU (Scriptable AUdio),
    split out a 'sau' static library; rename the program from
    'sgensys' to 'saugns' (Scriptable AUdio GeNeration System).
    In files under 'sau/', change the SGS-prefix to SAU.
    Rename all scripts from *.sgs to *.sau.
    
    Update descriptions in README.
    
    Set saugns version to v0.3-dev, keep v0.2-dev in sau.
    
    Change log:
     * 2018-12-05: Rename to 'saugns'. Change SGS prefix to SAG.
     * 2019-01-21: Update for newer code. Change prefix to AGS.
     * 2019-01-22: Bump copyright years. Change prefix to SAU.
           Rename scripts from *.sgs to *.sau, polish README.
     * 2019-07-04: Update for newer code. Tag "v0.3.0".
     * 2019-08-18: Increase version suffix to 'b'.
     * 2019-12-25: Update for newer code.
     * 2020-01-03: Increase version suffix to 'c'.
     * 2020-02-09: Update for newer code. Tag "v0.3.6e-2".
     * 2020-07-26: Update for newer code. Set version to v0.3-dev.
     * 2022-12-31: Update to latest code. Split out 'sau' library.
     * 2023-12-14: Rebase down refactoring, minor fixes, and more.
     * 2024-07-01: Rebase down some further redesign work.

---
+++ sau/generator/osc.h
@@ -0,0 +1,242 @@
+/* SAU library: Oscillator implementation.
+ * Copyright (c) 2011, 2017-2022 Joel K. Pettersson
+ * <joelkpettersson@gmail.com>.
+ *
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version, WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * View the file COPYING for details, or if missing, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include <sau/wave.h>
+#include <sau/math.h>
+
+/*
+ * Use pre-integrated LUTs ("PILUTs")?
+ *
+ * Turn off to use the raw naive LUTs,
+ * kept for testing/"viewing" of them.
+ */
+#define USE_PILUT 1
+
+/**
+ * Calculate the coefficent, based on the sample rate, used for
+ * the per-sample phase by multiplying with the frequency used.
+ */
+#define SAU_Phasor_COEFF(srate) (((float) UINT32_MAX)/(srate))
+
+typedef struct SAU_Phasor {
+	uint32_t phase;
+	float coeff;
+} SAU_Phasor;
+
+#define SAU_OSC_RESET_DIFF  (1<<0)
+#define SAU_OSC_RESET       ((1<<1) - 1)
+
+typedef struct SAU_Osc {
+	SAU_Phasor phasor;
+	uint8_t wave;
+	uint8_t flags;
+#if USE_PILUT
+	uint32_t prev_phase;
+	double prev_Is;
+	float prev_diff_s;
+#endif
+} SAU_Osc;
+
+/**
+ * Initialize instance for use.
+ */
+static inline void SAU_init_Osc(SAU_Osc *restrict o, uint32_t srate) {
+	*o = (SAU_Osc){
+#if USE_PILUT
+		.phasor = (SAU_Phasor){
+			.phase = SAU_Wave_picoeffs[SAU_WAVE_N_sin].phase_adj,
+			.coeff = SAU_Phasor_COEFF(srate),
+		},
+#else
+		.phasor = (SAU_Phasor){
+			.phase = 0,
+			.coeff = SAU_Phasor_COEFF(srate),
+		},
+#endif
+		.wave = SAU_WAVE_N_sin,
+		.flags = SAU_OSC_RESET,
+	};
+}
+
+static inline void SAU_Osc_set_phase(SAU_Osc *restrict o, uint32_t phase) {
+#if USE_PILUT
+	o->phasor.phase = phase + SAU_Wave_picoeffs[o->wave].phase_adj;
+#else
+	o->phasor.phase = phase;
+#endif
+}
+
+static inline void SAU_Osc_set_wave(SAU_Osc *restrict o, uint8_t wave) {
+#if USE_PILUT
+	uint32_t old_offset = SAU_Wave_picoeffs[o->wave].phase_adj;
+	uint32_t offset = SAU_Wave_picoeffs[wave].phase_adj;
+	o->phasor.phase += offset - old_offset;
+	o->wave = wave;
+	o->flags |= SAU_OSC_RESET_DIFF;
+#else
+	o->wave = wave;
+#endif
+}
+
+/**
+ * Calculate length of wave cycle for \p freq.
+ *
+ * \return number of samples
+ */
+static inline uint32_t SAU_Osc_cycle_len(SAU_Osc *restrict o, float freq) {
+	return SAU_ftoi(((float) UINT32_MAX) / (o->phasor.coeff * freq));
+}
+
+/**
+ * Calculate position in wave cycle for \p freq, based on \p pos.
+ *
+ * \return number of samples
+ */
+static inline uint32_t SAU_Osc_cycle_pos(SAU_Osc *restrict o,
+		float freq, uint32_t pos) {
+	uint32_t inc = SAU_ftoi(o->phasor.coeff * freq);
+	uint32_t phs = inc * pos;
+	return phs / inc;
+}
+
+/**
+ * Calculate offset relative to wave cycle for \p freq, based on \p pos.
+ *
+ * Can be used to reduce time length to something rounder and reduce clicks.
+ */
+static inline int32_t SAU_Osc_cycle_offs(SAU_Osc *restrict o,
+		float freq, uint32_t pos) {
+	uint32_t inc = SAU_ftoi(o->phasor.coeff * freq);
+	uint32_t phs = inc * pos;
+	return (phs - SAU_Wave_SLEN) / inc;
+}
+
+#if !USE_PILUT
+# define P(inc, ofs) ofs + o->phase; (o->phase += inc)     /* post-increment */
+#else
+# define P(inc, ofs) ofs + (o->phase += inc)               /* pre-increment */
+#endif
+
+/**
+ * Fill phase-value buffer for use with SAU_Osc_run().
+ */
+static sauMaybeUnused void SAU_Phasor_fill(SAU_Phasor *restrict o,
+		uint32_t *restrict phase_ui32,
+		size_t buf_len,
+		const float *restrict freq_f,
+		const float *restrict pm_f,
+		const float *restrict fpm_f) {
+	const float fpm_scale = 1.f / SAU_HUMMID;
+	if (!pm_f && !fpm_f) {
+		for (size_t i = 0; i < buf_len; ++i) {
+			float s_f = freq_f[i];
+			phase_ui32[i] = P(SAU_ftoi(o->coeff * s_f), 0);
+		}
+	} else if (!fpm_f) {
+		for (size_t i = 0; i < buf_len; ++i) {
+			float s_f = freq_f[i];
+			float s_pofs = pm_f[i];
+			phase_ui32[i] = P(SAU_ftoi(o->coeff * s_f),
+					SAU_ftoi(s_pofs * (float)INT32_MAX));
+		}
+	} else if (!pm_f) {
+		for (size_t i = 0; i < buf_len; ++i) {
+			float s_f = freq_f[i];
+			float s_pofs = fpm_f[i] * fpm_scale * s_f;
+			phase_ui32[i] = P(SAU_ftoi(o->coeff * s_f),
+					SAU_ftoi(s_pofs * (float)INT32_MAX));
+		}
+	} else {
+		for (size_t i = 0; i < buf_len; ++i) {
+			float s_f = freq_f[i];
+			float s_pofs = pm_f[i] + (fpm_f[i] * fpm_scale * s_f);
+			phase_ui32[i] = P(SAU_ftoi(o->coeff * s_f),
+					SAU_ftoi(s_pofs * (float)INT32_MAX));
+		}
+	}
+}
+
+#undef P /* done */
+
+#if !USE_PILUT
+/*
+ * Implementation of SAU_Osc_run()
+ * using naive LUTs with linear interpolation.
+ *
+ * Uses post-incremented phase each sample.
+ */
+static void SAU_Osc_naive_run(SAU_Osc *restrict o,
+		float *restrict buf, size_t buf_len,
+		const uint32_t *restrict phase_buf) {
+	const float *const lut = SAU_Wave_luts[o->wave];
+	for (size_t i = 0; i < buf_len; ++i) {
+		buf[i] = SAU_Wave_get_lerp(lut, phase_buf[i]);
+	}
+}
+#endif
+
+#if USE_PILUT
+/* Set up for differentiation (re)start with usable state. */
+static void SAU_Osc_reset(SAU_Osc *restrict o, uint32_t phase) {
+	const float *const lut = SAU_Wave_piluts[o->wave];
+	const float diff_scale = SAU_Wave_DVSCALE(o->wave);
+	const float diff_offset = SAU_Wave_DVOFFSET(o->wave);
+	if (o->flags & SAU_OSC_RESET_DIFF) {
+		/* one-LUT-value diff works fine for any freq, 0 Hz included */
+		int32_t phase_diff = SAU_Wave_SLEN;
+		o->prev_Is = SAU_Wave_get_herp(lut, phase - phase_diff);
+		double Is = SAU_Wave_get_herp(lut, phase);
+		double x = (diff_scale / phase_diff);
+		o->prev_diff_s = (Is - o->prev_Is) * x + diff_offset;
+		o->prev_Is = Is;
+		o->prev_phase = phase;
+	}
+	o->flags &= ~SAU_OSC_RESET;
+}
+#endif
+
+/**
+ * Run for \p buf_len samples, generating output.
+ *
+ * Uses pre-incremented phase each sample.
+ */
+static sauMaybeUnused void SAU_Osc_run(SAU_Osc *restrict o,
+		float *restrict buf, size_t buf_len,
+		const uint32_t *restrict phase_buf) {
+#if USE_PILUT /* higher-quality audio */
+	const float *const lut = SAU_Wave_piluts[o->wave];
+	const float diff_scale = SAU_Wave_DVSCALE(o->wave);
+	const float diff_offset = SAU_Wave_DVOFFSET(o->wave);
+	if (buf_len > 0 && o->flags & SAU_OSC_RESET)
+		SAU_Osc_reset(o, phase_buf[0]);
+	for (size_t i = 0; i < buf_len; ++i) {
+		float s;
+		uint32_t phase = phase_buf[i];
+		int32_t phase_diff = phase - o->prev_phase;
+		if (phase_diff == 0) {
+			s = o->prev_diff_s;
+		} else {
+			double Is = SAU_Wave_get_herp(lut, phase);
+			double x = (diff_scale / phase_diff);
+			s = (Is - o->prev_Is) * x + diff_offset;
+			o->prev_Is = Is;
+			o->prev_diff_s = s;
+			o->prev_phase = phase;
+		}
+		buf[i] = s;
+	}
+#else /* test naive LUT */
+	SAU_Osc_naive_run(o, buf, buf_len, phase_buf);
+#endif
+}
---
+++ sau/scanner.h
@@ -0,0 +1,217 @@
+/* SAU library: Script scanner module.
+ * Copyright (c) 2014, 2017-2022 Joel K. Pettersson
+ * <joelkpettersson@gmail.com>.
+ *
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version, WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * View the file COPYING for details, or if missing, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "file.h"
+#include "symtab.h"
+
+struct SAU_Scanner;
+typedef struct SAU_Scanner SAU_Scanner;
+
+/**
+ * Number of values for which character filters are defined.
+ *
+ * Values below this are given their own function pointer;
+ * SAU_Scanner_get_filter() handles mapping of other values.
+ */
+#define SAU_SCAN_FILTER_COUNT 128
+
+/**
+ * Number of old scan positions which can be returned to.
+ */
+#define SAU_SCAN_UNGET_MAX 63
+
+/**
+ * Function type used for filtered character getting.
+ * Each Scanner instance uses a table of these.
+ *
+ * The function takes the raw character value, processes it and
+ * may read further (updating the current scan frame) before
+ * returning the character to use. May instead return 0 to
+ * skip the character and prompt another read (and possibly a
+ * corresponding filter call).
+ *
+ * NULL can be used as a function value, meaning that the character
+ * should be used without filtering.
+ *
+ * Filter functions may call other filter functions,
+ * and are allowed (not done by default functions) to alter the table.
+ */
+typedef uint8_t (*SAU_ScanFilter_f)(SAU_Scanner *o, uint8_t c);
+
+/**
+ * Special character values.
+ */
+enum {
+	/**
+	 * Returned for spaces and tabs after filtering.
+	 * Also used for comparison with SAU_Scanner_tryc().
+	 */
+	SAU_SCAN_SPACE = ' ',
+	/**
+	 * Returned for linebreaks after filtering.
+	 * Also used for comparison with SAU_Scanner_tryc()
+	 * and SAU_Scanner_tryc_nospace().
+	 */
+	SAU_SCAN_LNBRK = '\n',
+	/**
+	 * Used internally. Returned by character filter
+	 * to indicate that EOF is reached, error-checking done,
+	 * and scanning complete for the file.
+	 */
+	SAU_SCAN_EOF = 0xFF,
+};
+
+/**
+ * Flags set by character filters.
+ */
+enum {
+	SAU_SCAN_C_ERROR = 1<<0, // character-level error encountered in script
+	SAU_SCAN_C_SPACE = 1<<1, // space scanned during the get
+	SAU_SCAN_C_LNBRK = 1<<2, // linebreak scanned during the get
+	SAU_SCAN_C_LNBRK_POSUP = 1<<3, // pending position update for linebreak
+};
+
+extern const SAU_ScanFilter_f SAU_Scanner_def_filters[SAU_SCAN_FILTER_COUNT];
+
+/**
+ * Whitespace filtering levels, used with
+ * SAU_Scanner_setws_level() to assign standard filter functions.
+ *
+ * Default is SAU_SCAN_WS_ALL. Note that other filter functions,
+ * e.g. comment filters, must filter the whitespace markers they
+ * return using whichever filter functions are set, in order to
+ * avoid excess marker characters in the output.
+ */
+enum {
+	SAU_SCAN_WS_ALL = 0, // include all ws characters mapped to SAU_SCAN_*
+	SAU_SCAN_WS_NONE,    // remove all ws characters
+};
+
+uint8_t SAU_Scanner_filter_invalid(SAU_Scanner *restrict o, uint8_t c);
+uint8_t SAU_Scanner_filter_space_keep(SAU_Scanner *restrict o, uint8_t c);
+uint8_t SAU_Scanner_filter_linebreak_keep(SAU_Scanner *restrict o, uint8_t c);
+uint8_t SAU_Scanner_filter_ws_none(SAU_Scanner *restrict o, uint8_t c);
+uint8_t SAU_Scanner_filter_linecomment(SAU_Scanner *restrict o, uint8_t c);
+uint8_t SAU_Scanner_filter_blockcomment(SAU_Scanner *restrict o,
+		uint8_t check_c);
+uint8_t SAU_Scanner_filter_slashcomments(SAU_Scanner *restrict o, uint8_t c);
+uint8_t SAU_Scanner_filter_char1comments(SAU_Scanner *restrict o, uint8_t c);
+
+uint8_t SAU_Scanner_setws_level(SAU_Scanner *restrict o, uint8_t ws_level);
+
+/**
+ * Scanner state flags for current file.
+ */
+enum {
+	SAU_SCAN_S_ERROR = 1<<0, // true if at least one error has been printed
+	SAU_SCAN_S_DISCARD = 1<<1, // don't save scan frame next get
+	SAU_SCAN_S_QUIET = 1<<2, // suppress warnings (but still print errors)
+};
+
+/**
+ * Scan frame with character-level information for a get.
+ */
+typedef struct SAU_ScanFrame {
+	int32_t line_num;
+	int32_t char_num;
+	uint8_t c;
+	uint8_t c_flags;
+} SAU_ScanFrame;
+
+/**
+ * Scanner type.
+ */
+struct SAU_Scanner {
+	SAU_File *f;
+	SAU_Symtab *symtab;
+	SAU_ScanFilter_f *filters; // copy of SAU_Scanner_def_filters
+	SAU_ScanFrame sf;
+	uint8_t undo_pos;
+	uint8_t undo_ungets;
+	uint8_t s_flags;
+	uint8_t match_c; // for use by character filters
+	uint8_t ws_level; // level of SAU_Scanner_setws_level(), presuming use
+	uint8_t *strbuf;
+	void *data; // for use by user
+	SAU_ScanFrame undo[SAU_SCAN_UNGET_MAX + 1];
+};
+
+SAU_Scanner *SAU_create_Scanner(SAU_Symtab *restrict symtab) sauMalloclike;
+void SAU_destroy_Scanner(SAU_Scanner *restrict o);
+
+bool SAU_Scanner_open(SAU_Scanner *restrict o,
+		const char *restrict script, bool is_path);
+void SAU_Scanner_close(SAU_Scanner *restrict o);
+
+/**
+ * Get character filter to call for character \p c,
+ * or NULL if the character is simply to be accepted.
+ *
+ * Values below SAU_SCAN_FILTER_COUNT are assigned the filter for
+ * the raw value; other values are assigned the filter for '\0'.
+ *
+ * \return SAU_ScanFilter_f or NULL
+ */
+static inline SAU_ScanFilter_f SAU_Scanner_getfilter(SAU_Scanner *restrict o,
+		uint8_t c) {
+	if (c >= SAU_SCAN_FILTER_COUNT) c = 0;
+	return o->filters[c];
+}
+
+/**
+ * Call character filter for character \p c, unless a blank entry.
+ * If calling, will set \a match_c for use by the filter function.
+ *
+ * \return result
+ */
+static inline uint8_t SAU_Scanner_usefilter(SAU_Scanner *restrict o,
+		uint8_t c, uint8_t match_c) {
+	SAU_ScanFilter_f f = SAU_Scanner_getfilter(o, c);
+	if (f != NULL) {
+		o->match_c = match_c;
+		return f(o, c);
+	}
+	return c;
+}
+
+/**
+ * Callback type allowing reading of named constants using SAU_Scanner_getd().
+ * Should return non-zero length if number read and \p var set.
+ */
+typedef size_t (*SAU_ScanNumConst_f)(SAU_Scanner *restrict o,
+		double *restrict var);
+
+uint8_t SAU_Scanner_retc(SAU_Scanner *restrict o);
+uint8_t SAU_Scanner_getc(SAU_Scanner *restrict o);
+uint8_t SAU_Scanner_filterc(SAU_Scanner *restrict o, uint8_t c,
+		SAU_ScanFilter_f filter_f);
+bool SAU_Scanner_tryc(SAU_Scanner *restrict o, uint8_t testc);
+uint32_t SAU_Scanner_ungetc(SAU_Scanner *restrict o);
+bool SAU_Scanner_geti(SAU_Scanner *restrict o,
+		int32_t *restrict var, bool allow_sign,
+		size_t *restrict str_len);
+bool SAU_Scanner_getd(SAU_Scanner *restrict o,
+		double *restrict var, bool allow_sign,
+		size_t *restrict str_len,
+		SAU_ScanNumConst_f numconst_f);
+bool SAU_Scanner_get_symstr(SAU_Scanner *restrict o,
+		SAU_Symstr **restrict symstrp);
+uint8_t SAU_Scanner_skipws(SAU_Scanner *restrict o);
+
+void SAU_Scanner_warning(const SAU_Scanner *restrict o,
+		const SAU_ScanFrame *restrict sf,
+		const char *restrict fmt, ...) sauPrintflike(3, 4);
+void SAU_Scanner_error(SAU_Scanner *restrict o,
+		const SAU_ScanFrame *restrict sf,
+		const char *restrict fmt, ...) sauPrintflike(3, 4);
