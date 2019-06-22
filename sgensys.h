f5bc2c9c32d07457fa2eb69785bafd169ee580f0
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-06-22 04:07:03 +0200

    Basic ramp curve changes. Change 'r' syntax. Clean-up.
    Ramp curve changes:
     * Rename the old 'exp' and 'log'
       to 'esd' (exponentially saturate or decay) and
       to 'lsd' (logarithmically saturate or decay),
       respectively. (The 'esd' type is the most
       natural-sounding for envelope-like use.)
     * Add new 'exp' and 'log' which internally
       select one of 'esd' or 'lsd' depending on whether
       the curve rises or falls, in order to behave much
       more like people would expect
       from curves named 'exp' and 'log'.
    
    Syntax change: Disable multiplicative inverse for 'r',
    change description from "frequency ratio" to
    "relative frequency". It's more intuitive to handle
    n:m carrier-modulator frequency ratios as (m/n) values
    than (n/m) values, when actually adjusting them.
    (With multiplicative inverse, a higher value meant a
    lower frequency; now, a higher value means a higher
    frequency.) This reverses a change from 2011-03-04.
    
    Update scripts and README for changes,
    add "engine_rumble.sgs"
    and "hearingrange.sgs" (sweep from 20 Hz to 20 kHz), and
    move devtests/drum.sgs to examples/unnamed3.sgs.
    
    Clean-up changes:
     * Remove parser SCOPE_SAME (unused since 2012).
     * Small flag handling change for parser end of event.
     * Split the generator's mixing code
       into a mixer module (without functional change).
     * Further refactoring of the file module, part 1.
     * Use DJB2 hash (with length addition) in symtab.
       A little better than the old DIY hash.
       No significant functional change.
    
    Increase version to v0.2.12.
    
    Reindent generator.c.
    
    Change log:
     * 2019-05-28: Reindent generator.c.
     * 2019-06-16: Merge back refactoring,
           keep old buf size in generator.
       Old branch:
        - 2019-06-13: Split out mixer module from generator.c.
     * 2019-06-18: Update generator buf size to
           match new older version.
     * 2019-06-30: Merge down slope->ramp and curve changes,
           along with script updates and a symtab hash change.
           Set version to v0.2.12.
       Old branch:
        - 2019-06-22: Begin v0.3.1-beta.
        - 2019-06-27: Rename 'exp'/'log' slope types
              to 'esd'/'lsd', add new 'exp'/'log' types which
              select 'esd' or 'lsd' for increase/decrease.
              Update scripts for changes, add "engine_rumble.sau",
              move devtests/drum.sau to examples/unnamed3.sau, and
              add "freqrange.sau" (sweep from 20 Hz to 20 kHz).
        - 2019-06-28: Change "slope change type" to "curve type"
              in script naming and parser, for consistency.
              Use DJB2 hash (with length addition) in symtab.
              Set version to v0.3.1. Update comments in slope.c.
              Rename "freqrange.sau" to "hearingrange.sau".
        - 2019-06-30: Rename slope module to ramp.
     * 2019-07-04: Merge down 'r' syntax change.
       Old branch:
        - 2019-07-04: v0.3.1-beta: Begin syntax tweaks.
              Disable multiplicative inverse for 'r'.
              Update scripts and README.
     * 2019-07-31: README typo fix & ramp.c comment fix.
     * 2019-08-03: Fix description of 'r' syntax wrt freq ratio.
     * 2019-08-07: Merge Makefile -ffast-math portability fix.
     * 2019-09-12: Move down clean-up change from v0.3.2.
       Old branch:
        - 2019-08-22: Break out SAU_File_end() function.
     * 2019-12-12: Fix indentation in generator.c.
     * 2019-12-25: Merge ramp default time changes.
     * 2020-01-03: Merge clean-up changes.
     * 2020-01-04: Move down clean-ups:
       Old branch:
        - 2019-07-11: (From v0.3.1) Microflensing.
              parser: Remove unused mul_inv option from scan_num(),
              use a SAU__noinline to help clang keep size down.
        - 2019-12-14: parser: Remove SCOPE_SAME (unused since 2012).

--- sgensys.h
+++ sgensys.h
@@ -15,7 +15,7 @@
 #include "program.h"
 #include "ptrlist.h"
 
-#define SGS_VERSION_STR "sgensys v0.2.11"
+#define SGS_VERSION_STR "sgensys v0.2.12"
 
 size_t SGS_build(const SGS_PtrList *restrict script_args, bool are_paths,
 		SGS_PtrList *restrict prg_objs);
