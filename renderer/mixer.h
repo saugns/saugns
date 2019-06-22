1b5c08339506603ffee985327ff542df0e4c20e2
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-06-22 04:07:03 +0200

    Basic ramp curve changes. Refactor more. Add scripts.
    Ramp curve changes:
     * Rename the old 'exp' and 'log'
       to 'xpe' (exponential envelope) and
       to 'lge' (logarithmic envelope),
       respectively. (The 'xpe' type is the most
       natural-sounding for envelope-like uses.)
     * Add new 'exp' and 'log' which internally
       select one of 'xpe' or 'lge' depending on whether
       the curve rises or falls, in order to behave much
       more like people would expect
       from curves named 'exp' and 'log'.
     * Add 'cos' curve, which rises or falls like a cosine
       moves from bottom to top or top to bottom. (Rougher
       approximation than would be used for an oscillator,
       but 99% accuracy seems good enough for ramp curve.)
    
    Update scripts and README for changes,
    add "engine_rumble.sgs"
    and "hearingrange.sgs" (sweep from 20 Hz to 20 kHz), and
    move devtests/drum.sgs to examples/unnamed3.sgs.
    
    Refactoring changes:
     * Tidy up parser scan_ramp() usage.
       Add and use parameter flag masks.
     * Split the generator's mixing code
       into a mixer module (without functional change).
     * Further refactoring of the file module, part 2.
    
    Reindent generator.c.
    
    Change log:
     * 2019-05-28: Reindent generator.c.
     * 2019-06-16: Merge back refactoring,
           keep old buf size in generator.
       Old branch:
        - 2019-06-13: Split out mixer module from generator.c.
     * 2019-06-18: Update generator buf size to
           match new older version.
     * 2019-06-30: Merge down slope->ramp and curve changes,
           along with script updates.
           Set version to v0.2.12.
       Old branch:
        - 2019-06-22: Begin v0.3.1-beta.
        - 2019-06-27: Rename 'exp'/'log' slope types
              to 'esd'/'lsd', add new 'exp'/'log' types which
              select 'esd' or 'lsd' for increase/decrease.
              Update scripts for changes, add "engine_rumble.sau",
              move devtests/drum.sau to examples/unnamed3.sau, and
              add "freqrange.sau" (sweep from 20 Hz to 20 kHz).
        - 2019-06-28: Change "slope change type" to "curve type"
              in script naming and parser, for consistency.
              Set version to v0.3.1. Update comments in slope.c.
              Rename "freqrange.sau" to "hearingrange.sau".
        - 2019-06-30: Rename slope module to ramp.
     * 2019-08-07: Merge Makefile -ffast-math portability fix.
     * 2019-12-12: Fix indentation in generator.c.
     * 2019-12-25: Merge ramp default time changes.
     * 2020-01-03: Merge clean-up changes.
     * 2020-07-23: Merge ramp & generator changes.
     * 2020-07-26: Keep version at v0.2-beta.
     * 2020-08-21: Split out this commit from 'r' syntax
           changes and more (which used to be above).
     * 2021-06-27: Rename ramp 'esd' to 'xpe', 'lsd' to 'lge'.
     * 2021-09-07: Add 'cos' ramp curve.
     * 2022-01-10: Move down file module changes.
       Old branch:
        - 2019-09-12: Merge down 08-24 file module refactoring,
              with finishing touch.
        - 2020-01-03: Merge -pedantic change for file.c.
        - 2020-07-23: Change file module to ISC license.
     * 2022-02-03: Move down related refactoring.
       Old branch:
        - 2020-08-25: Add, use parameter flag masks.
        - 2020-09-18: Rebase, tweak previous change.
        - 2020-09-19: Refactor parser scan_ramp() to
              handle both multi-value and state cases.

---
+++ renderer/mixer.h
@@ -0,0 +1,48 @@
+/* sgensys: Audio mixer module.
+ * Copyright (c) 2019 Joel K. Pettersson
+ * <joelkpettersson@gmail.com>.
+ *
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version, WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * View the file COPYING for details, or if missing, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "../ramp.h"
+
+#define SGS_MIX_BUFLEN 1024
+
+typedef struct SGS_Mixer {
+	float *mix_l, *mix_r;
+	float *pan_buf;
+	uint32_t srate;
+	float scale;
+} SGS_Mixer;
+
+SGS_Mixer *SGS_create_Mixer(void) sgsMalloclike;
+void SGS_destroy_Mixer(SGS_Mixer *restrict o);
+
+/**
+ * Set sample rate used for panning.
+ */
+static inline void SGS_Mixer_set_srate(SGS_Mixer *restrict o, uint32_t srate) {
+	o->srate = srate;
+}
+
+/**
+ * Set amplitude scaling.
+ */
+static inline void SGS_Mixer_set_scale(SGS_Mixer *restrict o, float scale) {
+	o->scale = scale;
+}
+
+void SGS_Mixer_clear(SGS_Mixer *restrict o);
+void SGS_Mixer_add(SGS_Mixer *restrict o,
+		float *restrict buf, size_t len,
+		SGS_Ramp *restrict pan, uint32_t *restrict pan_pos);
+void SGS_Mixer_write(SGS_Mixer *restrict o,
+		int16_t **restrict spp, size_t len);
