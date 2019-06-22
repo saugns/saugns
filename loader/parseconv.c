6887dd8367eb70fc850398359d66c83d3416ff37
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-06-22 04:07:03 +0200

    Basic ramp curve changes. Refactor more.
    Ramp curve changes:
     * Rename the old 'exp' and 'log'
       to 'xpe' (exponential envelope) and
       to 'lge' (logarithmic envelope),
       respectively. (The 'xpe' type is the most
       natural-sounding for envelope-like uses.)
     * Add new 'exp' and 'log' which internally
       select one of 'xpe' or 'lge' depending on whether
       the curve rises or falls, in order to behave much
       more like people would expect
       from curves named 'exp' and 'log'.
     * Add 'cos' curve, which rises or falls like a cosine
       moves from bottom to top or top to bottom. (Rougher
       approximation than would be used for an oscillator,
       but 99% accuracy seems good enough for ramp curve.)
    
    Update scripts and README for changes,
    add "engine_rumble.sgs"
    and "hearingrange.sgs" (sweep from 20 Hz to 20 kHz), and
    move devtests/drum.sgs to examples/unnamed3.sgs.
    
    Refactoring changes:
     * Further refactoring of the file module, part 1.
     * Split the generator's mixing code
       into a mixer module (without functional change).
     * Use DJB2 hash (with length addition) in symtab.
       A little better than the old DIY hash.
       No significant functional change.
    
    Reindent generator.c.
    
    Change log:
     * 2019-05-28: Reindent generator.c.
     * 2019-06-16: Merge back refactoring,
           keep old buf size in generator.
       Old branch:
        - 2019-06-13: Split out mixer module from generator.c.
     * 2019-06-18: Update generator buf size to
           match new older version.
     * 2019-06-30: Merge down slope->ramp and curve changes,
           along with script updates and a symtab hash change.
           Set version to v0.2.12.
       Old branch:
        - 2019-06-22: Begin v0.3.1-beta.
        - 2019-06-27: Rename 'exp'/'log' slope types
              to 'esd'/'lsd', add new 'exp'/'log' types which
              select 'esd' or 'lsd' for increase/decrease.
              Update scripts for changes, add "engine_rumble.sau",
              move devtests/drum.sau to examples/unnamed3.sau, and
              add "freqrange.sau" (sweep from 20 Hz to 20 kHz).
        - 2019-06-28: Change "slope change type" to "curve type"
              in script naming and parser, for consistency.
              Use DJB2 hash (with length addition) in symtab.
              Set version to v0.3.1. Update comments in slope.c.
              Rename "freqrange.sau" to "hearingrange.sau".
        - 2019-06-30: Rename slope module to ramp.
     * 2019-08-07: Merge Makefile -ffast-math portability fix.
     * 2019-09-12: Move down clean-up change from v0.3.2.
       Old branch:
        - 2019-08-22: Break out SAU_File_end() function.
     * 2019-12-12: Fix indentation in generator.c.
     * 2019-12-25: Merge ramp default time changes.
     * 2020-01-03: Merge clean-up changes.
     * 2020-07-23: Merge ramp & generator changes.
     * 2020-07-26: Keep version at v0.2-beta.
     * 2020-08-21: Split out the above from
           'r' syntax changes and more.
     * 2021-06-27: Rename ramp 'esd' to 'xpe', 'lsd' to 'lge'.
     * 2021-09-07: Add 'cos' ramp curve.

--- loader/parseconv.c
+++ loader/parseconv.c
@@ -27,14 +27,15 @@ static SGS_ProgramOpGraph
 *create_OpGraph(const SGS_ScriptEvData *restrict vo_in) {
 	uint32_t size;
 	size = vo_in->op_graph.count;
-	if (!size) return NULL;
-
+	if (!size)
+		return NULL;
 	const SGS_ScriptOpData **ops;
 	uint32_t i;
 	ops = (const SGS_ScriptOpData**) SGS_PtrList_ITEMS(&vo_in->op_graph);
 	SGS_ProgramOpGraph *o;
 	o = malloc(sizeof(SGS_ProgramOpGraph) + sizeof(uint32_t) * (size - 1));
-	if (!o) return NULL;
+	if (!o)
+		return NULL;
 	o->opc = size;
 	for (i = 0; i < size; ++i) {
 		o->ops[i] = ops[i]->op_id;
@@ -48,14 +49,15 @@ static SGS_ProgramOpAdjcs
 	size = op_in->fmods.count +
 		op_in->pmods.count +
 		op_in->amods.count;
-	if (!size) return NULL;
-
+	if (!size)
+		return NULL;
 	const SGS_ScriptOpData **ops;
 	uint32_t i;
 	uint32_t *data;
 	SGS_ProgramOpAdjcs *o;
 	o = malloc(sizeof(SGS_ProgramOpAdjcs) + sizeof(uint32_t) * (size - 1));
-	if (!o) return NULL;
+	if (!o)
+		return NULL;
 	o->fmodc = op_in->fmods.count;
 	o->pmodc = op_in->pmods.count;
 	o->amodc = op_in->amods.count;
@@ -376,7 +378,8 @@ static void ParseConv_traverse_voice(ParseConv *restrict o,
 	SGS_ProgramVoData *vd = (SGS_ProgramVoData*) ev->vo_data;
 	const SGS_ProgramOpGraph *graph = vas->op_graph;
 	uint32_t i;
-	if (!graph) return;
+	if (!graph)
+		return;
 	for (i = 0; i < graph->opc; ++i) {
 		op_ref.id = graph->ops[i];
 //		fprintf(stderr, "visit node %u\n", op_ref.id);
@@ -483,7 +486,6 @@ static SGS_Program *_ParseConv_copy_out(ParseConv *restrict o,
 	prg->duration_ms = o->duration_ms;
 	prg->name = parse->name;
 	return prg;
-
 ERROR:
 	if (events != NULL) free(events);
 	if (prg != NULL) free(prg);
@@ -564,6 +566,8 @@ static void Program_destroy_event_data(SGS_ProgramEvent *restrict e) {
  * Destroy instance.
  */
 void SGS_discard_Program(SGS_Program *restrict o) {
+	if (!o)
+		return;
 	if (o->events != NULL) {
 		for (size_t i = 0; i < o->ev_count; ++i) {
 			SGS_ProgramEvent *e = &o->events[i];
@@ -578,7 +582,8 @@ static void print_linked(const char *restrict header,
 		const char *restrict footer,
 		uint32_t count, const uint32_t *restrict nodes) {
 	uint32_t i;
-	if (!count) return;
+	if (!count)
+		return;
 	fprintf(stdout, "%s%u", header, nodes[0]);
 	for (i = 0; ++i < count; )
 		fprintf(stdout, ", %u", nodes[i]);
