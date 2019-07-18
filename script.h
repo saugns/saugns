7e0d32dcfc082b30cbdc2520c7965779599249f3
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-07-18 08:26:17 +0200

    Change AM/FM/PM syntax & handling.
    Syntax changes:
     * For AM and FM parameters, parse the second value
       after a ',' and the modulator list after an
       appended '~', rather than second value and/or
       modulator list after a leading '!'.
       (E.g., for FM, "fX,Y~[...]" instead of "fX f!Y[...]",
       where X and Y are numbers or numerical expressions.
       Any of X, Y, or [...] can be left out now as before.)
     * Allow value ramp values for the second frequency and
       amplitude. Make the syntax consistent in this regard.
     * Make the phase & PM syntax slightly more permissive.
       For consistency with the new AM & FM syntax, have 'p'
       allow phase followed by PM modulator list in one go.
    
    Add position-updating skip function for ramp,
    similar to run function except there's no output.
    Use to update second freq and amp ramps in generator
    without generating samples when no FM or AM is done.
    
    Also:
     * Remove parser SCOPE_SAME (unused since 2012).
     * Small flag handling change for parser end of event.
     * Further refactoring of the file module, part 2.
    
    Increase version to v0.2.13.
    
    Update scripts and README.
    
    This resolved the "todo" regarding dynfreq/dynamp
    ratio flags.
    
    Change log:
     * 2019-08-04: Move syntax changes and parser fixes
           down from v0.3.1:
       Old branch:
        - 2019-07-11: Microflensing.
              parser: Remove unused mul_inv option from scan_num(),
              use a SAU__noinline to help clang keep size down.
        - 2019-07-18: Change AM & FM syntax.
              Make 'p' more permissive for consistency.
        - 2019-07-20: Use ramps for second freq, amp.
              TODO: Fill in the missing pieces in
              generator for fully consistent handling.
        - 2019-07-21: Simplify addition in generator.
              Remove incomplete and unneeded extra code.
              Clarify remaining added TODOs.
        - 2019-07-30: Add SAU_Ramp_skip(), use in generator.
              Remove now-resolved new TODOs.
     * 2019-08-06: parser: Add missing lines to backport
           (matching commits for later versions),
           v0.2.13 now functions as expected.
     * 2019-08-07: Move 08-06 Makefile -ffast-math portability fix
           to v0.2.3 and v0.2.4.
     * 2019-09-12: Merge down 08-24 file module refactoring,
           with finishing touch.
     * 2019-12-21: Merge 'S a' multiplier apply-to-ramp fix.
     * 2019-12-22: Fix missing default time adjustment
           for amp2 and freq2 ramps. Adjust details to
           avoid inf. time.
     * 2019-12-25: Merge clean-up and ramp changes as of 12-25.
       Old branch:
        - 2019-12-14: parser: Remove SCOPE_SAME (unused since 2012).
        - 2019-12-24: Move down clean-up change from v0.3.3b.
              Merge ramp default time changes.

--- script.h
+++ script.h
@@ -42,8 +42,9 @@ typedef struct SGS_ScriptOpData {
 	uint32_t op_params;
 	uint32_t time_ms, silence_ms;
 	uint8_t wave;
-	SGS_Ramp freq, amp;
-	float phase, dynfreq, dynamp;
+	SGS_Ramp freq, freq2;
+	SGS_Ramp amp, amp2;
+	float phase;
 	/* node adjacents in operator linkage graph */
 	SGS_PtrList fmods, pmods, amods;
 } SGS_ScriptOpData;
