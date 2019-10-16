6be806fba1fead614e9adc535c8bd9f36bae1af3
Joel K. Pettersson <joelkpettersson@gmail.com>
2019-10-16 17:14:13 +0200

    Split scriptconv stage from parseconv. Fix edge case.
    Part 2 of redesign for parseconv & generator
    (planned in 2013). Not exactly as originally
    thought of, but as a basic preparation, split
    parser output representation from script data
    representation which is later converted to
    the internal audio generation "program".
    
    Fixes output for updates to operators created in
    lists cleared in the same event. The updates are
    now made to the right (unused) operator, instead
    of to the wrong operator.
    
    Design changes:
     * The parser is still mostly the same, with
       a very small simplification.
     * The old parseconv has become scriptconv,
       with minor adjustments for input data changes.
     * New parseconv mostly copies parser output,
       but leaves flat per-event operator list.
     * Now scriptconv prints all, not one, errors
       when checking validity of the produced data.
    
    Todo: More refactoring, further redesign
    (for which the door is now open).
    
    Change log:
     * 2019-10-16: Placeholder for separating stages.
     * 2019-10-23: Split parseconv module into
           parseconv and scriptconv.
     * 2019-10-25: Minimal cleanup.
     * 2019-10-26: Fill in more, still not functional.
     * 2019-10-27: Make functional.
           Set version to v0.3.3.
     * 2019-10-28: Fix 'test-builder' build.
     * 2019-10-31: Move down to after v0.2.14.
           Set version to v0.2.15.
     * 2019-12-12: In parseconv, remove unneeded typecast.
           In scriptconv, update commented-out operator re-use
           code, copied from voice re-use code. (Now compiles,
           but still not functional.)
     * 2019-12-22: Merge composite timing fix.
           Merge add missing default time adjustment
           for amp2 and freq2 ramps.
     * 2019-12-24: Merge ramp and v0.2.5-related changes.
     * 2019-12-25: Merge ramp default time changes.
     * 2020-01-04: Merge split voicegraph
           from parseconv/scriptconv, and adapt some
           refactoring of scriptconv, including make
           output verification print every error, not one.
           Simplify allocation,
           turn SAU_ProgramEvent array into array of pointers.
           Update naming where "parse" should be "script".
     * 2020-07-23: Merge renaming in ramp module.
           Adjust for rearrangement of other changes.
           Set version to v0.2.14.
     * 2020-07-26: Keep version at v0.2-beta.
     * 2020-10-21: Move to ssndgen v0.2-dev.

--- builder/scriptconv.h
+++ builder/scriptconv.h
@@ -1,4 +1,4 @@
-/* ssndgen: Parse result to audio program converter.
+/* ssndgen: Script data to audio program converter.
  * Copyright (c) 2011-2012, 2017-2020 Joel K. Pettersson
  * <joelkpettersson@gmail.com>.
  *
@@ -46,7 +46,7 @@ enum {
  * Per-operator state used during program data allocation.
  */
 typedef struct SSG_OpAllocState {
-	SSG_ScriptOpData *last_pod;
+	SSG_ScriptOpData *last_sod;
 	SSG_ProgramOpAdjcs *adjcs;
 	uint32_t flags;
 	//uint32_t duration_ms;
