9ebe21b00654d8d8677fb873c0179c70e3ab4b61
Joel K. Pettersson <joelkpettersson@gmail.com>
2020-01-29 10:31:19 +0100

    Refactor program.h types and their uses.
    Re-do much of the basic clean-up in the "event overhaul"
    redesign of 2011-04-10, while keeping the old design.
    
    Refactor program.h types:
     * Remove SETTOP/SETNESTED node "types".
       Flatten out spec.set and spec.nested subfields.
     * Move EXEC/ENTERED flags to generator.c.
       Remove now-unused flag field from program node.
     * Merge mods field and flags into values field and flags.
       Change int type for values from uint8_t to uint32_t.
    
    Further use a parameter flag mask to set
    all flags for initial operator nodes in parser.
    Changing the generator to handle initial
    and updating nodes the same remains.
    
    Rename generator SetNode to UpdateNode.
    Use a separate calloc'd array for
    preallocated data of each type.
    
    Change log:
     * 2020-01-29: Part 1.
     * 2020-01-30: generator: Correct change to set EXEC flag.
           Use separate calloc'd arrays for preallocated data.

--- program.h
+++ program.h
@@ -14,45 +14,40 @@
 #pragma once
 #include "wave.h"
 
+/* Node types. */
 enum {
   MGS_TYPE_TOP = 0,
   MGS_TYPE_NESTED,
-  MGS_TYPE_SETTOP,
-  MGS_TYPE_SETNESTED,
   MGS_TYPE_ENV
 };
 
-enum {
-  MGS_FLAG_EXEC = 1<<0,
-  MGS_FLAG_ENTERED = 1<<1
-};
-
+/* Operator attributes. */
 enum {
   MGS_ATTR_FREQRATIO = 1<<0,
   MGS_ATTR_DYNFREQRATIO = 1<<1
 };
 
+/* Audio panning modes. */
 enum {
   MGS_MODE_CENTER = 0,
   MGS_MODE_LEFT   = 1,
   MGS_MODE_RIGHT  = 2
 };
 
-enum {
-  MGS_TIME = 1<<0,
-  MGS_WAVE = 1<<1,
-  MGS_FREQ = 1<<2,
-  MGS_DYNFREQ = 1<<3,
-  MGS_PHASE = 1<<4,
-  MGS_AMP = 1<<5,
-  MGS_DYNAMP = 1<<6,
-  MGS_ATTR = 1<<7
-};
-
+/* Operator parameters. */
 enum {
   MGS_PMODS = 1<<0,
   MGS_FMODS = 1<<1,
-  MGS_AMODS = 1<<2
+  MGS_AMODS = 1<<2,
+  MGS_TIME = 1<<3,
+  MGS_WAVE = 1<<4,
+  MGS_FREQ = 1<<5,
+  MGS_DYNFREQ = 1<<6,
+  MGS_PHASE = 1<<7,
+  MGS_AMP = 1<<8,
+  MGS_DYNAMP = 1<<9,
+  MGS_ATTR = 1<<10,
+  MGS_PARAM_MASK = (1<<11) - 1
 };
 
 typedef struct MGS_ProgramNodeChain {
@@ -62,26 +57,20 @@ typedef struct MGS_ProgramNodeChain {
 
 typedef struct MGS_ProgramNode {
   struct MGS_ProgramNode *next;
-  uint8_t type, flag, attr, wave, mode;
+  struct MGS_ProgramNode *ref_prev;
+  uint8_t type, attr, wave, mode;
   float time, delay, freq, dynfreq, phase, amp, dynamp;
   uint32_t id;
+  uint32_t values;
   MGS_ProgramNodeChain pmod, fmod, amod;
-  union { /* type-specific data */
-    struct {
-      struct MGS_ProgramNode *link;
-    } nested;
-    struct {
-      uint8_t values;
-      uint8_t mods;
-      struct MGS_ProgramNode *ref;
-    } set;
-  } spec;
+  struct MGS_ProgramNode *nested_next;
 } MGS_ProgramNode;
 
 struct MGS_SymTab;
 
 struct MGS_Program {
-  MGS_ProgramNode *nodelist;
+  MGS_ProgramNode *top_list;
+  MGS_ProgramNode *nested_list;
   uint32_t nodec;
   uint32_t topc; /* nodes >= topc are nested ones, ids starting over from 0 */
   struct MGS_SymTab *symtab;
