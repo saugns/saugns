f53030e47516e418751823b578cd228cef232659
Joel K. Pettersson <joelkpettersson@gmail.com>
2020-07-22 17:48:11 +0200

    Fix -r 1 hang, make 96000 default. Reorganize code.
    Move audiodev and wavfile modules to player/,
    rename main "renderer.c" to "player/player.c"
    and rename keyword-like SGS__* macros to sgs*
    without functional change. Also relicense the
    audiodev and wavfile code from LGPL-3 to ISC.
    
    Change default -r sample rate to 96000 Hz,
    undoing the 2012-02-10 change to 44100 Hz.
    
    Add minimum per-channel buffer length
    (currently 1 sample) in player.c, fix
    hang if/when -r 1 loops 0-length run.
    
    Change log:
     * 2020-07-22: Rename renderer part -> player
           and move the audiodev & wavfile files.
           Also includes renaming SGS__* defines,
           for consistency, to sgs* (more like in
           2020-06 "mgensys"), and relicensing of
           audiodev & wavfile from LGPL-3 to ISC.
     * 2020-07-26: Reduce diff with 2020-06 "mgensys" experiment,
           changing default sample rate to 96000 Hz (was 44100 Hz
           after 2012-02-10). Also use ISC license header for the
           common module and math.h, to keep permissive code free
           from dependencies on including non-permissive code.
     * 2020-11-09: Renaming in "player/player.c".
     * 2020-12-14: Ensure output buffer holds >= 1 sample, fix
           hang with -r 1 when 0 samples were ran in run loop.

--- player/audiodev/sndio.c
+++ player/audiodev/sndio.c
@@ -1,14 +1,18 @@
 /* sgensys: sndio audio output support.
- * Copyright (c) 2018 Joel K. Pettersson
+ * Copyright (c) 2018-2020 Joel K. Pettersson
  * <joelkpettersson@gmail.com>.
  *
- * This file and the software of which it is part is distributed under the
- * terms of the GNU Lesser General Public License, either version 3 or (at
- * your option) any later version, WITHOUT ANY WARRANTY, not even of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * View the file COPYING for details, or if missing, see
- * <https://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <sndio.h>
@@ -33,8 +37,8 @@ static inline SGS_AudioDev *open_sndio(const char *restrict name,
 	par.pchan = channels;
 	par.rate = *srate;
 	par.xrun = SIO_SYNC;
-	if ((!sio_setpar(hdl, &par)) ||
-	    (!sio_getpar(hdl, &par))) goto ERROR;
+	if ((!sio_setpar(hdl, &par)) || (!sio_getpar(hdl, &par)))
+		goto ERROR;
 	if (par.rate != *srate) {
 		SGS_warning("sndio", "sample rate %d unsupported, using %d",
 			*srate, par.rate);
@@ -60,7 +64,7 @@ ERROR:
  * Destroy instance. Close sndio device,
  * ending playback in the process.
  */
-static inline void close_sndio(SGS_AudioDev *o) {
+static inline void close_sndio(SGS_AudioDev *restrict o) {
 	sio_close(o->ref.handle);
 	free(o);
 }
