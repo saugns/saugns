d1916a2a862fcc6f34b18b9a7753cbf38a2ff6a8
Joel K. Pettersson <joelkpettersson@gmail.com>
2020-07-22 23:58:37 +0200

    Split voicegraph code. Refactor further. Change 'r' syntax.
    Syntax change: Disable multiplicative inverse for 'r',
    change description from "frequency ratio" to
    "relative frequency". It's more intuitive to handle
    n:m carrier-modulator frequency ratios as (m/n) values
    than (n/m) values, when actually adjusting them.
    (With multiplicative inverse, a higher value meant a
    lower frequency; now, a higher value means a higher
    frequency.) This reverses a change from 2011-03-04.
    
    Update README.
    
    Refactoring changes:
     * Further refactoring of the file module, part 2.
     * Remove parser SCOPE_SAME (unused since 2012).
     * Small flag handling change for parser end of event.
     * Small mempool performance improvement.
       Remove its dependence on arrtype.
     * Modularize parseconv a bit. Split out some
       types into parseconv.h, move voice graph traversal
       code into voicegraph.c.
    
    Change log:
     * 2019-07-04: Merge down 'r' syntax change.
       Old branch:
        - 2019-07-04: v0.3.1-beta: Begin syntax tweaks.
              Disable multiplicative inverse for 'r'.
              Update scripts and README.
     * 2019-07-31: README typo fix & ramp.c comment fix.
     * 2019-08-03: Fix description of 'r' syntax wrt freq ratio.
     * 2020-01-04: Move down clean-ups:
       Old branch:
        - 2019-07-11: (From v0.3.1) Microflensing.
              parser: Remove unused mul_inv option from scan_num(),
              use a SAU__noinline to help clang keep size down.
        - 2019-12-14: parser: Remove SCOPE_SAME (unused since 2012).
     * 2020-07-23: Move in mempool update
           from 2020-06 "mgensys" experiment.
       Old branch:
        - 2020-02-04: Minor mempool performance improvement using
              a first_i start for binary search, increased
              past non-free blocks on each block addition.
              Add MGS_MEM_DEBUG flag to common.h and mempool.c,
              for debug-friendly memory handling. Makes mempool
              calloc every allocation. This allows full testing
              of memory use with e.g. OpenBSD "vm.malloc_conf=S".
     * 2020-08-21: Move down file module changes.
       Old branch:
        - 2019-09-12: Merge down 08-24 file module refactoring,
              with finishing touch.
        - 2020-01-03: Merge -pedantic change for file.c.
        - 2020-07-23: Change file module to ISC license.
     * 2020-08-22: Move down parseconv refactoring.
       Old branch:
        - 2019-12-31: Split voicegraph from scriptconv,
              also adding builder.h. Renaming and refactoring.
        - 2020-01-01: Add missing SAU_ prefix for VoiceGraph type.
        - 2020-01-04: Move down split voicegraph from scriptconv
              from v0.3.6d - making it a split from parseconv.
              Keep it simple and without functional change.
              Rename the new header parseconv.h instead.

---
+++ builder/parseconv.h
@@ -0,0 +1,81 @@
+/* sgensys: Parse result to audio program converter.
+ * Copyright (c) 2011-2012, 2017-2020 Joel K. Pettersson
+ * <joelkpettersson@gmail.com>.
+ *
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version, WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * View the file COPYING for details, or if missing, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "../script.h"
+#include "../program.h"
+#include "../arrtype.h"
+
+/*
+ * Voice allocation state flags.
+ */
+enum {
+	SGS_VAS_GRAPH = 1<<0,
+};
+
+/*
+ * Per-voice state used during program data allocation.
+ */
+typedef struct SGS_VoAllocState {
+	SGS_ScriptEvData *last_ev;
+	SGS_ProgramOpGraph *op_graph;
+	uint32_t flags;
+	uint32_t duration_ms;
+} SGS_VoAllocState;
+
+SGS_DEF_ArrType(SGS_VoAlloc, SGS_VoAllocState, _)
+
+/*
+ * Operator allocation state flags.
+ */
+enum {
+	SGS_OAS_VISITED = 1<<0,
+};
+
+/*
+ * Per-operator state used during program data allocation.
+ */
+typedef struct SGS_OpAllocState {
+	SGS_ScriptOpData *last_pod;
+	SGS_ProgramOpAdjcs *adjcs;
+	uint32_t flags;
+	//uint32_t duration_ms;
+} SGS_OpAllocState;
+
+SGS_DEF_ArrType(SGS_OpAlloc, SGS_OpAllocState, _)
+
+SGS_DEF_ArrType(OpRefArr, SGS_ProgramOpRef, )
+
+/**
+ * Voice data, held during program building and set per event.
+ */
+typedef struct SGS_VoiceGraph {
+	OpRefArr op_list;
+	SGS_VoAlloc *va;
+	SGS_OpAlloc *oa;
+	uint32_t op_nest_depth;
+} SGS_VoiceGraph;
+
+/**
+ * Initialize instance for use.
+ */
+static inline void SGS_init_VoiceGraph(SGS_VoiceGraph *restrict o,
+		SGS_VoAlloc *restrict va, SGS_OpAlloc *restrict oa) {
+	o->va = va;
+	o->oa = oa;
+}
+
+void SGS_fini_VoiceGraph(SGS_VoiceGraph *restrict o);
+
+bool SGS_VoiceGraph_set(SGS_VoiceGraph *restrict o,
+		const SGS_ProgramEvent *restrict ev);
