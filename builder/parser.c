e9c6eef555eb4289dcb53b47fe1ed765c9206a10
Joel K. Pettersson <joelkpettersson@gmail.com>
2020-08-25 18:49:54 +0200

    Change panning/channel mixing syntax. Tidy parser a little.
    Syntax changes:
     * Replace panning 'P' keyword with channel mixing parameter
       'c' and 'S c' for default. The scale changes from 0.0-1.0
       left-right to (-1.0) left, 0.0 center, 1.0 right. And the
       letters 'C', 'L', and 'R' become named constants for 0.0,
       (-1.0), and 1.0.
    
    The panning or "channel mixing syntax" is now the same as in
    the 2020-06 "mgensys" version, replacing the 'P' (2011-12-13)
    syntax and earlier 'b' (2011-04-10) variant.
    
    Adjust SGS_Scanner_getd() to call a callback if provided for
    named constant reading, using this both for the new channel
    C/L/R constants and the old note syntax code after conversion.
    
    Update README.
    
    Change log:
     * 2020-08-25: Add, use parameter flag masks.
     * 2020-09-18: Rebase, tweak previous change.
     * 2020-09-19: Refactor parser scan_ramp() to
           handle both multi-value and state cases.
           Add callback to SAU_Scanner_getd() for named constants.
           Port 2020-06 "mgensys" channel mixing syntax.
           Move devtests/panning.sau to examples/tests/.
           Update README.
     * 2020-09-21: Tidy up changed scanner code.
           Add handling for sign before named constant.
           (Redone for consistency, not currently used.)
     * 2020-10-03: Make scanner always call numconst_f when given,
           simply check return length. Remove useless warning when
           note syntax callback called for non-note characters.
     * 2021-07-10: Move down to after scanner clean-up.

--- builder/parser.c
+++ builder/parser.c
@@ -27,9 +27,6 @@
 #define IS_UPPER(c) ((c) >= 'A' && (c) <= 'Z')
 #define IS_ALPHA(c) (IS_LOWER(c) || IS_UPPER(c))
 
-/* Sensible to print, for ASCII only. */
-#define IS_VISIBLE(c) ((c) >= '!' && (c) <= '~')
-
 struct ScanLookup {
 	SGS_ScriptOptions sopt;
 	const char *const*wave_names;
@@ -46,6 +43,7 @@ static const SGS_ScriptOptions def_sopt = {
 	.def_time_ms = 1000,
 	.def_freq = 444.f,
 	.def_relfreq = 1.f,
+	.def_chanmix = 0.f,
 };
 
 static bool init_ScanLookup(struct ScanLookup *restrict o,
@@ -71,7 +69,7 @@ static bool init_ScanLookup(struct ScanLookup *restrict o,
 static bool handle_unknown_or_eof(SGS_Scanner *restrict o, uint8_t c) {
 	if (c == 0)
 		return false;
-	if (IS_VISIBLE(c)) {
+	if (SGS_IS_ASCIIVISIBLE(c)) {
 		SGS_Scanner_warning(o, NULL,
 				"invalid character '%c'", c);
 	} else {
@@ -107,11 +105,9 @@ static void warn_closing_without_opening(SGS_Scanner *restrict o,
 			close_c, open_c);
 }
 
-typedef float (*NumSym_f)(SGS_Scanner *restrict o);
-
 struct NumParser {
 	SGS_Scanner *sc;
-	NumSym_f numsym_f;
+	SGS_ScanNumConst_f numconst_f;
 	SGS_ScanFrame sf_start;
 	bool has_infnum;
 };
@@ -136,16 +132,11 @@ static double scan_num_r(struct NumParser *restrict o,
 	}
 	if (c == '(') {
 		num = scan_num_r(o, NUMEXP_SUB, level+1);
-	} else if (o->numsym_f && IS_ALPHA(c)) {
-		SGS_Scanner_ungetc(sc);
-		num = o->numsym_f(sc);
-		if (isnan(num))
-			return NAN;
 	} else {
 		size_t read_len;
 		SGS_Scanner_ungetc(sc);
-		SGS_Scanner_getd(sc, &num, false, &read_len);
-		if (read_len == 0)
+		SGS_Scanner_getd(sc, &num, false, &read_len, o->numconst_f);
+		if (read_len == 0 || isnan(num))
 			return NAN;
 	}
 	if (minus) num = -num;
@@ -196,8 +187,8 @@ DEFER:
 	return num;
 }
 static sgsNoinline bool scan_num(SGS_Scanner *restrict o,
-		NumSym_f scan_numsym, float *restrict var) {
-	struct NumParser np = {o, scan_numsym, o->sf, false};
+		SGS_ScanNumConst_f scan_numconst, float *restrict var) {
+	struct NumParser np = {o, scan_numconst, o->sf, false};
 	uint8_t ws_level = o->ws_level;
 	float num = scan_num_r(&np, NUMEXP_NUM, 0);
 	SGS_Scanner_setws_level(o, ws_level); // restore if changed
@@ -227,8 +218,28 @@ static sgsNoinline bool scan_time_val(SGS_Scanner *restrict o,
 	return true;
 }
 
+static size_t scan_chanmix_const(SGS_Scanner *restrict o,
+		double *restrict val) {
+	char c = SGS_File_GETC(o->f);
+	switch (c) {
+	case 'C':
+		*val = 0.f;
+		return 1;
+	case 'L':
+		*val = -1.f;
+		return 1;
+	case 'R':
+		*val = 1.f;
+		return 1;
+	default:
+		SGS_File_DECP(o->f);
+		return 0;
+	}
+}
+
 #define OCTAVES 11
-static float scan_note(SGS_Scanner *restrict o) {
+static size_t scan_note_const(SGS_Scanner *restrict o,
+		double *restrict val) {
 	static const float octaves[OCTAVES] = {
 		(1.f/16.f),
 		(1.f/8.f),
@@ -274,40 +285,43 @@ static float scan_note(SGS_Scanner *restrict o) {
 			25.f/12.f
 		}
 	};
+	SGS_File *f = o->f;
 	struct ScanLookup *sl = o->data;
-	float freq;
-	uint8_t c = SGS_Scanner_getc(o);
+	size_t len = 0, num_len;
+	uint8_t c;
+	double freq;
 	int32_t octave;
 	int32_t semitone = 1, note;
 	int32_t subnote = -1;
-	size_t read_len;
+	c = SGS_File_GETC(f); ++len;
 	if (c >= 'a' && c <= 'g') {
 		subnote = c - 'c';
 		if (subnote < 0) /* a, b */
 			subnote += 7;
-		c = SGS_Scanner_getc(o);
+		c = SGS_File_GETC(f); ++len;
 	}
 	if (c < 'A' || c > 'G') {
-		SGS_Scanner_warning(o, NULL,
-"invalid note specified - should be C, D, E, F, G, A or B");
-		return NAN;
+		SGS_File_UNGETN(f, len);
+		return 0;
 	}
 	note = c - 'C';
 	if (note < 0) /* A, B */
 		note += 7;
-	c = SGS_Scanner_getc(o);
+	c = SGS_File_GETC(f); ++len;
 	if (c == 's')
 		semitone = 2;
 	else if (c == 'f')
 		semitone = 0;
-	else
-		SGS_Scanner_ungetc(o);
-	SGS_Scanner_geti(o, &octave, false, &read_len);
-	if (read_len == 0)
+	else {
+		SGS_File_DECP(f); --len;
+	}
+	SGS_Scanner_geti(o, &octave, false, &num_len);
+	len += num_len;
+	if (num_len == 0)
 		octave = 4;
 	else if (octave >= OCTAVES) {
 		SGS_Scanner_warning(o, NULL,
-"invalid octave specified for note - valid range 0-10");
+"invalid note octave number, using 4 (valid range 0-10)");
 		octave = 4;
 	}
 	freq = sl->sopt.A4_freq * (3.f/5.f); /* get C4 */
@@ -316,7 +330,8 @@ static float scan_note(SGS_Scanner *restrict o) {
 		freq *= 1.f + (notes[semitone][note+1] /
 				notes[semitone][note] - 1.f) *
 			(notes[1][subnote] - 1.f);
-	return freq;
+	*val = (double) freq;
+	return len;
 }
 
 static SGS_SymStr *scan_label(SGS_Scanner *restrict o,
@@ -364,9 +379,9 @@ static bool scan_wavetype(SGS_Scanner *restrict o, size_t *restrict found_id) {
 }
 
 static bool scan_ramp_state(SGS_Scanner *restrict o,
-		NumSym_f scan_numsym,
+		SGS_ScanNumConst_f scan_numconst,
 		SGS_Ramp *restrict ramp, bool mult) {
-	if (!scan_num(o, scan_numsym, &ramp->v0))
+	if (!scan_num(o, scan_numconst, &ramp->v0))
 		return false;
 	if (mult) {
 		ramp->flags |= SGS_RAMPP_STATE_RATIO;
@@ -377,8 +392,11 @@ static bool scan_ramp_state(SGS_Scanner *restrict o,
 	return true;
 }
 
-static bool scan_ramp(SGS_Scanner *restrict o, NumSym_f scan_numsym,
+static bool scan_ramp(SGS_Scanner *restrict o, SGS_ScanNumConst_f scan_numconst,
 		SGS_Ramp *restrict ramp, bool mult) {
+	if (!SGS_Scanner_tryc(o, '{')) {
+		return scan_ramp_state(o, scan_numconst, ramp, mult);
+	}
 	struct ScanLookup *sl = o->data;
 	bool goal = false;
 	bool time_set = (ramp->flags & SGS_RAMPP_TIME) != 0;
@@ -411,7 +429,7 @@ static bool scan_ramp(SGS_Scanner *restrict o, NumSym_f scan_numsym,
 				time_set = true;
 			break;
 		case 'v':
-			if (scan_num(o, scan_numsym, &vt))
+			if (scan_num(o, scan_numconst, &vt))
 				goal = true;
 			break;
 		case '}':
@@ -649,15 +667,7 @@ static void end_operator(ParseLevel *restrict pl) {
 		/*
 		 * Reset all operator state for initial event.
 		 */
-		op->op_params |= SGS_POPP_ADJCS |
-			SGS_POPP_WAVE |
-			SGS_POPP_TIME |
-			SGS_POPP_SILENCE |
-			SGS_POPP_FREQ |
-			SGS_POPP_FREQ2 |
-			SGS_POPP_PHASE |
-			SGS_POPP_AMP |
-			SGS_POPP_AMP2;
+		op->op_params |= SGS_POP_PARAMS;
 	} else {
 		if (op->wave != pop->wave)
 			op->op_params |= SGS_POPP_WAVE;
@@ -678,7 +688,7 @@ static void end_event(ParseLevel *restrict pl) {
 	SGS_ScriptEvData *e = pl->event;
 	end_operator(pl);
 	if (SGS_Ramp_ENABLED(&e->pan))
-		e->vo_params |= SGS_PVOP_PAN;
+		e->vo_params |= SGS_PVO_PARAMS & ~SGS_PVOP_OPLIST;
 	SGS_ScriptEvData *pve = e->voice_prev;
 	if (!pve) {
 		/*
@@ -698,6 +708,7 @@ static void end_event(ParseLevel *restrict pl) {
 static void begin_event(ParseLevel *restrict pl,
 		bool is_composite) {
 	SGS_Parser *o = pl->o;
+	struct ScanLookup *sl = &o->sl;
 	SGS_ScriptEvData *e, *pve;
 	end_event(pl);
 	pl->event = calloc(1, sizeof(SGS_ScriptEvData));
@@ -726,7 +737,7 @@ static void begin_event(ParseLevel *restrict pl,
 		/*
 		 * New voice with initial parameter values.
 		 */
-		e->pan.v0 = 0.5f; /* center */
+		e->pan.v0 = sl->sopt.def_chanmix;
 		e->pan.flags |= SGS_RAMPP_STATE;
 	}
 	if (!is_composite) {
@@ -953,8 +964,13 @@ static bool parse_settings(ParseLevel *restrict pl) {
 			if (scan_num(sc, NULL, &sl->sopt.ampmult))
 				sl->sopt.changed |= SGS_SOPT_AMPMULT;
 			break;
+		case 'c':
+			if (scan_num(sc, scan_chanmix_const,
+						&sl->sopt.def_chanmix))
+				sl->sopt.changed |= SGS_SOPT_DEF_CHANMIX;
+			break;
 		case 'f':
-			if (scan_num(sc, scan_note, &sl->sopt.def_freq))
+			if (scan_num(sc, scan_note_const, &sl->sopt.def_freq))
 				sl->sopt.changed |= SGS_SOPT_DEF_FREQ;
 			break;
 		case 'n': {
@@ -971,7 +987,7 @@ static bool parse_settings(ParseLevel *restrict pl) {
 			break; }
 		case 'r':
 			if (scan_num(sc, NULL, &sl->sopt.def_relfreq))
-				sl->sopt.changed |= SGS_SOPT_DEF_RATIO;
+				sl->sopt.changed |= SGS_SOPT_DEF_RELFREQ;
 			break;
 		case 't':
 			if (scan_time_val(sc, &sl->sopt.def_time_ms))
@@ -995,17 +1011,9 @@ static bool parse_ev_amp(ParseLevel *restrict pl) {
 	SGS_Parser *o = pl->o;
 	SGS_Scanner *sc = o->sc;
 	SGS_ScriptOpData *op = pl->operator;
-	if (SGS_Scanner_tryc(sc, '{')) {
-		scan_ramp(sc, NULL, &op->amp, false);
-	} else {
-		scan_ramp_state(sc, NULL, &op->amp, false);
-	}
+	scan_ramp(sc, NULL, &op->amp, false);
 	if (SGS_Scanner_tryc(sc, ',')) {
-		if (SGS_Scanner_tryc(sc, '{')) {
-			scan_ramp(sc, NULL, &op->amp2, false);
-		} else {
-			scan_ramp_state(sc, NULL, &op->amp2, false);
-		}
+		scan_ramp(sc, NULL, &op->amp2, false);
 	}
 	if (SGS_Scanner_tryc(sc, '~') && SGS_Scanner_tryc(sc, '[')) {
 		if (op->amods.count > 0) {
@@ -1017,24 +1025,27 @@ static bool parse_ev_amp(ParseLevel *restrict pl) {
 	return false;
 }
 
+static bool parse_ev_chanmix(ParseLevel *restrict pl) {
+	SGS_Parser *o = pl->o;
+	SGS_Scanner *sc = o->sc;
+	SGS_ScriptEvData *e = pl->event;
+	SGS_ScriptOpData *op = pl->operator;
+	if (op->op_flags & SGS_SDOP_NESTED)
+		return true; // reject
+	scan_ramp(sc, scan_chanmix_const, &e->pan, false);
+	return false;
+}
+
 static bool parse_ev_freq(ParseLevel *restrict pl, bool rel_freq) {
 	SGS_Parser *o = pl->o;
 	SGS_Scanner *sc = o->sc;
 	SGS_ScriptOpData *op = pl->operator;
 	if (rel_freq && !(op->op_flags & SGS_SDOP_NESTED))
 		return true; // reject
-	NumSym_f numsym_f = rel_freq ? NULL : scan_note;
-	if (SGS_Scanner_tryc(sc, '{')) {
-		scan_ramp(sc, numsym_f, &op->freq, rel_freq);
-	} else {
-		scan_ramp_state(sc, numsym_f, &op->freq, rel_freq);
-	}
+	SGS_ScanNumConst_f numconst_f = rel_freq ? NULL : scan_note_const;
+	scan_ramp(sc, numconst_f, &op->freq, rel_freq);
 	if (SGS_Scanner_tryc(sc, ',')) {
-		if (SGS_Scanner_tryc(sc, '{')) {
-			scan_ramp(sc, numsym_f, &op->freq2, rel_freq);
-		} else {
-			scan_ramp_state(sc, numsym_f, &op->freq2, rel_freq);
-		}
+		scan_ramp(sc, numconst_f, &op->freq2, rel_freq);
 	}
 	if (SGS_Scanner_tryc(sc, '~') && SGS_Scanner_tryc(sc, '[')) {
 		if (op->fmods.count > 0) {
@@ -1070,22 +1081,12 @@ static bool parse_step(ParseLevel *restrict pl) {
 	SGS_Parser *o = pl->o;
 	struct ScanLookup *sl = &o->sl;
 	SGS_Scanner *sc = o->sc;
-	SGS_ScriptEvData *e = pl->event;
 	SGS_ScriptOpData *op = pl->operator;
 	pl->location = SDPL_IN_EVENT;
 	for (;;) {
 		uint8_t c = SGS_Scanner_getc(sc);
 		switch (c) {
 		case SGS_SCAN_SPACE:
-			break;
-		case 'P':
-			if ((pl->pl_flags & SDPL_NESTED_SCOPE) != 0)
-				goto UNKNOWN;
-			if (SGS_Scanner_tryc(sc, '{')) {
-				scan_ramp(sc, NULL, &e->pan, false);
-			} else {
-				scan_ramp_state(sc, NULL, &e->pan, false);
-			}
 			break;
 		case '\\':
 			if (parse_waittime(pl)) {
@@ -1096,6 +1097,9 @@ static bool parse_step(ParseLevel *restrict pl) {
 			break;
 		case 'a':
 			if (parse_ev_amp(pl)) goto UNKNOWN;
+			break;
+		case 'c':
+			if (parse_ev_chanmix(pl)) goto UNKNOWN;
 			break;
 		case 'f':
 			if (parse_ev_freq(pl, false)) goto UNKNOWN;
