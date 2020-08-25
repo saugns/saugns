5ec27984ce777746367b6de1c55a65053e60d1b8
Joel K. Pettersson <joelkpettersson@gmail.com>
2020-08-25 18:49:54 +0200

    Change panning/channel mixing syntax. Parser clean-up.
    Syntax changes:
     * Replace panning 'P' keyword with channel mixing parameter
       'c' and 'S c' for default. The scale changes from 0.0-1.0
       left-right to (-1.0) left, 0.0 center, 1.0 right. And the
       letters 'C', 'L', and 'R' become named constants for 0.0,
       (-1.0), and 1.0.
    
    The panning or "channel mixing syntax" is now the same as in
    the 2020-06 "mgensys" version, replacing the 'P' (2011-12-13)
    syntax and earlier 'b' (2011-04-10) variant.
    
    Adjust SAU_Scanner_getd() to call a callback if provided for
    named constant reading, using this both for the new channel
    C/L/R constants and the old note syntax code after conversion.
    
    Update README.
    
    Change log:
     * 2020-08-25: Add, use parameter flag masks.
     * 2020-09-18: Rebase, tweak previous change.
     * 2020-09-19: Refactor parser scan_ramp() to
           handle both multi-value and state cases.
           Add callback to SAU_Scanner_getd() for named constants.
           Port 2020-06 "mgensys" channel mixing syntax.
           Move devtests/panning.sau to examples/tests/.
           Update README.
     * 2020-09-21: Tidy up changed scanner code.
           Add handling for sign before named constant.
           (Redone for consistency, not currently used.)
     * 2020-10-03: Make scanner always call numconst_f when given,
           simply check return length. Remove useless warning when
           note syntax callback called for non-note characters.

--- builder/scanner.h
+++ builder/scanner.h
@@ -1,5 +1,5 @@
 /* saugns: Script scanner module.
- * Copyright (c) 2014, 2017-2019 Joel K. Pettersson
+ * Copyright (c) 2014, 2017-2020 Joel K. Pettersson
  * <joelkpettersson@gmail.com>.
  *
  * This file and the software of which it is part is distributed under the
@@ -185,6 +185,13 @@ static inline uint8_t SAU_Scanner_usefilter(SAU_Scanner *restrict o,
 	return c;
 }
 
+/**
+ * Callback type allowing reading of named constants using SAU_Scanner_getd().
+ * Should return non-zero length if number read and \p var set.
+ */
+typedef size_t (*SAU_ScanNumConst_f)(SAU_Scanner *restrict o,
+		double *restrict var);
+
 uint8_t SAU_Scanner_getc(SAU_Scanner *restrict o);
 bool SAU_Scanner_tryc(SAU_Scanner *restrict o, uint8_t testc);
 uint32_t SAU_Scanner_ungetc(SAU_Scanner *restrict o);
@@ -193,7 +200,8 @@ bool SAU_Scanner_geti(SAU_Scanner *restrict o,
 		size_t *restrict str_len);
 bool SAU_Scanner_getd(SAU_Scanner *restrict o,
 		double *restrict var, bool allow_sign,
-		size_t *restrict str_len);
+		size_t *restrict str_len,
+		SAU_ScanNumConst_f numconst_f);
 bool SAU_Scanner_getsymstr(SAU_Scanner *restrict o,
 		const void **restrict strp, size_t *restrict lenp);
 
