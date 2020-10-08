85873efb7f941e43816484e1a38d51823acc0172
Joel K. Pettersson <joelkpettersson@gmail.com>
2020-10-08 17:56:01 +0200

    Refactor scriptconv and program.h types.
    Refactor scriptconv and program.h types, using simpler
    types and mempool for all produced data. Replace types
    SSG_ProgramOpGraph and SSG_ProgramOpAdjcs with the new
    SSG_ProgramOpList (like the old graph type).
    
    Add and use "mpmemdup" methods for arrtype and ptrlist
    replacing ordinary "memdup" and traversal for freeing.
    
    Extracted from saugns v0.3.6 changes, separating their
    more basic clean-ups from changes to replace node list
    type.
    
    Todo: Remove the SSG_POPP_ADJCS flag (done in v0.3.6).
    Flag removal best done later, then deceptively simple.
    
    Change log:
     * 2019-12-26: Remove SAU_POPP_ADJCS flag,
           replace checks using other information.
           Replace SAU_ProgramOpGraph and SAU_ProgramOpAdjcs
           types with SAU_ProgramOpList, simplify code.
     * 2019-12-27: Add a sauNoinline in scriptconv.c.
     * 2020-10-07: Merge with changes from after new node list
           code was added and used, preserving old ptrlist usage.
       Old branch:
        - 2019-12-31: Renaming and refactoring.
              Add and use arrtype "mpmemdup" for scriptconv
              and voicegraph arrays, removing all but one free.
        - 2020-01-01: Add and use ptrlist "mpmemdup"; simplify,
              turn SAU_ProgramEvent array into array of pointers.
        - 2020-01-03: Merge de-const-ify ptrlist void pointers.
        - 2020-01-04: Move down from later v0.3.6 commits.
        - 2020-02-27: Remove const from parameters for arrtype
              memdup functions.
        - 2020-07-26: ISC-relicense arrtype, ptrlist, nodelist.
     * 2020-10-08: Re-add SAU_POPP_ADJCS flag, reduce diff.
           This effectively splits the v0.3.6 redesign into
           two parts.
     * 2020-10-09: Move down to sgensys v0.2-beta.
     * 2020-10-21: Move to ssndgen v0.2-dev.
     * 2020-10-27: Merge generator flensing.

--- ptrlist.h
+++ ptrlist.h
@@ -2,13 +2,17 @@
  * Copyright (c) 2011-2012, 2018-2020 Joel K. Pettersson
  * <joelkpettersson@gmail.com>.
  *
- * This file and the software of which it is part is distributed under the
- * terms of the GNU Lesser General Public License, either version 3 or (at
- * your option) any later version, WITHOUT ANY WARRANTY, not even of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * View the file COPYING for details, or if missing, see
- * <https://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #pragma once
@@ -45,8 +49,12 @@ typedef struct SSG_PtrList {
 #define SSG_PtrList_GET(o, i) \
 	((void*) SSG_PtrList_ITEMS(o)[i])
 
+struct SSG_MemPool;
+
 bool SSG_PtrList_add(SSG_PtrList *restrict o, void *restrict item);
 void SSG_PtrList_clear(SSG_PtrList *restrict o);
 bool SSG_PtrList_memdup(SSG_PtrList *restrict o, void ***restrict dst);
+bool SSG_PtrList_mpmemdup(SSG_PtrList *restrict o, void ***restrict dst,
+		struct SSG_MemPool *restrict mempool);
 void SSG_PtrList_soft_copy(SSG_PtrList *restrict dst,
 		const SSG_PtrList *restrict src);
