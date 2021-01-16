1b413ab9a9a827be3d7c70ca0d78c812d07a8513
Joel K. Pettersson <joelkpettersson@gmail.com>
2021-01-16 21:22:35 +0100

    add sauAssume() for gcc and clang, empty otherwise.

--- sau/common.h
+++ sau/common.h
@@ -37,11 +37,19 @@
 # define sauNoinline __attribute__((noinline))
 # define sauPrintflike(string_index, first_to_check) \
 	__attribute__((format(printf, string_index, first_to_check)))
+# ifdef __clang__
+#  define sauAssume(expr) \
+	__builtin_assume(expr)
+# else
+#  define sauAssume(expr) \
+	do {if (!(expr)) __builtin_unreachable();} while (false)
+# endif
 #else
 # define sauMalloclike
 # define sauMaybeUnused
 # define sauNoinline
 # define sauPrintflike(string_index, first_to_check)
+# define sauAssume(expr)
 #endif
 
 /*
