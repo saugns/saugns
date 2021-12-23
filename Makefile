saugns/sgensys

Rework scanner, symtab, clean up parser a bit.
Redesign scanner, and use the new way of
filtering whitespace in parser. Further
testing of line and char position numbering
needed, but now better than with the v0.3.0
scanner design. Also use refactored symtab
with no functional change.
 * Add whitespace filtering level setting.
   Setting the level replaces the whitespace filtering
   functions in the table for the scanner instance.
 * Remove "nospace" versions of get/try functions.
   Simplify get function. Remove old space-skipping
   inline functions.

Change log:
 * 2019-07-07: scanner: Add ws filter level setting.
       Remove nospace versions of functions.
       Use next design. Consistent for filtering,
       even if changed. Clear up ws types naming,
       really fill in everything. Tidying.
 * 2019-07-09: Scanner flag fix.
       (Recommitted after fixing the fix.)
       Set version to v0.3.1.
 * 2019-07-11: Microflensing.
       scanner: Remove "RAW" (NULL-pointer) ws level,
       rename "PASS" to "ALL" and make it the default
       (instead of "RED1").
 * 2019-08-05: Place after new v0.3.0,
       with July syntax changes split out into v0.2.13.
 * 2019-08-17: Remove scanner ws levels between ALL and NONE.
       (Not needed in current program.)
 * 2019-10-31: (Rebased on top of new v0.2.15.)
 * 2019-12-24: Merge ramp and v0.2.5-related changes.
 * 2020-02-09: Refactor num.exp. parsing.
 * 2021-07-10: Move down redesign changes
       to after change to use the scanner
       and merge with symtab refactoring.
       Keep version at v0.2-dev.
   Old branch:
    - 2019-12-27: symtab: For varying TabItem size,
          use sizeof and key[] instead of offsetof and key[1].
          (Resulting object file identical.)
    - 2020-01-26: Import from saugns v0.3.6d.
          Remove symtab get/set functions,
          rename types, expose MGS_SymStr type - used directly
          in parser to simplify code. Also changes
          to append NULL pointer to 

Makefile
@@ -88,16 +88,16 @@ builder.o: builder.c sgensys.h script.h ptrlist.h program.h ramp.h wave.h math.h
 builder/file.o: builder/file.c builder/file.h common.h
 	$(CC) -c $(CFLAGS) builder/file.c -o builder/file.o
 
-builder/lexer.o: builder/lexer.c builder/lexer.h builder/file.h builder/symtab.h builder/scanner.h math.h common.h
+builder/lexer.o: builder/lexer.c builder/lexer.h builder/file.h builder/symtab.h mempool.h builder/scanner.h math.h common.h
 	$(CC) -c $(CFLAGS) builder/lexer.c -o builder/lexer.o
 
 builder/parseconv.o: builder/parseconv.c builder/parseconv.h program.h ramp.h wave.h math.h script.h ptrlist.h arrtype.h common.h
 	$(CC) -c $(CFLAGS) builder/parseconv.c -o builder/parseconv.o
 
-builder/parser.o: builder/parser.c builder/scanner.h builder/file.h builder/symtab.h script.h ptrlist.h program.h ramp.h wave.h math.h common.h
+builder/parser.o: builder/parser.c builder/scanner.h builder/file.h builder/symtab.h mempool.h script.h ptrlist.h program.h ramp.h wave.h math.h common.h
 	$(CC) -c $(CFLAGS) builder/parser.c -o builder/parser.o
 
-builder/scanner.o: builder/scanner.c builder/scanner.h builder/file.h builder/symtab.h math.h common.h
+builder/scanner.o: builder/scanner.c builder/scanner.h builder/file.h builder/symtab.h mempool.h math.h common.h
 	$(CC) -c $(CFLAGS) builder/scanner.c -o builder/scanner.o
 
 builder/symtab.o: builder/symtab.c builder/symtab.h mempool.h common.h
@@ -112,6 +112,9 @@ mempool.o: mempool.c mempool.h common.h
 player/audiodev.o: common.h player/audiodev.c player/audiodev.h player/audiodev/*.c
 	$(CC) -c $(CFLAGS) player/audiodev.c -o player/audiodev.o
 
+player/player.o: common.h player/audiodev.h player/player.c player/wavfile.h renderer/generator.h math.h program.h ptrlist.h ramp.h sgensys.h wave.h
+	$(CC) -c $(CFLAGS_FAST) player/player.c -o player/player.o
+
 player/wavfile.o: common.h player/wavfile.c player/wavfile.h
 	$(CC) -c $(CFLAGS) player/wavfile.c -o player/wavfile.o
 
@@ -120,9 +123,6 @@ ptrlist.o: ptrlist.c ptrlist.h common.h
 
 ramp.o: ramp.c ramp.h math.h common.h
 	$(CC) -c $(CFLAGS_FAST) ramp.c
-
-player/player.o: common.h player/audiodev.h player/player.c player/wavfile.h renderer/generator.h math.h program.h ptrlist.h ramp.h sgensys.h wave.h
-	$(CC) -c $(CFLAGS_FAST) player/player.c -o player/player.o
 
 renderer/generator.o: renderer/generator.c renderer/generator.h renderer/mixer.h renderer/osc.h program.h ramp.h wave.h math.h common.h
 	$(CC) -c $(CFLAGS_FAST) renderer/generator.c -o renderer/generator.o

saugns/saugns

 * 2021-07-12: Change more struct array [1] members to [].
 * 2021-07-24: Move down a little parser style changes.

Makefile
@@ -85,16 +85,16 @@ loader/loader.o: loader/loader.c sgensys.h script.h ptrlist.h program.h ramp.h w
 loader/file.o: loader/file.c loader/file.h common.h
 	$(CC) -c $(CFLAGS) loader/file.c -o loader/file.o
 
-loader/lexer.o: loader/lexer.c loader/lexer.h loader/file.h loader/symtab.h loader/scanner.h math.h common.h
+loader/lexer.o: loader/lexer.c loader/lexer.h loader/file.h loader/symtab.h mempool.h loader/scanner.h math.h common.h
 	$(CC) -c $(CFLAGS) loader/lexer.c -o loader/lexer.o
 
 loader/parseconv.o: loader/parseconv.c loader/parseconv.h program.h ramp.h wave.h math.h script.h ptrlist.h arrtype.h common.h
 	$(CC) -c $(CFLAGS) loader/parseconv.c -o loader/parseconv.o
 
-loader/parser.o: loader/parser.c loader/scanner.h loader/file.h loader/symtab.h script.h ptrlist.h program.h ramp.h wave.h math.h common.h
+loader/parser.o: loader/parser.c loader/scanner.h loader/file.h loader/symtab.h mempool.h script.h ptrlist.h program.h ramp.h wave.h math.h common.h
 	$(CC) -c $(CFLAGS) loader/parser.c -o loader/parser.o
 
-loader/scanner.o: loader/scanner.c loader/scanner.h loader/file.h loader/symtab.h math.h common.h
+loader/scanner.o: loader/scanner.c loader/scanner.h loader/file.h loader/symtab.h mempool.h math.h common.h
 	$(CC) -c $(CFLAGS) loader/scanner.c -o loader/scanner.o
 
 loader/symtab.o: loader/symtab.c loader/symtab.h mempool.h common.h