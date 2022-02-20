b71105d30c2db5f3af73fc27c094d34a935563d2
Joel K. Pettersson <joelkpettersson@gmail.com>
2022-02-20 16:36:25 +0100

    Add freqamp-PM syntax. Refactor & optimize oscillator.
    Language changes:
    * Add "p,f[...]" frequency-amplified PM modulator list syntax.
      Unlike for normal PM, the modulation index (amplitudes) are
      multiplied by the carrier frequency, scaled down so ~632 Hz
      gives normal amplitude.
    
    Refactor oscillator, split "phasor" type with new
    buffer-filling function, make the main run functions
    use passed pre-filled integer phase increase and offset
    buffers, instead of buffers for frequency and phase offset
    in floating-point. The splitting of the processing loop turns
    out to increase performance signficantly, in additon.
    
    Add ~632 Hz constant to math.h, used as a sensible
    middle-point in the frequency range (the geometric
    mean of 20 and 20000, common for audio equipment.)
    
    Change log:
     * 2022-02-20: Implement "p+f[]" syntax, refactor.
     * 2022-02-21: Update documentation.
     * 2022-02-22: Tweak scripts & use the new syntax:
           'examples/errorsignal.sgs'
           'examples/tests/vibrato-pm.sgs'  (new file)
           'devtests/music04.sgs'
     * 2022-02-23: Change naming in the documentation.
           (frequency-multiplied -> frequency-linked.)
           Adjust -p printout, use 3-letter mod abbrs.
     * 2022-06-14: Rename 'phasor' to 'freqor'  (as it
           provides non-integrated values). Remove env
           version of oscillator code, split out mixer
           functions for buffer handling including amp
           and use in generator.c - same output/result
           and also tests faster on my x86-64 laptops.
     * 2022-06-15: Simplify new generator.c code more.
     * 2022-07-11: Merge new syntax changes, fPM usage
           adjusted to "p,f[]" matching later version.
     * 2022-08-24: Simplify the oscillator more making
           a plain phasor out of the freqor. Removes a
           buffer from the generator code, works well.
     * 2022-10-06: Refactor wave module a bit with the
           X Macro pattern leaving LUTs as-is for now.
     * 2022-12-26: LGPL, not ISC, for generator/ code.
     * 2023-10-16: Tidy in generator.c (add a restrict
           to gen_bufs, mix_bufs; fabsf() vs. fabs()).

--- generator/osc.h
+++ generator/osc.h
@@ -2,17 +2,13 @@
  * Copyright (c) 2011, 2017-2022 Joel K. Pettersson
  * <joelkpettersson@gmail.com>.
  *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version, WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * View the file COPYING for details, or if missing, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 #pragma once
@@ -27,12 +23,29 @@
  */
 #define USE_PILUT 1
 
+/**
+ * Calculate the coefficent, based on the sample rate, used for
+ * the per-sample phase by multiplying with the frequency used.
+ */
+#define SGS_Phasor_COEFF(srate) (((float) UINT32_MAX)/(srate))
+
+typedef struct SGS_Phasor {
+	uint32_t phase;
+	float coeff;
+} SGS_Phasor;
+
+void SGS_Phasor_fill(SGS_Phasor *restrict o,
+		uint32_t *restrict phase_ui32,
+		size_t buf_len,
+		const float *restrict freq_f,
+		const float *restrict pm_f,
+		const float *restrict fpm_f);
+
 #define SGS_OSC_RESET_DIFF  (1<<0)
 #define SGS_OSC_RESET       ((1<<1) - 1)
 
 typedef struct SGS_Osc {
-	uint32_t phase;
-	float coeff;
+	SGS_Phasor phasor;
 	uint8_t wave;
 	uint8_t flags;
 #if USE_PILUT
@@ -43,33 +56,31 @@ typedef struct SGS_Osc {
 } SGS_Osc;
 
 /**
- * Calculate the coefficent, based on the sample rate,
- * used to give the per-sample phase increment
- * by multiplying with the frequency used.
- */
-#define SGS_Osc_COEFF(srate) (((float) UINT32_MAX)/(srate))
-
-/**
  * Initialize instance for use.
  */
 static inline void SGS_init_Osc(SGS_Osc *restrict o, uint32_t srate) {
 	*o = (SGS_Osc){
 #if USE_PILUT
-		.phase = SGS_Wave_picoeffs[SGS_WAVE_SIN].phase_adj,
+		.phasor = (SGS_Phasor){
+			.phase = SGS_Wave_picoeffs[SGS_WAVE_N_sin].phase_adj,
+			.coeff = SGS_Phasor_COEFF(srate),
+		},
 #else
-		.phase = 0,
+		.phasor = (SGS_Phasor){
+			.phase = 0,
+			.coeff = SGS_Phasor_COEFF(srate),
+		},
 #endif
-		.coeff = SGS_Osc_COEFF(srate),
-		.wave = SGS_WAVE_SIN,
+		.wave = SGS_WAVE_N_sin,
 		.flags = SGS_OSC_RESET,
 	};
 }
 
 static inline void SGS_Osc_set_phase(SGS_Osc *restrict o, uint32_t phase) {
 #if USE_PILUT
-	o->phase = phase + SGS_Wave_picoeffs[o->wave].phase_adj;
+	o->phasor.phase = phase + SGS_Wave_picoeffs[o->wave].phase_adj;
 #else
-	o->phase = phase;
+	o->phasor.phase = phase;
 #endif
 }
 
@@ -77,7 +88,7 @@ static inline void SGS_Osc_set_wave(SGS_Osc *restrict o, uint8_t wave) {
 #if USE_PILUT
 	int32_t old_offset = SGS_Wave_picoeffs[o->wave].phase_adj;
 	int32_t offset = SGS_Wave_picoeffs[wave].phase_adj;
-	o->phase += offset - old_offset;
+	o->phasor.phase += offset - old_offset;
 	o->wave = wave;
 	o->flags |= SGS_OSC_RESET_DIFF;
 #else
@@ -91,7 +102,7 @@ static inline void SGS_Osc_set_wave(SGS_Osc *restrict o, uint8_t wave) {
  * \return number of samples
  */
 static inline uint32_t SGS_Osc_cycle_len(SGS_Osc *restrict o, float freq) {
-	return lrintf(((float) UINT32_MAX) / (o->coeff * freq));
+	return lrintf(((float) UINT32_MAX) / (o->phasor.coeff * freq));
 }
 
 /**
@@ -101,7 +112,7 @@ static inline uint32_t SGS_Osc_cycle_len(SGS_Osc *restrict o, float freq) {
  */
 static inline uint32_t SGS_Osc_cycle_pos(SGS_Osc *restrict o,
 		float freq, uint32_t pos) {
-	uint32_t inc = lrintf(o->coeff * freq);
+	uint32_t inc = lrintf(o->phasor.coeff * freq);
 	uint32_t phs = inc * pos;
 	return phs / inc;
 }
@@ -113,20 +124,11 @@ static inline uint32_t SGS_Osc_cycle_pos(SGS_Osc *restrict o,
  */
 static inline int32_t SGS_Osc_cycle_offs(SGS_Osc *restrict o,
 		float freq, uint32_t pos) {
-	uint32_t inc = lrintf(o->coeff * freq);
+	uint32_t inc = lrintf(o->phasor.coeff * freq);
 	uint32_t phs = inc * pos;
 	return (phs - SGS_Wave_SLEN) / inc;
 }
 
 void SGS_Osc_run(SGS_Osc *restrict o,
 		float *restrict buf, size_t buf_len,
-		uint32_t layer,
-		const float *restrict freq,
-		const float *restrict amp,
-		const float *restrict pm_f);
-void SGS_Osc_run_env(SGS_Osc *restrict o,
-		float *restrict buf, size_t buf_len,
-		uint32_t layer,
-		const float *restrict freq,
-		const float *restrict amp,
-		const float *restrict pm_f);
+		const uint32_t *restrict phase_buf);
