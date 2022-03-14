c798d855520f42be265d05d3a978379f1502f2ff
Joel K. Pettersson <joelkpettersson@gmail.com>
2022-03-15 00:32:51 +0100

    test further with counter...

--- renderer/osc.c
+++ renderer/osc.c
@@ -137,7 +137,7 @@ static void SAU_Osc_reset(SAU_Osc *o) {
 
 #define SAU_FIBH32 2654435769UL // 32-bit Fibonacci hashing constant
 #define SAU_ROL32(x, r) \
-	((uint32_t)(x) << ((r) & 31) | ((uint32_t)(x) >> 32-((r) & 31)))
+	((uint32_t)(x) << ((r) & 31) | ((uint32_t)(x) >> (32-((r) & 31))))
 #define SAU_ROR32(x, r) \
 	((uint32_t)(x) >> ((r) & 31) | ((uint32_t)(x) << (32-((r) & 31))))
 static inline int32_t warp(uint32_t phase) {
@@ -171,7 +171,7 @@ void SAU_Osc_run(SAU_Osc *restrict o,
 			uint64_t nstate = (o->nstate += pinc_buf[i]) + s_pm;
 			uint32_t nval = nstate >> 32;
 			uint32_t phase = nstate;
-			s = warp(nval) * 1.f/INT32_MAX;
+			s = warp(o->testcount++) * 1.f/INT32_MAX;
 			s *= amp[i];
 			if (layer > 0) s += buf[i];
 			buf[i] = s;
@@ -182,7 +182,7 @@ void SAU_Osc_run(SAU_Osc *restrict o,
 			uint64_t nstate = (o->nstate += pinc_buf[i]);
 			uint32_t nval = nstate >> 32;
 			uint32_t phase = nstate;
-			s = warp(nval) * 1.f/INT32_MAX;
+			s = warp(o->testcount++) * 1.f/INT32_MAX;
 			s *= amp[i];
 			if (layer > 0) s += buf[i];
 			buf[i] = s;
@@ -265,7 +265,7 @@ void SAU_Osc_run_env(SAU_Osc *restrict o,
 			uint64_t nstate = (o->nstate += pinc_buf[i]) + s_pm;
 			uint32_t nval = nstate >> 32;
 			uint32_t phase = nstate;
-			s = warp(nval) * 1.f/INT32_MAX;
+			s = warp(o->testcount++) * 1.f/INT32_MAX;
 			float s_amp = amp[i] * 0.5f;
 			s = (s * s_amp) + fabs(s_amp);
 			if (layer > 0) s *= buf[i];
@@ -277,7 +277,7 @@ void SAU_Osc_run_env(SAU_Osc *restrict o,
 			uint64_t nstate = (o->nstate += pinc_buf[i]);
 			uint32_t nval = nstate >> 32;
 			uint32_t phase = nstate;
-			s = warp(nval) * 1.f/INT32_MAX;
+			s = warp(o->testcount++) * 1.f/INT32_MAX;
 			float s_amp = amp[i] * 0.5f;
 			s = (s * s_amp) + fabs(s_amp);
 			if (layer > 0) s *= buf[i];
