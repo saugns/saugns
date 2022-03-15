4ebd0409940d51feb98c4bedd4fe481406658020
Joel K. Pettersson <joelkpettersson@gmail.com>
2022-03-15 01:51:46 +0100

    add a xor-with-shifted in-between...

--- renderer/osc.c
+++ renderer/osc.c
@@ -140,9 +140,10 @@ static void SAU_Osc_reset(SAU_Osc *o) {
 	((uint32_t)(x) << ((r) & 31) | ((uint32_t)(x) >> (32-((r) & 31))))
 #define SAU_ROR32(x, r) \
 	((uint32_t)(x) >> ((r) & 31) | ((uint32_t)(x) << (32-((r) & 31))))
-static inline int32_t warp(uint32_t phase) {
+static inline int32_t noiseshape(uint32_t phase) {
 	uint32_t s = phase * SAU_FIBH32;
-	s *= SAU_ROR32(s, s + 21);
+	s ^= SAU_ROR32(s, 16);
+	s *= SAU_ROR32(s, s + 16);
 	return s;
 }
 
@@ -171,7 +172,7 @@ void SAU_Osc_run(SAU_Osc *restrict o,
 			uint64_t nstate = (o->nstate += pinc_buf[i]) + s_pm;
 			uint32_t nval = nstate >> 32;
 			uint32_t phase = nstate;
-			s = warp(o->testcount++) * 1.f/INT32_MAX;
+			s = noiseshape(o->testcount++) * 1.f/INT32_MAX;
 			s *= amp[i];
 			if (layer > 0) s += buf[i];
 			buf[i] = s;
@@ -182,7 +183,7 @@ void SAU_Osc_run(SAU_Osc *restrict o,
 			uint64_t nstate = (o->nstate += pinc_buf[i]);
 			uint32_t nval = nstate >> 32;
 			uint32_t phase = nstate;
-			s = warp(o->testcount++) * 1.f/INT32_MAX;
+			s = noiseshape(o->testcount++) * 1.f/INT32_MAX;
 			s *= amp[i];
 			if (layer > 0) s += buf[i];
 			buf[i] = s;
@@ -265,7 +266,7 @@ void SAU_Osc_run_env(SAU_Osc *restrict o,
 			uint64_t nstate = (o->nstate += pinc_buf[i]) + s_pm;
 			uint32_t nval = nstate >> 32;
 			uint32_t phase = nstate;
-			s = warp(o->testcount++) * 1.f/INT32_MAX;
+			s = noiseshape(o->testcount++) * 1.f/INT32_MAX;
 			float s_amp = amp[i] * 0.5f;
 			s = (s * s_amp) + fabs(s_amp);
 			if (layer > 0) s *= buf[i];
@@ -277,7 +278,7 @@ void SAU_Osc_run_env(SAU_Osc *restrict o,
 			uint64_t nstate = (o->nstate += pinc_buf[i]);
 			uint32_t nval = nstate >> 32;
 			uint32_t phase = nstate;
-			s = warp(o->testcount++) * 1.f/INT32_MAX;
+			s = noiseshape(o->testcount++) * 1.f/INT32_MAX;
 			float s_amp = amp[i] * 0.5f;
 			s = (s * s_amp) + fabs(s_amp);
 			if (layer > 0) s *= buf[i];
