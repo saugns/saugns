c7300886a47669600fd46ff5867d7f1e63d09323
Joel K. Pettersson <joelkpettersson@gmail.com>
2022-03-18 01:45:31 +0100

    ...

--- common.h
+++ common.h
@@ -87,7 +87,7 @@ int SAU_getopt(int argc, char *const*restrict argv,
 //#define SAU_SYMTAB_STATS 0
 
 /* Add int SAU_testopt and "-? <number>" cli option for it? */
-#define SAU_ADD_TESTOPT 1
+//#define SAU_ADD_TESTOPT 1
 
 /* Run scanner instead of lexer in 'test-scan' program. */
 #define SAU_TEST_SCANNER 0
