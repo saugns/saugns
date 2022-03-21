1d76be347cacb200ccc0d7dc4454738377c3bcc4
Joel K. Pettersson <joelkpettersson@gmail.com>
2022-03-21 08:23:06 +0100

    Higher-quality version.

--- math.h
+++ math.h
@@ -59,17 +59,19 @@ static inline double SAU_met(double x) {
 /**
  * Random access noise. Chaotic waveshaper which turns evenly spaced, and other
  * simple, number sequences into something similar to white noise. Returns zero
- * for zero. The frequency spectrum when used with a counter is flat. Only some
- * of the other properties of proper pseudo-random numbers also result, such as
- * fairness if used for dice throws and properties of sums of numbers. Can't be
- * used to generate things like properly distributed bit sequences, bit-by-bit.
+ * for zero. The frequency spectrum when used with a counter is flat. The third
+ * part is necessary in order to make the resulting pseudo-randomness passable,
+ * for more general non-cryptographic purposes. (Without it there is still e.g.
+ * fairness if used for dice throws and properties of sums of numbers are fine,
+ * but lower bits become so much worse than higher ones that the bits and bytes
+ * extracted from the output, if used as sequences of such, give poor results.)
  *
  * This function is mainly an alternative to using buffers of noise, for random
  * access. The index \p n can be used as a counter or varied for random access.
  *
  * \return pseudo-random number for index \p n
  */
-static inline int32_t SAU_ranoise(uint32_t n) {
+static inline int32_t SAU_ranoise32(uint32_t n) {
 	uint32_t s = n * SAU_FIBH32;
 	/*
 	 * 16 below appears a good offset number. For a high-quality result, it
@@ -78,6 +80,7 @@ static inline int32_t SAU_ranoise(uint32_t n) {
 	 * fails. Subtle audio qualities vary with the number; 16 seems smooth.
 	 */
 	s *= SAU_ROR32(s, s + 16);
+	s ^= (s >> 6) ^ (s >> 15); // improve worse lower bits with higher bits
 	return s;
 }
 
