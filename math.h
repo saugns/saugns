d35dc8525c9c91292f0a1e447b3a38db58c8b28e
Joel K. Pettersson <joelkpettersson@gmail.com>
2022-03-23 18:30:15 +0100

    Use new variation following further testing.

--- math.h
+++ math.h
@@ -74,12 +74,12 @@ static inline double SAU_met(double x) {
 static inline int32_t SAU_ranoise32(uint32_t n) {
 	uint32_t s = n * SAU_FIBH32;
 	/*
-	 * 16 below appears a good offset number. For a high-quality result, it
-	 * may be best to use a number in the range 8-23 inclusive. Statistical
+	 * 14 below appears a good offset number. For a high-quality result, it
+	 * may be best to use a number around 16 in 8-25 inclusive. Statistical
 	 * testing shows 5-27 as the maximal range beyond which Diehard Squeeze
 	 * fails. Subtle audio qualities vary with the number; 16 seems smooth.
 	 */
-	s *= SAU_ROR32(s, s + 16);
+	s *= SAU_ROR32(s, s + 14);
 	s ^= (s >> 6) ^ (s >> 15); // improve worse lower bits with higher bits
 	return s;
 }
