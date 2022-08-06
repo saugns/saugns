21b77770d8d515cf3a80a96d773c201d816438ae
Joel K. Pettersson <joelkpettersson@gmail.com>
2022-08-06 21:33:06 +0200

    Add num. variables, expand symtab. Change 'S' scope.
    Language changes:
     * Variable syntax (formerly called "Label").
       - Add "'name=..." variation of the syntax for
         variable assignment, for assigning a number.
         Variables are now dynamically typed.
       - Add "$name" expression, for using a variable
         in a numerical expression.
     * Script options.
       - Make the "S" options lexically scoped,
         restoring the older values from outside
         a "[]" list when the list is exited.
       - Apply the "S a" value set inside a "[]"
         list to any modulators which follow at the
         same list level. Previously it only applied
         to top-level carriers.
    
    The variable extension is backward-compatible.
    Also remove the restriction on assigning an
    object reference to an object reference.
    
    Add a second layer of data to the symbol table
    beyond the string hash table. Further, planned
    features will extend it for lexical scope.
    
    Change numerical expression parsing to always
    return double precision numbers, so variables
    can also use that. Convert to single when such
    fields are assigned.
    
    Except for a small performance boost, the mempool has been
    much the same since 2014, until now. Change to use dynamic
    sizing of memory blocks, double the size after every power
    of two block count reached. Use 512 as default start size.
    Performance seems basically unchanged running my old test.
    (Changes probably cancelled out as fewer blocks are made.)
    
    Change log:
     * 2021-01-10: Flatten types in mempool. Refactor to place
           new allocation pointer array for freeing separately
           and change sizing per block, inceasing it once each
           new power of two number of blocks has been reached.
     * 2021-01-12: Tweak mempool. Adjust mempool heuristic for
           skipping fully-used blocks to increase threshold if
           proportion below 2/3 of blocks and increase default
           block start size from 128 to 512. Simplify away the
           extra pointer per block and separate array, growing
           allocations downward instead.
     * 2022-07-28: Symtab changes, using arrtype.
     * 2022-08-06: Remove unsafe array item use, a
           pointer returned will now remain valid.
           Add numerical variables feature.
     * 2022-08-11: Move down to sgensys.
     * 2022-08-21: Make 'S' usage lexically scoped.
     * 2022-11-17: More changes to mempool.
           Style change in mempool;
           shorter mempool method names.
           Add mempool mpregdtor method;
           use mpregdtor in symtab.
     * 2023-10-17: Tidy in mempool. In symtab use uint8_t,
           not char (for unsigned arithmetic for consistency).
     * 2024-06-01: Clean up mempool binary search code, making
           it foolproof (overflows), though it worked as used.

--- symtab.h
+++ symtab.h
@@ -1,5 +1,5 @@
 /* sgensys: Symbol table module.
- * Copyright (c) 2011-2012, 2014, 2017-2020 Joel K. Pettersson
+ * Copyright (c) 2011-2012, 2014, 2017-2023 Joel K. Pettersson
  * <joelkpettersson@gmail.com>.
  *
  * This file and the software of which it is part is distributed under the
@@ -16,35 +16,51 @@
 #include "mempool.h"
 
 /**
- * Item stored for each unique string associated with the symbol table.
+ * Node stored for each unique string associated with the symbol table.
  */
 typedef struct SGS_Symstr {
 	struct SGS_Symstr *prev;
-	void *data;
-	size_t key_len;
-	char key[];
+	struct SGS_Symitem *item; // the last item with this string
+	uint32_t key_len;
+	uint8_t key[];
 } SGS_Symstr;
+
+/** Data type used in a symbol item. */
+enum {
+	SGS_SYM_DATA_NONE = 0,
+	SGS_SYM_DATA_ID,
+	SGS_SYM_DATA_NUM,
+	SGS_SYM_DATA_OBJ,
+};
+
+/**
+ * Item with type, string, and data.
+ */
+typedef struct SGS_Symitem {
+	uint32_t sym_type;
+	uint32_t data_use;
+	struct SGS_Symitem *prev; // the previous item with this string
+	SGS_Symstr *sstr;
+	union {
+		uint32_t id;
+		double num;
+		void *obj;
+	} data;
+} SGS_Symitem;
 
 struct SGS_Symtab;
 typedef struct SGS_Symtab SGS_Symtab;
 
 SGS_Symtab *SGS_create_Symtab(SGS_Mempool *restrict mempool) sgsMalloclike;
-void SGS_destroy_Symtab(SGS_Symtab *restrict o);
 
 SGS_Symstr *SGS_Symtab_get_symstr(SGS_Symtab *restrict o,
 		const void *restrict str, size_t len);
 
-/**
- * Get the unique copy of \p str held in the symbol table,
- * adding \p str to the string pool unless already present.
- *
- * \return unique copy of \p str, or NULL on allocation failure
- */
-static inline const void *SGS_Symtab_pool_str(SGS_Symtab *restrict o,
-		const void *restrict str, size_t len) {
-	SGS_Symstr *item = SGS_Symtab_get_symstr(o, str, len);
-	return (item != NULL) ? item->key : NULL;
-}
-const char **SGS_Symtab_pool_stra(SGS_Symtab *restrict o,
-		const char *const* restrict stra,
-		size_t n);
+SGS_Symitem *SGS_Symtab_add_item(SGS_Symtab *restrict o,
+		SGS_Symstr *restrict symstr, uint32_t type_id);
+SGS_Symitem *SGS_Symtab_find_item(SGS_Symtab *restrict o,
+		SGS_Symstr *restrict symstr, uint32_t type_id);
+
+bool SGS_Symtab_add_stra(SGS_Symtab *restrict o,
+		const char *const*restrict stra, size_t n,
+		uint32_t type_id);
