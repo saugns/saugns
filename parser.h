52923472e989d54ea0cc82a533888525c1a7830b
Joel K. Pettersson <joelkpettersson@gmail.com>
2022-08-22 16:01:38 +0200

    Further syntax changes, minor feature additions.
    Script language changes & feature additions:
     * AM/FM modulation syntax:
       Change prefix for second value and/or list
       from "!" to ",w" ('w' short for "wave envelope").
     * "S" options:
       Rename "n" to "f,n".
     * Label referencing after defining:
       From ":label" to "@label".
     * Multiple operator binding (unimplemented):
       From ":[...]" to "@[...]".
     * Numerical expressions:
       Allow shorthand multiplying (leaving out a "*")
       after closing ")", not only before opening "(".
    
    Feature changes & additions:
     * Disable multiplicative inverse for 'r' frequency ratio,
       i.e. use the value as is, not 1 / value. Describe it in
       terms of "relative frequency". It's more intuitive with
       n:m carrier-modulator frequency ratios as (m/n) values,
       than as (n/m) values, when actually adjusting them.
       (For multiplicatively inverse values, higher values
       meant a lower frequency; now, higher means higher.)
       This reverses a change from 2011-03-04.
     * Add downscaling of output amplitude by voice count
       by default. Using "S a" disables it.
     * Allow 'a' (amplitude) parameter for AM and FM modulators.
       (Previously disallowed within "a![]" and "f![]"/"r![]".)
    
    Default values:
     * Frequency ("O f", "S n"): From 444 to 440.
    
    Wave type changes:
     * Make 'saw' (saw wave) increasing instead of decreasing.
       (Negative frequency or amplitude gives a decreasing saw.)
    
    In parser result, include script options and name.
    
    Change log:
     * 2018-04-18: Implement downscaling of voice
           amplitude by voice count by default.
     * 2018-10-17: Make "saw" wave increasing
           instead of decreasing.
     * 2018-10-18: Enable 'a' parameter for
           "f!<>"/"r!<>" and "a!<>".
     * 2019-06-05: Merge syntax changes.
           Increase version to v0.3.1.
           Clean up a few scripts.
     * 2019-06-13: Set version to v0.2.4.
     * 2019-06-16: Backport further simple syntax changes.
       Old branch:
        - 2019-02-08: Fix inconsistent indentation
              in a few scripts.
        - 2019-06-13: Three simple syntax changes.
              Label ref: from ":label" to "@label".
              Subvalues: from within "[...]" to "{...}".
              Mul. op.b: from within "{...}" to "@[...]".
     * 2020-07-13: Keep version at v0.2-beta.
     * 2022-01-10: Merge down 'r' syntax change.
       Old branch:
        - 2019-07-04: v0.3.1-beta: Begin syntax tweaks.
              Disable multiplicative inverse for 'r'.
              Update scripts and README.
     * 2022-07-11: Merge changes as of the earlier commits.
           The following changes are now obsoleted/removed:
       Old branch:
        - 2018-10-23: Change PM list syntax from "p*<>" to "p+<>".
        - 2021-06-26: Check for stray '{'s & '['s.
     * 2022-07-12: Change "O f" and "S n" default to 440.
           Backport change to allow shorthand multiplying
           after ")", not only before "(".
       Old branch:
        - 2022-06-24: Extend num_r() function.
        - 2022-06-27: Fix new bugs in num_r().
              Add a new test to 'numexpr.sau'.
     * 2022-08-13: Add 'examples/tests/pm_waveshapes.sgs'.
     * 2022-08-22: Split out the above from the
           huge "General clean-up" change. Move down and use
           the SGS_ms_in_samples() function (2022-05-07).
     * 2022-08-24: Syntax: from "!" to ",w", and "n" to "f,n";
           more like later versions.

--- parser.h
+++ parser.h
@@ -75,8 +75,38 @@ typedef struct SGSEventNode {
   SGSPtrList graph;
 } SGSEventNode;
 
+/**
+ * Script data option flags.
+ *
+ * Set after parsing the setting of script options in a script.
+ */
+enum {
+  SGS_SOPT_AMPMULT = 1<<0,
+  SGS_SOPT_A4_FREQ = 1<<1,
+  SGS_SOPT_DEF_TIME = 1<<2,
+  SGS_SOPT_DEF_FREQ = 1<<3,
+  SGS_SOPT_DEF_RATIO = 1<<4,
+};
+
+/**
+ * Options set for a script, affecting parsing.
+ *
+ * The final state is included in the parse result.
+ */
+typedef struct SGSScriptOptions {
+  uint32_t set;  // flags (SGS_SOPT_*) set upon change by script
+  float ampmult; // amplitude multiplier for non-modulator operators
+  float A4_freq; // A4 tuning for frequency as note
+  /* operator parameter default values (use depends on context) */
+  int32_t def_time_ms;
+  float def_freq,
+        def_ratio;
+} SGSScriptOptions;
+
 typedef struct SGSParserResult {
   SGSEventNode *events;
+  const char *name; // currently simply set to the filename
+  SGSScriptOptions sopt;
 } SGSParserResult;
 
 SGSParserResult *SGSParser_parse(const char *filename);
