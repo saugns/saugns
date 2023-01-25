6d660eda48ceeaa5d4f8743d0409b54098490d52
Joel K. Pettersson <joelkpettersson@gmail.com>
2023-01-25 03:48:50 +0100

    Renaming in code, document libsau under sau/ better.
    Rename functions, types, etc. Mainly using...
     * 's/SAU_([a-z])/sau_\1/g'
     * 's/SAU_([A-Z][a-z])/sau\1/g'
    
    Move 'doc/' into 'sau/'.
    Add 'sau/README.md'.
    Split 'COPYING.LESSER' from 'COPYING',
    add these files under 'sau/' too.
    
    Also change my email address,
    to my new main email address.
    
    Set saugns version to v0.4-dev.
    
    Updated 2023-03-14 to clean up licensing info,
    and use ISC for all files outside 'sau/' while
    at it. (All outside libsau is only cli stuff.)
    
    Updated 2024-02-29, 'sau/doc/README.SAU' text.

--- sau/scanner.h
+++ sau/scanner.h
@@ -1,13 +1,13 @@
 /* SAU library: Script scanner module.
- * Copyright (c) 2014, 2017-2022 Joel K. Pettersson
- * <joelkpettersson@gmail.com>.
+ * Copyright (c) 2014, 2017-2023 Joel K. Pettersson
+ * <joelkp@tuta.io>.
  *
  * This file and the software of which it is part is distributed under the
  * terms of the GNU Lesser General Public License, either version 3 or (at
  * your option) any later version, WITHOUT ANY WARRANTY, not even of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * View the file COPYING for details, or if missing, see
+ * View the files COPYING.LESSER and COPYING for details, or if missing, see
  * <https://www.gnu.org/licenses/>.
  */
 
@@ -15,14 +15,14 @@
 #include "file.h"
 #include "symtab.h"
 
-struct SAU_Scanner;
-typedef struct SAU_Scanner SAU_Scanner;
+struct sauScanner;
+typedef struct sauScanner sauScanner;
 
 /**
  * Number of values for which character filters are defined.
  *
  * Values below this are given their own function pointer;
- * SAU_Scanner_get_filter() handles mapping of other values.
+ * sauScanner_get_filter() handles mapping of other values.
  */
 #define SAU_SCAN_FILTER_COUNT 128
 
@@ -47,7 +47,7 @@ typedef struct SAU_Scanner SAU_Scanner;
  * Filter functions may call other filter functions,
  * and are allowed (not done by default functions) to alter the table.
  */
-typedef uint8_t (*SAU_ScanFilter_f)(SAU_Scanner *o, uint8_t c);
+typedef uint8_t (*sauScanFilter_f)(sauScanner *o, uint8_t c);
 
 /**
  * Special character values.
@@ -55,13 +55,13 @@ typedef uint8_t (*SAU_ScanFilter_f)(SAU_Scanner *o, uint8_t c);
 enum {
 	/**
 	 * Returned for spaces and tabs after filtering.
-	 * Also used for comparison with SAU_Scanner_tryc().
+	 * Also used for comparison with sauScanner_tryc().
 	 */
 	SAU_SCAN_SPACE = ' ',
 	/**
 	 * Returned for linebreaks after filtering.
-	 * Also used for comparison with SAU_Scanner_tryc()
-	 * and SAU_Scanner_tryc_nospace().
+	 * Also used for comparison with sauScanner_tryc()
+	 * and sauScanner_tryc_nospace().
 	 */
 	SAU_SCAN_LNBRK = '\n',
 	/**
@@ -82,11 +82,11 @@ enum {
 	SAU_SCAN_C_LNBRK_POSUP = 1<<3, // pending position update for linebreak
 };
 
-extern const SAU_ScanFilter_f SAU_Scanner_def_filters[SAU_SCAN_FILTER_COUNT];
+extern const sauScanFilter_f sauScanner_def_filters[SAU_SCAN_FILTER_COUNT];
 
 /**
  * Whitespace filtering levels, used with
- * SAU_Scanner_setws_level() to assign standard filter functions.
+ * sauScanner_setws_level() to assign standard filter functions.
  *
  * Default is SAU_SCAN_WS_ALL. Note that other filter functions,
  * e.g. comment filters, must filter the whitespace markers they
@@ -98,17 +98,17 @@ enum {
 	SAU_SCAN_WS_NONE,    // remove all ws characters
 };
 
-uint8_t SAU_Scanner_filter_invalid(SAU_Scanner *restrict o, uint8_t c);
-uint8_t SAU_Scanner_filter_space_keep(SAU_Scanner *restrict o, uint8_t c);
-uint8_t SAU_Scanner_filter_linebreak_keep(SAU_Scanner *restrict o, uint8_t c);
-uint8_t SAU_Scanner_filter_ws_none(SAU_Scanner *restrict o, uint8_t c);
-uint8_t SAU_Scanner_filter_linecomment(SAU_Scanner *restrict o, uint8_t c);
-uint8_t SAU_Scanner_filter_blockcomment(SAU_Scanner *restrict o,
+uint8_t sauScanner_filter_invalid(sauScanner *restrict o, uint8_t c);
+uint8_t sauScanner_filter_space_keep(sauScanner *restrict o, uint8_t c);
+uint8_t sauScanner_filter_linebreak_keep(sauScanner *restrict o, uint8_t c);
+uint8_t sauScanner_filter_ws_none(sauScanner *restrict o, uint8_t c);
+uint8_t sauScanner_filter_linecomment(sauScanner *restrict o, uint8_t c);
+uint8_t sauScanner_filter_blockcomment(sauScanner *restrict o,
 		uint8_t check_c);
-uint8_t SAU_Scanner_filter_slashcomments(SAU_Scanner *restrict o, uint8_t c);
-uint8_t SAU_Scanner_filter_char1comments(SAU_Scanner *restrict o, uint8_t c);
+uint8_t sauScanner_filter_slashcomments(sauScanner *restrict o, uint8_t c);
+uint8_t sauScanner_filter_char1comments(sauScanner *restrict o, uint8_t c);
 
-uint8_t SAU_Scanner_setws_level(SAU_Scanner *restrict o, uint8_t ws_level);
+uint8_t sauScanner_setws_level(sauScanner *restrict o, uint8_t ws_level);
 
 /**
  * Scanner state flags for current file.
@@ -122,37 +122,37 @@ enum {
 /**
  * Scan frame with character-level information for a get.
  */
-typedef struct SAU_ScanFrame {
+typedef struct sauScanFrame {
 	int32_t line_num;
 	int32_t char_num;
 	uint8_t c;
 	uint8_t c_flags;
-} SAU_ScanFrame;
+} sauScanFrame;
 
 /**
  * Scanner type.
  */
-struct SAU_Scanner {
-	SAU_File *f;
-	SAU_Symtab *symtab;
-	SAU_ScanFilter_f *filters; // copy of SAU_Scanner_def_filters
-	SAU_ScanFrame sf;
+struct sauScanner {
+	sauFile *f;
+	sauSymtab *symtab;
+	sauScanFilter_f *filters; // copy of sauScanner_def_filters
+	sauScanFrame sf;
 	uint8_t undo_pos;
 	uint8_t undo_ungets;
 	uint8_t s_flags;
 	uint8_t match_c; // for use by character filters
-	uint8_t ws_level; // level of SAU_Scanner_setws_level(), presuming use
+	uint8_t ws_level; // level of sauScanner_setws_level(), presuming use
 	uint8_t *strbuf;
 	void *data; // for use by user
-	SAU_ScanFrame undo[SAU_SCAN_UNGET_MAX + 1];
+	sauScanFrame undo[SAU_SCAN_UNGET_MAX + 1];
 };
 
-SAU_Scanner *SAU_create_Scanner(SAU_Symtab *restrict symtab) sauMalloclike;
-void SAU_destroy_Scanner(SAU_Scanner *restrict o);
+sauScanner *sau_create_Scanner(sauSymtab *restrict symtab) sauMalloclike;
+void sau_destroy_Scanner(sauScanner *restrict o);
 
-bool SAU_Scanner_open(SAU_Scanner *restrict o,
+bool sauScanner_open(sauScanner *restrict o,
 		const char *restrict script, bool is_path);
-void SAU_Scanner_close(SAU_Scanner *restrict o);
+void sauScanner_close(sauScanner *restrict o);
 
 /**
  * Get character filter to call for character \p c,
@@ -161,9 +161,9 @@ void SAU_Scanner_close(SAU_Scanner *restrict o);
  * Values below SAU_SCAN_FILTER_COUNT are assigned the filter for
  * the raw value; other values are assigned the filter for '\0'.
  *
- * \return SAU_ScanFilter_f or NULL
+ * \return sauScanFilter_f or NULL
  */
-static inline SAU_ScanFilter_f SAU_Scanner_getfilter(SAU_Scanner *restrict o,
+static inline sauScanFilter_f sauScanner_getfilter(sauScanner *restrict o,
 		uint8_t c) {
 	if (c >= SAU_SCAN_FILTER_COUNT) c = 0;
 	return o->filters[c];
@@ -175,9 +175,9 @@ static inline SAU_ScanFilter_f SAU_Scanner_getfilter(SAU_Scanner *restrict o,
  *
  * \return result
  */
-static inline uint8_t SAU_Scanner_usefilter(SAU_Scanner *restrict o,
+static inline uint8_t sauScanner_usefilter(sauScanner *restrict o,
 		uint8_t c, uint8_t match_c) {
-	SAU_ScanFilter_f f = SAU_Scanner_getfilter(o, c);
+	sauScanFilter_f f = sauScanner_getfilter(o, c);
 	if (f != NULL) {
 		o->match_c = match_c;
 		return f(o, c);
@@ -186,32 +186,32 @@ static inline uint8_t SAU_Scanner_usefilter(SAU_Scanner *restrict o,
 }
 
 /**
- * Callback type allowing reading of named constants using SAU_Scanner_getd().
+ * Callback type allowing reading of named constants using sauScanner_getd().
  * Should return non-zero length if number read and \p var set.
  */
-typedef size_t (*SAU_ScanNumConst_f)(SAU_Scanner *restrict o,
+typedef size_t (*sauScanNumConst_f)(sauScanner *restrict o,
 		double *restrict var);
 
-uint8_t SAU_Scanner_retc(SAU_Scanner *restrict o);
-uint8_t SAU_Scanner_getc(SAU_Scanner *restrict o);
-uint8_t SAU_Scanner_filterc(SAU_Scanner *restrict o, uint8_t c,
-		SAU_ScanFilter_f filter_f);
-bool SAU_Scanner_tryc(SAU_Scanner *restrict o, uint8_t testc);
-uint32_t SAU_Scanner_ungetc(SAU_Scanner *restrict o);
-bool SAU_Scanner_geti(SAU_Scanner *restrict o,
+uint8_t sauScanner_retc(sauScanner *restrict o);
+uint8_t sauScanner_getc(sauScanner *restrict o);
+uint8_t sauScanner_filterc(sauScanner *restrict o, uint8_t c,
+		sauScanFilter_f filter_f);
+bool sauScanner_tryc(sauScanner *restrict o, uint8_t testc);
+uint32_t sauScanner_ungetc(sauScanner *restrict o);
+bool sauScanner_geti(sauScanner *restrict o,
 		int32_t *restrict var, bool allow_sign,
 		size_t *restrict str_len);
-bool SAU_Scanner_getd(SAU_Scanner *restrict o,
+bool sauScanner_getd(sauScanner *restrict o,
 		double *restrict var, bool allow_sign,
 		size_t *restrict str_len,
-		SAU_ScanNumConst_f numconst_f);
-bool SAU_Scanner_get_symstr(SAU_Scanner *restrict o,
-		SAU_Symstr **restrict symstrp);
-uint8_t SAU_Scanner_skipws(SAU_Scanner *restrict o);
+		sauScanNumConst_f numconst_f);
+bool sauScanner_get_symstr(sauScanner *restrict o,
+		sauSymstr **restrict symstrp);
+uint8_t sauScanner_skipws(sauScanner *restrict o);
 
-void SAU_Scanner_warning(const SAU_Scanner *restrict o,
-		const SAU_ScanFrame *restrict sf,
+void sauScanner_warning(const sauScanner *restrict o,
+		const sauScanFrame *restrict sf,
 		const char *restrict fmt, ...) sauPrintflike(3, 4);
-void SAU_Scanner_error(SAU_Scanner *restrict o,
-		const SAU_ScanFrame *restrict sf,
+void sauScanner_error(sauScanner *restrict o,
+		const sauScanFrame *restrict sf,
 		const char *restrict fmt, ...) sauPrintflike(3, 4);
