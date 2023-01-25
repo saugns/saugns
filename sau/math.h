cf8319a98f9b64d5f7eddc15e996a93819b4f1f1
Joel K. Pettersson <joelkp@tuta.io>
2023-01-25 06:42:59 +0100

    Add 'R' random segments oscillator, redesign for > 1 types.
    Language changes:
     * Add generator type 'R' (Random segments oscillator).
       It shares most parameters with type 'O'.
    
    Import 'raseg.h' from 2023-01 "mgensys"
    and additional code needed to build it,
    including renaming and extending the
    ramp module to line, adding map_*().
    Rename it 'rasg.h', type to sauRasG;
    refactor, otherwise tidy added code.
    
    Split into 2 commits for the successive
    design stages kept, of old 'R' work.
    
    Make & set additional 32-bit seed when
    'seed()' is used in a script to seed.
    Add sau_rand32() wrapper for generating a
    next 32-bit integer from the 32-bit seed,
    used when an 'R' instance is created,
    to set its cycle.
    
    Add extra fields to existing structs to
    include data for 'R' instances in "op" nodes,
    as a placeholder for a fuller OO redesign.
    
    Adjust generator.c design more thoroughly,
    dividing gen node types. Make the main type
    a union type including each specialized type.
    This makes the diff smaller and allows simple
    code which could allow generators to change
    types (reusing allocations) in the future.
    
    Update and tidy in 'doc/README.SAU'.
    
    Add and update some scripts using 'R'.
     - Rename files in 'examples/tests/'.
     - Add 'examples/tests/tone_seq-v6_Rexp.sau'.
     - Update 'examples/tests/rand-pm-ratio.sau'.
     - Add 'examples/sounds/Rcos-rm_voicelike.sau'.
     - Add 'examples/sounds/cat-purr.sau'.
    
    Change log:
     * 2023-01-25: Integrate 'R' generator into saugns,
           based on code in 2023-01 "mgensys". Adjust
           design minimally, tidy up a little. Adjust
           and add some new scripts. Part 1 of 2 parts.
       Old branch:
        - 2022-04-04: Add new minimal versions of
              my ranoise32() PRNG to math.h.
        - 2022-12-03: Add Raseg, add line map_*()
              functions -- all line types work.
        - 2023-01-02: Add 'R' parameter 'm' (mode),
              new option 'f' (fixed, naive oscillator).
        - 2023-01-03: Add 'm' mode option 's' (smooth),
              't' (ternary), 'b' (binary). Give 's' a
              smoothing level of 3.
        - 2023-01-04: Add 'm' mode level digit,
              making 'b' and 't' use it, with a
              less-shaped (more wiggly result).
              Randomize 'R' start cycle (seed).
     * 2023-01-30: Make parser proceed with 'cos'
           on invalid (or missing) 'R' line type.
           Don't warn if there's no label at all.
     * 2024-02-29: Naming: call 'R' an oscillator not
           a generator as it has frequency and phase.
           Split 'R' importing commit into 2 parts to
           keep its earlier design at hand as well.
           Adjust math.h, import older functions...
     * 2024-04-03: Update man page to mention 'R'.

--- sau/math.h
+++ sau/math.h
@@ -1,5 +1,5 @@
 /* SAU library: Math definitions.
- * Copyright (c) 2011-2012, 2017-2023 Joel K. Pettersson
+ * Copyright (c) 2011-2012, 2017-2024 Joel K. Pettersson
  * <joelkp@tuta.io>.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
@@ -26,6 +26,12 @@
 #define SAU_HUMMID    632.45553203367586639978 // human hearing range geom.mean
 #define SAU_GLDA        2.39996322972865332223 // golden angle 2*PI*(2.0 - phi)
 #define SAU_GLDA_1_2PI  0.38196601125010515180 // (in cycle %) 2.0 - phi
+#define SAU_FIBH32      0x9e3779b9             // 32-bit Fibonacci hash constant
+#define SAU_FIBH64      0x9e3779b97f4a7c15     // 64-bit Fibonacci hash constant
+
+/*
+ * Format conversion & general purpose functions.
+ */
 
 /**
  * Convert time in ms to time in samples for a sample rate.
@@ -77,6 +83,31 @@ static inline double sau_d01_from_ui64(uint64_t x) {
 	return (x >> 11) * 0x1.0p-53;
 }
 
+/** \return +1 if \p n is even, -1 if it's odd. */
+static inline int sau_oddness_as_sign(int n) {
+	return (1 - ((n & 1) * 2));
+}
+
+/** Portable 32-bit arithmetic right shift. */
+static inline int32_t sau_sar32(int32_t x, int s) {
+	return x < 0 ? ~(~x >> s) : x >> s;
+}
+
+/** 32-bit right rotation. */
+static inline uint32_t sau_ror32(uint32_t x, int r) {
+	return x >> r | x << (32 - r);
+}
+
+/** Multiplicatively mix bits using varying right-rotation,
+    for 32-bit unsigned \p x value, \p r rotation, \p ro rotation offset. */
+static inline uint32_t sau_muvaror32(uint32_t x, int r, int ro) {
+	return (x | (1U << (ro & 31)) | 1U) * sau_ror32(x, r + ro);
+}
+
+/*
+ * Math functions for use in SAU scripts.
+ */
+
 /**
  * Metallic value function. Golden ratio for \p x == 1, silver for x == 2, etc.
  * Also accepts zero (with the result one), and values in-between the integers.
@@ -122,7 +153,8 @@ struct sauMath_state;
 
 /** State for math functions for each parsing and interpretation unit. */
 struct sauMath_state {
-	uint64_t seed;
+	uint64_t seed64;
+	uint32_t seed32;
 	bool no_time;
 };
 
@@ -151,17 +183,157 @@ extern const uint8_t sauMath_params[SAU_MATH_NAMED];
 extern const union sauMath_sym_f sauMath_symbols[SAU_MATH_NAMED];
 
 /*
- * Simple PRNGs
+ * Simple PRNGs.
  */
+
+/**
+ * Random access noise, minimal lower-quality version. Chaotic waveshaper which
+ * turns sawtooth-ish number sequences into white noise. Returns zero for zero.
+ *
+ * This function is mainly an alternative to using buffers of noise, for random
+ * access. The index \p n can be used as a counter or varied for random access.
+ *
+ * \return pseudo-random number for index \p n
+ */
+static inline int32_t sau_ranoise32(uint32_t n) {
+	uint32_t s = n * SAU_FIBH32;
+	s = sau_muvaror32(s, s >> 27, 0);
+	return s;
+}
+
+/**
+ * Random access noise, minimal lower-quality version. Chaotic waveshaper which
+ * turns sawtooth-ish number sequences into white noise. Returns zero for zero.
+ *
+ * This "next" function returns a new value each time, corresponding to a state
+ * \p pos, which is increased. It may be initialized with any seed (0 is fine).
+ *
+ * \return pseudo-random number for state \p pos
+ */
+static inline int32_t sau_ranoise32_next(uint32_t *restrict pos) {
+	uint32_t s = *pos += SAU_FIBH32;
+	s = sau_muvaror32(s, s >> 27, 0);
+	return s;
+}
+
+/**
+ * Random access noise, smoother more LCG-ish version. Chaotic waveshaper which
+ * turns sawtooth-ish number sequences into white noise. Returns zero for zero.
+ * Lower bits have very poor-quality randomness, but the whole sounds 'smooth'.
+ *
+ * This function is mainly an alternative to using buffers of noise, for random
+ * access. The index \p n can be used as a counter or varied for random access.
+ *
+ * \return pseudo-random number for index \p n
+ */
+static inline int32_t sau_ransmooth32(uint32_t n) {
+	uint32_t s = n * SAU_FIBH32;
+	s *= sau_ror32(s, s >> 27);
+	return s;
+}
+
+/**
+ * Random access noise, smoother more LCG-ish version. Chaotic waveshaper which
+ * turns sawtooth-ish number sequences into white noise. Returns zero for zero.
+ * Lower bits have very poor-quality randomness, but the whole sounds 'smooth'.
+ *
+ * This function is mainly an alternative to using buffers of noise, for random
+ * access. The index \p n can be used as a counter or varied for random access.
+ *
+ * \return pseudo-random number for index \p n
+ */
+static inline int32_t sau_ransmooth32_next(uint32_t *restrict pos) {
+	uint32_t s = *pos += SAU_FIBH32;
+	s *= sau_ror32(s, s >> 27);
+	return s;
+}
+
+/**
+ * Random access noise, simpler binary output version. Chaotic waveshaper which
+ * turns sawtooth-ish number sequences into white noise. Returns zero for zero.
+ *
+ * This function is mainly an alternative to using buffers of noise, for random
+ * access. The index \p n can be used as a counter or varied for random access.
+ *
+ * \return pseudo-random 1 or 0 for index \p n
+ */
+static inline bool sau_ranbit32(uint32_t n) {
+	uint32_t s = n * SAU_FIBH32;
+	s *= sau_ror32(s, s >> 27);
+	return ((int32_t)s) < 0;
+}
+
+/**
+ * Random access noise, simpler binary output version. Chaotic waveshaper which
+ * turns sawtooth-ish number sequences into white noise. Returns zero for zero.
+ *
+ * This function is mainly an alternative to using buffers of noise, for random
+ * access. The index \p n can be used as a counter or varied for random access.
+ *
+ * \return pseudo-random 1 or 0 for index \p n
+ */
+static inline bool sau_ranbit32_next(uint32_t *restrict pos) {
+	uint32_t s = *pos += SAU_FIBH32;
+	s *= sau_ror32(s, s >> 27);
+	return ((int32_t)s) < 0;
+}
+
+/** Initial seed for mgs_xorshift32(). Other non-zero values can be used. */
+#define SAU_XORSHIFT32_SEED 2463534242UL
+
+/**
+ * Get Marsaglia xorshift32 state from non-zero \p seed.
+ */
+static inline uint32_t sau_xorshift32(uint32_t seed) {
+	uint32_t x = seed;
+	x ^= x << 13;
+	x ^= x >> 17;
+	x ^= x << 5;
+	return x;
+}
 
 /**
  * Fixed-increment SplitMix64, based on C version provided by Vigna.
  *
- * \return next pseudo-random value
+ * \return next pseudo-random number for state \p pos
  */
 static inline uint64_t sau_splitmix64_next(uint64_t *restrict pos) {
-	uint64_t z = (*pos += 0x9e3779b97f4a7c15);
+	uint64_t z = (*pos += SAU_FIBH64);
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
 	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
 	return z ^ (z >> 31);
+}
+
+/**
+ * Standard 32-bit PRNG for use together with math functions for SAU scripts.
+ */
+static inline uint32_t sau_rand32(struct sauMath_state *restrict o) {
+	if (o->seed32 == 0) o->seed32 = SAU_XORSHIFT32_SEED;
+	return (o->seed32 = sau_xorshift32(o->seed32));
+}
+
+/*
+ * Fast approximations.
+ */
+
+/**
+ * Degree 5 sin(PI * x) approximation function for limited input range.
+ *
+ * For \p x domain -0.5 <= x <= +0.5; use with pre-wrapped values only.
+ *
+ * Almost clean spectrum, adds a 5th harmonic at slightly below -84 dB.
+ */
+static inline float sau_sinpi_d5f(float x) {
+	/*
+	 * Coefficients generated for no end-point error,
+	 * on top of minimax, roughly doubling the error.
+	 * Slightly lower max error than Taylor degree 7.
+	 */
+	const float scale[] = {
+		+3.14042741234069229463,
+		-5.13655757476162831091,
+		+2.29939170159543653372,
+	};
+	float x2 = x*x;
+	return x*(scale[0] + x2*(scale[1] + x2*scale[2]));
 }
