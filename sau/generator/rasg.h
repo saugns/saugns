a41ce1602446dc2bcca4175efe322a094e0d2052
Joel K. Pettersson <joelkp@tuta.io>
2023-01-26 01:18:28 +0100

    Redesign 'R' and line map functions, add more modes.
    Language changes:
     * Add 'R' 'm' function 'g' (Gaussian),
       remove 's' (smooth) fulfilled by 't'.
       Use level digit for function 'f' too.
     * Add 'R' 'm' flag 's' (squared random).
    
    Import last version of 'raseg.h' from
    2023-01 "mgensys", rename and tidy...
    
    Update and tidy in 'doc/README.SAU'.
    
    Add and update some scripts using 'R'.
     - Rename files in 'examples/tests/'.
     - Add 'examples/tests/tone_seq-v6_Rexp.sau'.
     - Update 'examples/tests/rand-pm-ratio.sau'.
     - Add 'examples/sounds/Rcos-rm_voicelike.sau'.
     - Add 'examples/sounds/cat-purr.sau'.
    
    Change log:
     * 2023-01-25: Integrate 'R' generator into saugns,
           based on code in 2023-01 "mgensys". Adjust
           design minimally, tidy up a little. Adjust
           and add some new scripts. Part 2 of 2 parts.
       Old branch:
        - 2023-01-05: Refactor the line (formerly ramp)
              module & raseg code. Make line map_* take
              buffers for range ends; and re-use a main
              buffer, also get value positions from it.
        - 2023-01-06: Optimize and tidy the line fill_*
              functions (with small changes, same use).
              Make 'R' 'm' mode 'f' take level.
        - 2023-01-07: Use old (faster) 'm' mode 'f' for
              level 9. Add WIP mode 'g' (Gaussian), and
              test 's' as a new, "squared random" mode.
              Replace the old ranoise32 with ranfast32.
        - 2023-01-08: Turn 'm' mode 's' into combinable
              extra flag for use with any of the modes.
              Working 'm' mode 'g' but too slow, quiet.
        - 2023-01-11: First version of better mode 'g'.
        - 2023-01-12: Improve the 'g' performance more.
        - 2023-01-14: Make mode 'g' sound more correct.
     * 2023-03-17: Fix signed leftshift UB build errors
           with clang on x86-64 (fixes mode 't').
     * 2024-02-29: Split 'R' importing into 2 parts, to
           keep its earlier design at hand as well.

--- sau/generator/rasg.h
+++ sau/generator/rasg.h
@@ -28,9 +28,7 @@ typedef struct sauCyclor {
 
 typedef struct sauRasG {
 	sauCyclor cyclor;
-	uint8_t line, mode, m_level;
-	uint8_t flags;
-//	float prev_x;
+	uint8_t line, func, level, flags;
 } sauRasG;
 
 /**
@@ -43,10 +41,11 @@ static inline void sau_init_RasG(sauRasG *restrict o, uint32_t srate) {
 			.coeff = sauCyclor_COEFF(srate),
 		},
 		.line = SAU_LINE_N_lin,
-		.mode = SAU_RAS_F_RAND,
-		.m_level = sau_ras_level(9 /* max one-digit number */),
-		.flags = 0,
-//		.prev_x = 0,
+		.func = SAU_RAS_F_RAND,
+		.level = sau_ras_level(9 /* max one-digit number */),
+		.flags = SAU_RAS_O_LINE_SET |
+			SAU_RAS_O_FUNC_SET |
+			SAU_RAS_O_LEVEL_SET,
 	};
 }
 
@@ -65,9 +64,9 @@ static void sauRasG_set_opt(sauRasG *restrict o,
 	if (opt->flags & SAU_RAS_O_LINE_SET)
 		o->line = opt->line;
 	if (opt->flags & SAU_RAS_O_FUNC_SET)
-		o->mode = opt->func;
+		o->func = opt->func;
 	if (opt->flags & SAU_RAS_O_LEVEL_SET)
-		o->m_level = opt->level;
+		o->level = opt->level;
 	o->flags = opt->flags |
 		SAU_RAS_O_LINE_SET |
 		SAU_RAS_O_FUNC_SET |
@@ -107,11 +106,6 @@ static inline int32_t sauRasG_cycle_offs(sauRasG *restrict o,
 	return (phs - sauWave_SLEN) / inc;
 }
 
-static void sauRasG_run(sauRasG *restrict o,
-		float *restrict buf, size_t buf_len,
-		const uint32_t *restrict cycle_buf,
-		const uint32_t *restrict phase_buf);
-
 #define P(inc, ofs) \
 	ofs + o->cycle_phase; (o->cycle_phase += inc)     /* post-increment */
 
@@ -123,18 +117,21 @@ static void sauRasG_run(sauRasG *restrict o,
  */
 static sauMaybeUnused void sauCyclor_fill(sauCyclor *restrict o,
 		uint32_t *restrict cycle_ui32,
-		uint32_t *restrict phase_ui32,
+		float *restrict phase_f,
 		size_t buf_len,
 		const float *restrict freq_f,
 		const float *restrict pm_f,
 		const float *restrict fpm_f) {
+	const float inv_int32_max = 1.f/(float)INT32_MAX;
 	const float fpm_scale = 1.f / SAU_HUMMID;
 	if (!pm_f && !fpm_f) {
 		for (size_t i = 0; i < buf_len; ++i) {
 			float s_f = freq_f[i];
 			uint64_t cycle_phase = P(llrintf(o->coeff * s_f), 0);
+			uint32_t phase;
 			cycle_ui32[i] = cycle_phase >> 31;
-			phase_ui32[i] = cycle_phase << 1;
+			phase = cycle_phase & ~(1U<<31);
+			phase_f[i] = ((int32_t) phase) * inv_int32_max;
 		}
 	} else if (!fpm_f) {
 		for (size_t i = 0; i < buf_len; ++i) {
@@ -142,8 +139,10 @@ static sauMaybeUnused void sauCyclor_fill(sauCyclor *restrict o,
 			float s_pofs = pm_f[i];
 			uint64_t cycle_phase = P(llrintf(o->coeff * s_f),
 					llrintf(s_pofs * (float)INT32_MAX));
+			uint32_t phase;
 			cycle_ui32[i] = cycle_phase >> 31;
-			phase_ui32[i] = cycle_phase << 1;
+			phase = cycle_phase & ~(1U<<31);
+			phase_f[i] = ((int32_t) phase) * inv_int32_max;
 		}
 	} else if (!pm_f) {
 		for (size_t i = 0; i < buf_len; ++i) {
@@ -151,8 +150,10 @@ static sauMaybeUnused void sauCyclor_fill(sauCyclor *restrict o,
 			float s_pofs = fpm_f[i] * fpm_scale * s_f;
 			uint64_t cycle_phase = P(llrintf(o->coeff * s_f),
 					llrintf(s_pofs * (float)INT32_MAX));
+			uint32_t phase;
 			cycle_ui32[i] = cycle_phase >> 31;
-			phase_ui32[i] = cycle_phase << 1;
+			phase = cycle_phase & ~(1U<<31);
+			phase_f[i] = ((int32_t) phase) * inv_int32_max;
 		}
 	} else {
 		for (size_t i = 0; i < buf_len; ++i) {
@@ -160,154 +161,224 @@ static sauMaybeUnused void sauCyclor_fill(sauCyclor *restrict o,
 			float s_pofs = pm_f[i] + (fpm_f[i] * fpm_scale * s_f);
 			uint64_t cycle_phase = P(llrintf(o->coeff * s_f),
 					llrintf(s_pofs * (float)INT32_MAX));
+			uint32_t phase;
 			cycle_ui32[i] = cycle_phase >> 31;
-			phase_ui32[i] = cycle_phase << 1;
+			phase = cycle_phase & ~(1U<<31);
+			phase_f[i] = ((int32_t) phase) * inv_int32_max;
 		}
 	}
 }
 
 #undef P /* done */
 
+typedef void (*sauRasG_map_f)(sauRasG *restrict o,
+		size_t buf_len,
+		float *restrict end_a_buf,
+		float *restrict end_b_buf,
+		const uint32_t *restrict cycle_buf);
+
 /**
  * Run for \p buf_len samples in 'uniform random' mode, generating output.
- *
- * Uses post-incremented phase each sample.
  */
-static sauMaybeUnused void sauRasG_run_rand(sauRasG *restrict o,
-		float *restrict buf, size_t buf_len,
-		const uint32_t *restrict cycle_buf,
-		const uint32_t *restrict phase_buf) {
-	sauLine_map_f map = sauLine_map_funcs[o->line];
+static sauMaybeUnused void sauRasG_map_rand(sauRasG *restrict o,
+		size_t buf_len,
+		float *restrict end_a_buf,
+		float *restrict end_b_buf,
+		const uint32_t *restrict cycle_buf) {
 	const float scale = 1.f/(float)INT32_MAX;
+	(void)o;
 	for (size_t i = 0; i < buf_len; ++i) {
 		uint32_t cycle = cycle_buf[i];
-		uint32_t phase = phase_buf[i];
-		float a = sau_ranoise32(cycle) * scale;
-		float b = sau_ranoise32(cycle + 1) * scale;
-		float p = ((int32_t) (phase >> 1)) * scale;
-		map(&buf[i], 1, a, b, &p);
+		end_a_buf[i] = ((int32_t)sau_ranfast32(cycle)) * scale;
+		end_b_buf[i] = ((int32_t)sau_ranfast32(cycle + 1)) * scale;
 	}
 }
 
-/**
- * Run for \p buf_len samples in 'smoothed random' mode, generating output.
+/*
+ * Approximation of a symmetric, highly soft-saturated modification
+ * of expression "sqrtf(-2.f * logf(x + 0.5f))"; more precisely, of
+ * "x > 0 ?
+ *  sqrtf(-2.f * logf(x + 0.5f)) :
+ *  (2.f*sqrtf(-2.f * logf(0.5f)) - sqrtf(-2.f * logf(0.5f - x)))".
  *
- * Uses post-incremented phase each sample.
+ * Unlike the original expression, the result is also downscaled by
+ * "2.f*sqrtf(-2.f * logf(0.5f))" (otherwise the maximum value), to
+ * the range of 0.0 to 1.0 inclusive.
  */
-static sauMaybeUnused void sauRasG_run_smooth(sauRasG *restrict o,
-		float *restrict buf, size_t buf_len,
-		const uint32_t *restrict cycle_buf,
-		const uint32_t *restrict phase_buf) {
-	sauLine_map_f map = sauLine_map_funcs[o->line];
-	const float scale = 1.f/(float)INT32_MAX;
-	int sar = o->m_level;
+static inline float soft_sqrtm2logp1_2_r01(float x) {
+	const float scale[] = {
+		-0.80270565422983103084,
+		+5.52274428214641442648,
+		-138.87126103150588693697,
+	};
+	float x2 = x*x;
+	float x4 = x2*x2;
+	return 0.5f + x*(scale[0] + x4*(scale[1] + x4*scale[2]));
+}
+
+/*
+ * Function used to distort initial soft-saurated curve
+ * so as to make it look and sound approximately right.
+ * (Graphed, looks like half a bell curve on its side.)
+ */
+static inline float ssgauss_dist4(float x) {
+	float x2 = x*x;
+	float gx = (x + x2)*0.5f;
+	return x*(1 - gx*(1 - x2));
+}
+
+/**
+ * Random access soft-saturated Gaussian noise, using approximation.
+ *
+ * Described at: https://joelkp.frama.io/blog/ran-softsat-gauss.html
+ *
+ * \return pseudo-random number between -1.0 and +1.0 for index \p n
+ */
+static inline float sau_franssgauss32(uint32_t n) {
+	int32_t s0 = sau_ranfast32(n);
+	int32_t s1 = sau_mcg32(s0);
+	float a = s0 * 1.f/(float)UINT32_MAX;
+	float b = s1 * 1.f/(float)UINT32_MAX;
+	float c = ssgauss_dist4(soft_sqrtm2logp1_2_r01(a));
+	b = c * sau_sinpi_d5f(b); // simplified for only sin(), no cos() output
+	return b;
+}
+
+/**
+ * Run for \p buf_len samples in 'Gaussian random' mode, generating output.
+ */
+static sauMaybeUnused void sauRasG_map_gauss(sauRasG *restrict o,
+		size_t buf_len,
+		float *restrict end_a_buf,
+		float *restrict end_b_buf,
+		const uint32_t *restrict cycle_buf) {
+	(void)o;
 	for (size_t i = 0; i < buf_len; ++i) {
 		uint32_t cycle = cycle_buf[i];
-		uint32_t phase = phase_buf[i];
-		int32_t offs = INT32_MAX + (cycle & 1) * 2;
-		float a = (sau_sar32(sau_ranoise32(cycle), sar)
-				+ offs) * scale;
-		float b = (sau_sar32(sau_ranoise32(cycle + 1), sar)
-				- offs) * scale;
-		float p = ((int32_t) (phase >> 1)) * scale;
-		map(&buf[i], 1, a, b, &p);
+		end_a_buf[i] = sau_franssgauss32(cycle);
+		end_b_buf[i] = sau_franssgauss32(cycle + 1);
 	}
 }
 
 /**
  * Run for \p buf_len samples in 'binary random' mode, generating output.
- * For increasing \a m_level > 0, each new level is half as squiggly, for
+ * For an increasing \a level > 0 each new level is half as squiggly, for
  * a near-binary mode when above 5 (with best quality seemingly from 27).
- *
- * Uses post-incremented phase each sample.
  */
-static sauMaybeUnused void sauRasG_run_bin(sauRasG *restrict o,
-		float *restrict buf, size_t buf_len,
-		const uint32_t *restrict cycle_buf,
-		const uint32_t *restrict phase_buf) {
-	sauLine_map_f map = sauLine_map_funcs[o->line];
+static sauMaybeUnused void sauRasG_map_bin(sauRasG *restrict o,
+		size_t buf_len,
+		float *restrict end_a_buf,
+		float *restrict end_b_buf,
+		const uint32_t *restrict cycle_buf) {
 	const float scale = 1.f/(float)INT32_MAX;
-	int sar = o->m_level;
+	int sar = o->level;
 	for (size_t i = 0; i < buf_len; ++i) {
 		uint32_t cycle = cycle_buf[i];
-		uint32_t phase = phase_buf[i];
 		int32_t offs = INT32_MAX + (cycle & 1) * 2;
-		float a = (sau_sar32(sau_ranoise32(cycle), sar)
+		end_a_buf[i] = (sau_sar32(sau_ranfast32(cycle), sar)
 				+ offs) * scale;
-		float b = (sau_sar32(sau_ranoise32(cycle + 1), sar)
+		end_b_buf[i] = (sau_sar32(sau_ranfast32(cycle + 1), sar)
 				- offs) * scale;
-		float p = ((int32_t) (phase >> 1)) * scale;
-		map(&buf[i], 1, a, b, &p);
 	}
 }
 
 /**
  * Run for \p buf_len samples in 'ternary random' mode, generating output.
- * For increasing \a m_level > 0, each new level is half as squiggly, with
+ * For an increasing \a level > 0 each new level is half as squiggly, with
  * a practically ternary mode when above 5, but 30 is technically perfect.
- *
- * Uses post-incremented phase each sample.
  */
-static sauMaybeUnused void sauRasG_run_tern(sauRasG *restrict o,
-		float *restrict buf, size_t buf_len,
-		const uint32_t *restrict cycle_buf,
-		const uint32_t *restrict phase_buf) {
-	sauLine_map_f map = sauLine_map_funcs[o->line];
+static sauMaybeUnused void sauRasG_map_tern(sauRasG *restrict o,
+		size_t buf_len,
+		float *restrict end_a_buf,
+		float *restrict end_b_buf,
+		const uint32_t *restrict cycle_buf) {
 	const float scale = 1.f/(float)INT32_MAX;
-	int sar = o->m_level;
+	int sar = o->level;
 	for (size_t i = 0; i < buf_len; ++i) {
 		uint32_t cycle = cycle_buf[i];
-		uint32_t phase = phase_buf[i];
 		int32_t sb = (cycle & 1) << 31;
-		float a = (sau_sar32(sau_ranoise32(cycle), sar)
-				+ (1<<31)-sb) * scale; // is first to cos-align
-		float b = (sau_sar32(sau_ranoise32(cycle + 1), sar)
+		int32_t sb_flip = (1U<<31) - sb;
+		end_a_buf[i] = (sau_sar32(sau_ranfast32(cycle), sar)
+				+ sb_flip) * scale; // is first to cos-align
+		end_b_buf[i] = (sau_sar32(sau_ranfast32(cycle + 1), sar)
 				+ sb) * scale;
-		float p = ((int32_t) (phase >> 1)) * scale;
-		map(&buf[i], 1, a, b, &p);
 	}
 }
 
 /**
- * Run for \p buf_len samples in 'fixed binary cycle' mode, generating output.
- *
- * Uses post-incremented phase each sample.
+ * Run for \p buf_len samples in 'fixed cycle' mode, generating output.
+ * Simple version, optimizing high level (pure base frequency) setting.
  */
-static sauMaybeUnused void sauRasG_run_fixed(sauRasG *restrict o,
-		float *restrict buf, size_t buf_len,
-		const uint32_t *restrict cycle_buf,
-		const uint32_t *restrict phase_buf) {
-	sauLine_map_f map = sauLine_map_funcs[o->line];
-	const float scale = 1.f/(float)INT32_MAX;
+static sauMaybeUnused void sauRasG_map_fixed_simple(sauRasG *restrict o,
+		size_t buf_len,
+		float *restrict end_a_buf,
+		float *restrict end_b_buf,
+		const uint32_t *restrict cycle_buf) {
+	(void)o;
 	for (size_t i = 0; i < buf_len; ++i) {
 		uint32_t cycle = cycle_buf[i];
-		uint32_t phase = phase_buf[i];
-		float a = sau_oddness_as_sign(cycle);
-		float b = -a;
-		float p = ((int32_t) (phase >> 1)) * scale;
-		map(&buf[i], 1, a, b, &p);
+		float a =
+		end_a_buf[i] = sau_oddness_as_sign(cycle);
+		end_b_buf[i] = -a;
+	}
+}
+
+/**
+ * Run for \p buf_len samples in 'fixed cycle' mode, generating output.
+ * For an increasing \a level > 0 each new level halves the randomness,
+ * the base frequency amplifying in its place (toward ultimate purity).
+ */
+static sauMaybeUnused void sauRasG_map_fixed(sauRasG *restrict o,
+		size_t buf_len,
+		float *restrict end_a_buf,
+		float *restrict end_b_buf,
+		const uint32_t *restrict cycle_buf) {
+	const float scale = 1.f/(float)INT32_MAX;
+	int slr = o->level;
+	for (size_t i = 0; i < buf_len; ++i) {
+		uint32_t cycle = cycle_buf[i];
+		int32_t sign = sau_oddness_as_sign(cycle);
+		end_a_buf[i] = (-sign * (int32_t)
+				(((uint32_t)sau_ranfast32(cycle) >> slr) -
+				 INT32_MAX)) * scale;
+		end_b_buf[i] = (sign * (int32_t)
+				(((uint32_t)sau_ranfast32(cycle + 1) >> slr) -
+				 INT32_MAX)) * scale;
 	}
 }
 
 /**
  * Run for \p buf_len samples, generating output.
+ * Expects phase values to be held inside \p main_buf;
+ * they will be replaced by the output.
  *
  * Uses post-incremented phase each sample.
  */
 static sauMaybeUnused void sauRasG_run(sauRasG *restrict o,
-		float *restrict buf, size_t buf_len,
-		const uint32_t *restrict cycle_buf,
-		const uint32_t *restrict phase_buf) {
-	switch (o->mode) {
-	case SAU_RAS_F_RAND:
-		sauRasG_run_rand(o, buf, buf_len, cycle_buf, phase_buf); break;
-	case SAU_RAS_F_BIN:
-		sauRasG_run_bin(o, buf, buf_len, cycle_buf, phase_buf); break;
-	case SAU_RAS_F_TERN:
-		sauRasG_run_tern(o, buf, buf_len, cycle_buf, phase_buf); break;
-	case SAU_RAS_F_SMOOTH:
-		sauRasG_run_smooth(o, buf, buf_len, cycle_buf,phase_buf);break;
-	case SAU_RAS_F_FIXED:
-		sauRasG_run_fixed(o, buf, buf_len, cycle_buf, phase_buf);break;
+		size_t buf_len,
+		float *restrict main_buf,
+		float *restrict end_a_buf,
+		float *restrict end_b_buf,
+		const uint32_t *restrict cycle_buf) {
+	sauRasG_map_f map;
+	switch (o->func) {
+	default:
+	case SAU_RAS_F_RAND: map = sauRasG_map_rand; break;
+	case SAU_RAS_F_GAUSS: map = sauRasG_map_gauss; break;
+	case SAU_RAS_F_BIN: map = sauRasG_map_bin; break;
+	case SAU_RAS_F_TERN: map = sauRasG_map_tern; break;
+	case SAU_RAS_F_FIXED: map = sauRasG_map_fixed;
+		if (o->level >= sau_ras_level(9))
+			map = sauRasG_map_fixed_simple;
+		break;
 	}
+	map(o, buf_len, end_a_buf, end_b_buf, cycle_buf);
+	if (o->flags & SAU_RAS_O_SQUARE) {
+		// square keeping sign; value uniformity to energy uniformity
+		for (size_t i = 0; i < buf_len; ++i) {
+			end_a_buf[i] *= fabsf(end_a_buf[i]);
+			end_b_buf[i] *= fabsf(end_b_buf[i]);
+		}
+	}
+	sauLine_map_funcs[o->line](main_buf, buf_len, end_a_buf, end_b_buf);
 }
