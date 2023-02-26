ae445faa5c163242fe2c7cc63be9ebbb81671256
Joel K. Pettersson <joelkp@tuta.io>
2023-02-26 01:45:53 +0100

    Rework and extend 'W' wave type collection.
    Language changes:
     * Wave types.
       - Add 'ean' (evenangle -- Tillman's idea/name),
         add 'cat' (catear -- my own idea/name),
         add 'eto' (eventooth -- Tillman's idea/name),
         add 'par' (parabola -- the x^2 variety),
         add 'mto' (mellowtooth, my own idea/name).
       - Rename 'ahs' to 'spa' (sine parabola),
         adjust phase offset to match other wave types.
       - Rename 'hrs' to 'hsi' (half-rectified sine),
         adjust phase offset to match other wave types.
       - Remove 'ssr', was of less general use.
    
    Organize wave types as 3 times 3 odd-even-all
    added harmonics types after 'sin'; each group
    has 3 types, in a mellow-medium-bright order.
    
    With 'tri' go 'ean' and 'par', as the mellow.
    With 'srs' go 'cat' and 'mto', as the medium.
    With 'sqr' go 'eto' and 'saw', as the bright.
    
    Additional types that don't fit in the scheme
    come after, in addition; more could be added.
    
    Change log:
     * 2023-02-28: Complete wave type changes and
           adjust scripts. To re-add 'ssr' later?
     * 2023-03-14: Make wave module license LGPL.

--- sau/wave.c
+++ sau/wave.c
@@ -2,17 +2,13 @@
  * Copyright (c) 2011-2012, 2017-2023 Joel K. Pettersson
  * <joelkp@tuta.io>.
  *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version, WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * View the files COPYING.LESSER and COPYING for details, or if missing, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 #include <sau/wave.h>
@@ -30,21 +26,21 @@ static float sqr_lut[sauWave_LEN];
 static float tri_lut[sauWave_LEN];
 static float pitri_lut[sauWave_LEN];
 
+static float eto_lut[sauWave_LEN];
+static float ean_lut[sauWave_LEN];
+static float piean_lut[sauWave_LEN];
+
 static float saw_lut[sauWave_LEN];
 static float par_lut[sauWave_LEN];
+static float pipar_lut[sauWave_LEN];
 
-static float ahs_lut[sauWave_LEN];
-static float piahs_lut[sauWave_LEN];
+static float srs_lut[sauWave_LEN], pisrs_lut[sauWave_LEN];
+static float cat_lut[sauWave_LEN], picat_lut[sauWave_LEN];
+static float mto_lut[sauWave_LEN], pimto_lut[sauWave_LEN];
+static float hsi_lut[sauWave_LEN], pihsi_lut[sauWave_LEN];
+static float spa_lut[sauWave_LEN], pispa_lut[sauWave_LEN];
 
-static float hrs_lut[sauWave_LEN];
-static float pihrs_lut[sauWave_LEN];
-
-static float srs_lut[sauWave_LEN];
-static float pisrs_lut[sauWave_LEN];
-static float ssr_lut[sauWave_LEN];
-static float pissr_lut[sauWave_LEN];
-
-#define SAU_WAVE__X_LUT_NAME(NAME) NAME##_lut,
+#define SAU_WAVE__X_LUT_NAME(NAME, COEFFS) NAME##_lut,
 
 float *const sauWave_luts[SAU_WAVE_NAMED] = {
 	SAU_WAVE__ITEMS(SAU_WAVE__X_LUT_NAME)
@@ -52,49 +48,21 @@ float *const sauWave_luts[SAU_WAVE_NAMED] = {
 
 float *const sauWave_piluts[SAU_WAVE_NAMED] = {
 	sin_lut,
-	tri_lut,
 	pitri_lut,
-	par_lut,
-	piahs_lut,
-	pihrs_lut,
 	pisrs_lut,
-	pissr_lut,
+	tri_lut,
+	piean_lut,
+	picat_lut,
+	ean_lut,
+	pipar_lut,
+	pimto_lut,
+	par_lut,
+	pihsi_lut,
+	pispa_lut,
 };
 
 const struct sauWaveCoeffs sauWave_picoeffs[SAU_WAVE_NAMED] = {
-	{
-		.amp_scale = 1.f / 0.78539693356f,
-		.amp_dc    = 0.f,
-		.phase_adj = INT32_MIN/2,
-	},{
-		.amp_scale = 1.f / 0.5f,
-		.amp_dc    = 0.f,
-		.phase_adj = INT32_MIN/2,
-	},{
-		.amp_scale = 1.f / 0.99902343750f,
-		.amp_dc    = 0.f,
-		.phase_adj = 0,
-	},{
-		.amp_scale = 1.f / 1.00048828125f,
-		.amp_dc    = 0.f,
-		.phase_adj = 0,
-	},{
-		.amp_scale = 1.f / 0.93224668503f,
-		.amp_dc    = 0.27323962859f - (1.00038196601f - 1.f),
-		.phase_adj = 0,
-	},{
-		.amp_scale = 1.f / 0.71259796619f,
-		.amp_dc    = -0.36338006155f - (-1.00002840285f + 1.f),
-		.phase_adj = 0,
-	},{
-		.amp_scale = 1.f / 0.65553373098f,
-		.amp_dc    = 0.f,
-		.phase_adj = 0,
-	},{
-		.amp_scale = 1.f / 0.79131034491f,
-		.amp_dc    = -0.13136863776f - (-1.00000757464 + 1.f),
-		.phase_adj = 0,
-	},
+	SAU_WAVE__ITEMS(SAU_WAVE__X_COEFFS)
 };
 
 const char *const sauWave_names[SAU_WAVE_NAMED + 1] = {
@@ -105,10 +73,14 @@ const char *const sauWave_names[SAU_WAVE_NAMED + 1] = {
 /*
  * Fill \p lut with integrated version of \p in_lut,
  * adjusted to have a peak amplitude of +/- \p scale.
- * The \in_dc DC offset for \p in_lut must be provided.
  */
 static void fill_It(float *restrict lut, size_t len, const float scale,
-		const float *restrict in_lut, double in_dc) {
+		const float *restrict in_lut) {
+	double in_dc = 0.f;
+	for (size_t i = 0; i < len; ++i) {
+		in_dc += in_lut[i];
+	}
+	in_dc /= len;
 	double in_sum = 0.f;
 	float lb = 0.f, ub = 0.f;
 	for (size_t i = 0; i < len; ++i) {
@@ -118,7 +90,7 @@ static void fill_It(float *restrict lut, size_t len, const float scale,
 		if (x > ub) ub = x;
 		lut[i] = x;
 	}
-	float out_scale = scale * 1.f/((ub - lb) * 0.5f);
+	float out_scale = scale / ((ub - lb) * 0.5f);
 	float out_dc = -(ub + lb) * 0.5f;
 	for (size_t i = 0; i < len; ++i) {
 		lut[i] = (lut[i] + out_dc) * out_scale;
@@ -141,15 +113,17 @@ void sau_global_init_Wave(void) {
 	/*
 	 * Fully fill:
 	 *  - sin, It -cosin
+	 *  - par, It pipar
+	 *  - spa, It pispa
 	 *
 	 * First half:
-	 *  - sqr, It -cotri
 	 *  - tri, It pitri
-	 *  - saw, It par
 	 *  - srs, It pisrs
-	 *  - ssr, It pissr
+	 *  - sqr, It -cotri
+	 *  - mto, It pimto
+	 *  - saw, -It copar
+	 *  - hsi, It pihsi
 	 */
-	double srs_half_dc = 0.f, ssr_half_dc = 0.f, ssr_dc;
 	for (i = 0; i < HALFLEN; ++i) {
 		const double x = i * (1.f/HALFLEN);
 		//const double x_rev = (HALFLEN-i) * (1.f/HALFLEN);
@@ -160,93 +134,108 @@ void sau_global_init_Wave(void) {
 
 		sqr_lut[i] = val_scale;
 
-		srs_half_dc += srs_lut[i] = val_scale * sqrtf(sin_x);
+		const float srs_x = sqrtf(sin_x);
+		srs_lut[i] = val_scale * srs_x;
+		hsi_lut[i] = val_scale * (sin_x*2 - 1.f);
+		mto_lut[i] = val_scale * (srs_x*2 - 1.f);
 
-		ssr_half_dc += ssr_lut[i] = val_scale * (sin_x * sin_x);
+		const float spa_x = sin(SAU_PI * 0.5f * (1 + x));
+		spa_lut[i + QUARTERLEN] = val_scale * (spa_x*2 - 1.f);
 	}
-	srs_half_dc *= (1.f/sauWave_LEN);
-	ssr_half_dc *= (1.f/sauWave_LEN);
-	ssr_dc = ssr_half_dc - srs_half_dc;
 	for (i = 0; i < HALFLEN; ++i) {
 		const double x = i * (1.f/(HALFLEN-1));
-		const double x_rev = ((HALFLEN-1)-i) * (1.f/(HALFLEN-1));
+		const double x_rev = (HALFLEN-i) * (1.f/HALFLEN);
 
-		saw_lut[i] = val_scale * (x - 1.f);
-		par_lut[i] = val_scale * ((x_rev * x_rev) * 2.f - 1.f);
+		par_lut[i + QUARTERLEN] =
+			val_scale * ((x_rev * x_rev) * 2.f - 1.f);
+		saw_lut[i] = val_scale * (1.f - x);
 	}
+	par_lut[HALFLEN+QUARTERLEN] = -val_scale;
+	spa_lut[HALFLEN+QUARTERLEN] = -val_scale;
 	for (i = 0; i < QUARTERLEN; ++i) {
 		const double x = i * (1.f/QUARTERLEN);
 		const double x_rev = (QUARTERLEN-i) * (1.f/QUARTERLEN);
 
+		pitri_lut[i] = val_scale * ((x * x) - 1.f);
+		pitri_lut[i + QUARTERLEN] = val_scale * (1.f - (x_rev * x_rev));
+
 		tri_lut[i] = val_scale * x;
 		tri_lut[i + QUARTERLEN] = val_scale * x_rev;
 
-		/* pre-integrated triangle shape */
-		pitri_lut[i] = val_scale * ((x * x) - 1.f);
-		pitri_lut[i + QUARTERLEN] = val_scale * (1.f - (x_rev * x_rev));
+		par_lut[i] = par_lut[HALFLEN - i];
+		par_lut[i + HALFLEN+QUARTERLEN] =
+			par_lut[HALFLEN+QUARTERLEN - i];
+		spa_lut[i] = spa_lut[HALFLEN - i];
+		spa_lut[i + HALFLEN+QUARTERLEN] =
+			spa_lut[HALFLEN+QUARTERLEN - i];
 	}
 	/* Second half:
-	 *  - sqr, It -cotri
 	 *  - tri, It pitri
-	 *  - saw, It par
 	 *  - srs, It pisrs
-	 *  - ssr, It pissr
+	 *  - sqr, It -cotri
+	 *  - mto, It pimto
+	 *  - saw, -It copar
+	 *  - hsi, It pihsi
 	 */
 	for (i = HALFLEN; i < sauWave_LEN; ++i) {
-		sqr_lut[i] = -sqr_lut[i - HALFLEN];
-		tri_lut[i] = -tri_lut[i - HALFLEN];
 		pitri_lut[i] = -pitri_lut[i - HALFLEN];
+		tri_lut[i] = -tri_lut[i - HALFLEN];
+		sqr_lut[i] = -val_scale;
 
 		saw_lut[i] = -saw_lut[(sauWave_LEN-1) - i];
-		par_lut[i] = par_lut[(sauWave_LEN-1) - i];
 
-		ssr_lut[i] = srs_lut[i] = -srs_lut[i - HALFLEN];
+		hsi_lut[i] = -val_scale;
+		mto_lut[i] = -val_scale;
+		srs_lut[i] = -srs_lut[i - HALFLEN];
 	}
 	/* Full cycle:
-	 *  - ahs, It piahs
-	 *  - hrs, It pihrs
+	 *  - ean, It piean
+	 *  - cat, It picat
+	 *  - eto, -It coean
 	 */
-	double ahs_dc = 0.f;
-	double hrs_dc = 0.f;
+	const float ean_dc_adj = (1.14603185654 - 1.f) / 2.f;
+	const float ean_scale_adj = val_scale / 1.07301592827;
+	const float eto_scale_adj = val_scale / 1.21094322205;
 	for (i = 0; i < sauWave_LEN; ++i) {
-		const double x = i * (1.f/HALFLEN);
-
-		float ahs_x = sin((SAU_PI * x) * 0.5f + SAU_ASIN_1_2);
-		ahs_x = fabs(ahs_x) - 0.5f;
-		ahs_x += ahs_x;
-		ahs_x *= val_scale;
-		ahs_lut[i] = ahs_x;
-		ahs_dc += ahs_x;
-
-		float hrs_x = sin((SAU_PI * x) + SAU_ASIN_1_2);
-		if (hrs_x > 0.f) {
-			hrs_x -= 0.5f;
-			hrs_x += hrs_x;
-			hrs_x *= val_scale;
-		} else {
-			hrs_x = -val_scale;
-		}
-		hrs_lut[i] = hrs_x;
-		hrs_dc += hrs_x;
+		int j = (i*2) < sauWave_LEN ? (i*2) : (i*2) - sauWave_LEN;
+		ean_lut[i] =
+			(sin_lut[i] + par_lut[i] - tri_lut[i] + ean_dc_adj) *
+			ean_scale_adj;
+		cat_lut[i] = sin_lut[i] + mto_lut[i] - srs_lut[i];
+		eto_lut[i] = (sin_lut[i] + saw_lut[j]) * eto_scale_adj;
 	}
-	ahs_dc *= (1.f/sauWave_LEN);
-	hrs_dc *= (1.f/sauWave_LEN);
-	fill_It(piahs_lut, sauWave_LEN, val_scale, ahs_lut, ahs_dc);
-	fill_It(pihrs_lut, sauWave_LEN, val_scale, hrs_lut, hrs_dc);
-	fill_It(pisrs_lut, sauWave_LEN, val_scale, srs_lut, 0.f);
-	fill_It(pissr_lut, sauWave_LEN, val_scale, ssr_lut, ssr_dc);
+	/*fill_It(ean_lut, sauWave_LEN, -val_scale, eto_lut);*/
+	fill_It(piean_lut, sauWave_LEN, val_scale, ean_lut);
+	fill_It(picat_lut, sauWave_LEN, val_scale, cat_lut);
+	fill_It(pipar_lut, sauWave_LEN, val_scale, par_lut);
+	fill_It(pisrs_lut, sauWave_LEN, val_scale, srs_lut);
+	fill_It(pimto_lut, sauWave_LEN, val_scale, mto_lut);
+	fill_It(pihsi_lut, sauWave_LEN, val_scale, hsi_lut);
+	fill_It(pispa_lut, sauWave_LEN, val_scale, spa_lut);
+
+#if 0
+//	for (int i = 0; i < SAU_WAVE_NAMED; ++i)
+//		sauWave_print(i, false);
+	sauWave_print(SAU_WAVE_N_spa, true);
+#endif
 }
+
+/* Write data meant for conversion to image? */
+#define PLOT_DATA 0
+#define PLOT_TWICE 1
 
 /**
  * Print an index-value table for a LUT.
  */
-void sauWave_print(uint8_t id) {
+void sauWave_print(uint8_t id, bool verbose) {
 	if (id >= SAU_WAVE_NAMED)
 		return;
 	const float *lut = sauWave_luts[id];
 	const float *pilut = sauWave_piluts[id];
+#if !PLOT_DATA
 	const char *lut_name = sauWave_names[id];
 	sau_printf("LUT: %s\n", lut_name);
+#endif
 	double sum = 0.f, sum2 = 0.f, mag_sum = 0.f, mag_sum2 = 0.f;
 	float prev_s = lut[sauWave_LEN - 1], prev_s2 = pilut[sauWave_LEN - 1];
 	float peak_max = 0.f, peak_max2 = 0.f;
@@ -255,7 +244,7 @@ void sauWave_print(uint8_t id) {
 	for (int i = 0; i < sauWave_LEN; ++i) {
 		float s = lut[i], s2 = pilut[i];
 		float abs_s = fabsf(s), abs_s2 = fabsf(s2);
-		float slope_s = (s - prev_s), slope_s2 = (s2 - prev_s2);
+		double slope_s = (s - prev_s), slope_s2 = (s2 - prev_s2);
 		sum += s; sum2 += s2;
 		mag_sum += abs_s; mag_sum2 += abs_s2;
 		if (peak_max < abs_s) peak_max = abs_s;
@@ -265,30 +254,48 @@ void sauWave_print(uint8_t id) {
 		if (slope_min > slope_s) slope_min = slope_s;
 		if (slope_min2 > slope_s2) slope_min2 = slope_s2;
 		prev_s = s; prev_s2 = s2;
-		sau_printf("[\t%d]: \t%.11f\tIv %.11f\n", i, s, s2);
+#if PLOT_DATA
+		sau_printf("%.11f\t%.11f\n", i/(float)sauWave_LEN, s);
+#else
+		if (verbose)
+			sau_printf("[\t%d]: \t%.11f\tIv %.11f\n", i, s, s2);
+#endif
 	}
-	float len_scale = (float) sauWave_LEN;
-	float diff_min = slope_min2 * DVSCALE;
-	float diff_max = slope_max2 * DVSCALE;
-	float diff_scale = sauWave_picoeffs[id].amp_scale;
-	float diff_offset = sauWave_picoeffs[id].amp_dc;
+#if PLOT_DATA
+# if PLOT_TWICE
+	for (int i = 0; i < sauWave_LEN; ++i) {
+		float s = lut[i];
+		sau_printf("%.11f\t%.11f\n",
+				(i+sauWave_LEN)/(float)sauWave_LEN, s);
+	}
+# endif
+#else
+	double len_scale = sauWave_LEN;
+	double diff_scale = sauWave_picoeffs[id].amp_scale;
+	double diff_offset = sauWave_picoeffs[id].amp_dc;
+	double diff_min = slope_min2 * DVSCALE;
+	double diff_min_adj = diff_min * diff_scale + diff_offset;
+	double diff_max = slope_max2 * DVSCALE;
+	double diff_max_adj = diff_max * diff_scale + diff_offset;
+	double tweak_dc = -(diff_min + diff_max) / 2.f;
+	double tweak_scale = 2.f / (diff_max - diff_min);
 	sau_printf("\tp.m.avg %.11f\tIt %.11f\n"
 			"\tp.m.max %.11f\tIt %.11f\n"
 			"\tdc.offs %.11f\tIt %.11f\n"
 			"\t+slope  %.11f\tIt %.11f\n"
 			"\t-slope  %.11f\tIt %.11f\n"
 			"It\tdiff.min %.11f\t(adj. to %.11f)\n"
-			"It\tdiff.max %.11f\t(adj. to %.11f)\n",
-			mag_sum / len_scale,
-			mag_sum2 / len_scale,
-			peak_max,
-			peak_max2,
-			sum / len_scale,
-			sum2 / len_scale,
-			slope_max,
-			slope_max2,
-			slope_min,
-			slope_min2,
-			diff_min, diff_min * diff_scale + diff_offset,
-			diff_max, diff_max * diff_scale + diff_offset);
+			"It\tdiff.max %.11f\t(adj. to %.11f)\n"
+			"tweak\tdc.offs %.11f\n"
+			"tweak\tscale %.11f\n",
+			mag_sum / len_scale, mag_sum2 / len_scale,
+			peak_max, peak_max2,
+			sum / len_scale, sum2 / len_scale,
+			slope_max, slope_max2,
+			slope_min, slope_min2,
+			diff_min, (float) diff_min_adj,
+			diff_max, (float) diff_max_adj,
+			tweak_dc * tweak_scale,
+			tweak_scale);
+#endif
 }
