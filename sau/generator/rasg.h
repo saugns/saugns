3e351c390f90aa3b557a03c284a335b77237b515
Joel K. Pettersson <joelkp@tuta.io>
2023-05-01 19:40:16 +0200

    remove 'mB', 'mC'

--- sau/generator/rasg.h
+++ sau/generator/rasg.h
@@ -469,23 +469,6 @@ static sauMaybeUnused void sauRasG_run(sauRasG *restrict o,
 		break;
 	}
 	map(o, buf_len, end_a_buf, end_b_buf, cycle_buf);
-	if (o->flags & SAU_RAS_O_B) {
-		for (size_t i = 0; i < buf_len; ++i) {
-			if (end_a_buf[i] > end_b_buf[i]) {
-				float tmp = end_a_buf[i];
-				end_a_buf[i] = end_b_buf[i];
-				end_b_buf[i] = tmp;
-				main_buf[i] = 1.f - main_buf[i];
-			}
-		}
-	}
-	if (o->flags & SAU_RAS_O_C) {
-		for (size_t i = 0; i < buf_len; ++i) {
-			main_buf[i] = 1.f - main_buf[i];
-		}
-		float *tmp_buf = end_a_buf;
-		end_a_buf = end_b_buf; end_b_buf = tmp_buf;
-	}
 	if (o->flags & SAU_RAS_O_SQUARE) {
 		// square keeping sign; value uniformity to energy uniformity
 		for (size_t i = 0; i < buf_len; ++i) {
