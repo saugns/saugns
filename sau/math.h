9966930cf8e02896c760352db87b03168c695bc8
Joel K. Pettersson <joelkp@tuta.io>
2023-05-01 20:10:28 +0200

    Add 'R' 'm' flag 'h' for half-shape waveforms.
    Filling in an 'R' waveforms gap, sawtooth-like
    waveforms are now possible. A new flag 'h' for
    changing the waveform drawn using a line type,
    with a 'lin' line gives a decreasing sawtooth.
    
    Implemented by sorting the two values in every
    pair of half-cycle ends so that each line goes
    in the same direction; I picked maximum-first.
    
    Doing this doubles the base frequency, as each
    cycle then uses one line, rather than two. The
    fix is to just generate values at halved rate.
    
    Has imperfections; a sawtooth-ish waveform has
    harmonics louder than the surrounding noise in
    the spectrum. Also, violet noise becomes a bit
    impure and plays an octave low (6 dB bassier).
    
    Change log:
     * 2023-03-15: Make math module LGPL, and also
           all files using it, remove in unneeded.
     * 2023-05-19: Add 'R mh' option, half-shapes.

--- sau/math.h
+++ sau/math.h
@@ -2,17 +2,13 @@
  * Copyright (c) 2011-2012, 2017-2023 Joel K. Pettersson
  * <joelkp@tuta.io>.
  *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version, WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * View the files COPYING.LESSER and COPYING for details, or if missing, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 #pragma once
@@ -96,6 +92,25 @@ static inline int32_t sau_sar32(int32_t x, int s) {
 /** 32-bit right rotation. */
 static inline uint32_t sau_ror32(uint32_t x, int r) {
 	return x >> r | x << (32 - r);
+}
+
+/** Pick smallest of two float values. */
+static inline float sau_minf(float x, float y) {
+	x = x > y ? y : x;
+	return x;
+}
+
+/** Pick largest of two float values. */
+static inline float sau_maxf(float x, float y) {
+	x = x < y ? y : x;
+	return x;
+}
+
+/** Clamp value of \p x to a float range. */
+static inline float sau_fclampf(float x, float min, float max) {
+	x = x < min ? min : x;
+	x = x > max ? max : x;
+	return x;
 }
 
 /*
