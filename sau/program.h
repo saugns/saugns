f5e3800cfa59df22b24b72c98029a76fd367426b
Joel K. Pettersson <joelkp@tuta.io>
2023-05-01 03:52:19 +0200

    Add 'R' 'm' flag 'z' for zig-zag flip distortion.
    A simple distortion effect, which can give varied
    results depending on how it's combined with other
    mode flags and line type. Sounds thin and bright.
    
    Adding 'z' to 'R m' flips all half-cycle boundary
    amplitude pairs. To the extent that amplitudes do
    not simply follow a fixed oscillation, a somewhat
    messy inharmonic jagged waveform edge is brought.
    
    Implemented by swapping two pointers if flag set.

--- sau/program.h
+++ sau/program.h
@@ -100,12 +100,13 @@ static inline unsigned int sau_ras_level(unsigned int digit) {
 
 /** Random segments option flags. */
 enum {
-	SAU_RAS_O_VIOLET = 1<<0,
-	SAU_RAS_O_SQUARE = 1<<1,
-	SAU_RAS_O_FUNC_FLAGS = (1<<2)-1,
-	SAU_RAS_O_LINE_SET = 1<<2,
-	SAU_RAS_O_FUNC_SET = 1<<3,
-	SAU_RAS_O_LEVEL_SET = 1<<4,
+	SAU_RAS_O_VIOLET = 1U<<0,
+	SAU_RAS_O_SQUARE = 1U<<1,
+	SAU_RAS_O_ZIGZAG = 1U<<2,
+	SAU_RAS_O_FUNC_FLAGS = (1U<<5)-1,
+	SAU_RAS_O_LINE_SET = 1U<<5,
+	SAU_RAS_O_FUNC_SET = 1U<<6,
+	SAU_RAS_O_LEVEL_SET = 1U<<7,
 };
 
 /*
