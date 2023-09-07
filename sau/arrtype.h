c9c21990e2f2d82c012f6b950ad694af1089ed1f
Joel K. Pettersson <joelkp@tuta.io>
2023-09-07 10:09:22 +0200

    More flexible arrtype allocation size, cloning.
    Changes meant to allow using arrtype in various
    additional ways. Allow using a borrowed initial
    allocation (like the deleted ptrarr module did)
    and -- a mutually exclusive option -- to freely
    set the initial allocation size in the struct.
    
    Change log:
     * 2023-09-07: Document arrtype features which use direct
           setting of the field members. LGPL arrtype module.
           Change arrtype to allow use of a borrowed, initial
           allocation, cloned upon need; like ptrarr except a
           method is not provided and no old_count in struct.
           Add math.h functions for rounding power of 2 size.
     * 2023-09-21: Readd arrtype upsize method to public API.
     * 2025-09-15: Rebase to merge with 2024 arrtype changes.

--- sau/arrtype.h
+++ sau/arrtype.h
@@ -1,18 +1,14 @@
 /* SAU library: Generic array module.
- * Copyright (c) 2018-2024 Joel K. Pettersson
+ * Copyright (c) 2018-2025 Joel K. Pettersson
  * <joelkp@tuta.io>.
  *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * This file and the software of which it is part is distributed under the
+ * terms of the GNU Lesser General Public License, either version 3 or (at
+ * your option) any later version, WITHOUT ANY WARRANTY, not even of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * View the files COPYING.LESSER and COPYING for details, or if missing, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 #pragma once
@@ -23,6 +19,17 @@
  * macros being used to declare and define the concrete type.
  *
  * Each concrete type uses inline wrappers around generic methods.
+ *
+ * Some optional functionality relies on directly setting
+ * fields of an instance (of any arrtype type declared):
+ *  - Set \a count to zero for a soft clear, to start over
+ *    with adding items while keeping the old allocation.
+ *  - Set \a asize to a non-zero size in bytes, prior to the
+ *    very first allocation, to make that allocation larger.
+ *  - Copy \a a from elsewhere and ensure \a asize is zero
+ *    and \a count set to the number of items to reuse, to
+ *    clone those \a a items upon the first resizing call.
+ *    (To reuse a prior allocation until a new is needed.)
  */
 
 struct sauMempool;
@@ -118,5 +125,7 @@ bool sauArrType_mpmemdup(void *restrict o,
 		void **restrict dst, size_t item_size,
 		struct sauMempool *restrict mempool);
 
-/** Byte (uint8_t) array type. */
+/*
+ * Arrays of primitive types.
+ */
 sauArrType(sauByteArr, uint8_t, )
