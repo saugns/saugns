a96243efdfa0356b3255e49b3a600094476b3e44
Joel K. Pettersson <joelkp@tuta.io>
2024-03-09 20:33:56 +0100

    Reduce default strength to 'a.l0.01' (match YM2612).
    Sounds similar to Aly James's old comparison between
    his emulation and Sega MD model I. This remains less
    exact, but 0.01 gives the same strength as his 0.02.

--- sau/parser.c
+++ sau/parser.c
@@ -1064,7 +1064,7 @@ static void begin_operator(sauParser *restrict o,
 			o->obj_arr.a[pl->nest.list->ref.obj_id].parent_op_obj :
 			op->ref.obj_id;
 		op->amp = create_line(o, false, SAU_PSWEEP_AMP);
-		if (!is_nested) op->amp_lec = 0.025f;
+		if (!is_nested) op->amp_lec = 0.01f; // 0.01 as Aly James 0.02
 	}
 	op->event = e;
 	link_ev_obj(o, &op->ref, &pop->ref);
