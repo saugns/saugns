d2d8cc78b711a77e9404313ceee9e5329433a3af
Joel K. Pettersson <joelkp@tuta.io>
2024-03-10 02:49:41 +0100

    Fix for negative strength in wave-env/mod-val-range.

--- sau/generator.c
+++ sau/generator.c
@@ -479,7 +479,7 @@ static void block_mix_mul_waveenv(GenNode *restrict gen,
 		sau_nzerof(buf, buf_len);
 		return;
 	}
-	float lec = - gen->amp_lec;
+	float lec = - gen->amp_lec * 0.5f;
 	float le_th = - sqrtf(fabsf(gen->amp_lec)) * (0.5f/128);
 	float le_gr = 1.f - fabsf(gen->amp_lec);
 	float le_prev = gen->amp_le_prev;
@@ -493,7 +493,7 @@ static void block_mix_mul_waveenv(GenNode *restrict gen,
 			le_prev = le_in;
 			le_avg = (le_avg + le_s) * 0.5f;
 			le_s = le_avg;
-			s = (s + fabsf(s_amp)) * le_gr + (le_s * 0.5f - lec);
+			s = s * le_gr + (le_s - lec) + fabsf(s_amp);
 			buf[i] *= s;
 		}
 	} else {
@@ -505,7 +505,7 @@ static void block_mix_mul_waveenv(GenNode *restrict gen,
 			le_prev = le_in;
 			le_avg = (le_avg + le_s) * 0.5f;
 			le_s = le_avg;
-			s = (s + fabsf(s_amp)) * le_gr + (le_s * 0.5f - lec);
+			s = s * le_gr + (le_s - lec) + fabsf(s_amp);
 			buf[i] = s;
 		}
 	}
