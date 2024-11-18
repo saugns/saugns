f6586a58a787e44d72aa2cb52ca7d39eceedebae
Joel K. Pettersson <joelkp@tuta.io>
2024-11-18 20:55:26 +0100

    Implement Casio-style half-cycles PD variations.
    Implement both a proportional 'width' distortion
    (using PD for PWM), and a 'height' version which
    doesn't change the size of the halves but scales
    the phase change happening in each half instead.
    
    Both are interesting to modulate. Matches Csound
    pdhalf (width) and pdhalfy (height) except range
    isn't limited and audio-rate modulation is used.
    They still lack offset controls to rotate phase.

--- sau/parser.c
+++ sau/parser.c
@@ -1598,6 +1598,12 @@ static bool parse_gen_main(sauParser *restrict o, uint8_t gen_type,
 		case 'c':
 			return parse_par_modranges(o, NULL, &gen->pd_c, false,
 					SAU_PSWEEP_PDC, SAU_MOD_N_wc_pd);
+		case 'p':
+			return parse_par_modranges(o, NULL, &gen->pd_p, false,
+					SAU_PSWEEP_PDP, SAU_MOD_N_wp_pd);
+		case 'y':
+			return parse_par_modranges(o, NULL, &gen->pd_y, false,
+					SAU_PSWEEP_PDY, SAU_MOD_N_wy_pd);
 		default:
 			return c != 0;
 		}
@@ -2267,6 +2273,8 @@ static void time_gen_lines(sauScriptGenData *restrict gen) {
 	time_range(gen->freq, dur_ms);
 	time_range(gen->pm_a, dur_ms);
 	time_range(gen->pd_c, dur_ms);
+	time_range(gen->pd_p, dur_ms);
+	time_range(gen->pd_y, dur_ms);
 }
 
 static uint32_t time_gen(sauScriptGenData *restrict gen) {
