c3cfa49dd20590f94ed901d72ca3a3927f871fd6
Joel K. Pettersson <joelkp@tuta.io>
2024-11-18 20:55:26 +0100

    Implement Casio-style half-cycles PD variations.
    Implement both a proportional 'width' distortion
    (using PD for PWM), and a 'height' version which
    doesn't change the size of the halves but scales
    the phase change happening in each half instead.
    
    Both are interesting to modulate. Matches Csound
    pdhalf (width) and pdhalfy (height) except range
    isn't limited and audio-rate modulation is used.
    They still lack offset controls to rotate phase.

--- sau/parser/parseconv.h
+++ sau/parser/parseconv.h
@@ -301,6 +301,8 @@ ParseConv_convert_gendata(ParseConv *restrict o,
 	ogd->freq = gen->freq;
 	ogd->pm_a = gen->pm_a;
 	ogd->pd_c = gen->pd_c;
+	ogd->pd_p = gen->pd_p;
+	ogd->pd_y = gen->pd_y;
 	ogd->phase = gen->phase;
 	ogd->use_type = use_type;
 	/* TODO: separation of types */
