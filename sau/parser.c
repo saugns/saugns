67d8f5b6f2491d3f7cbbf309ce6f2a4f3679cb52
Joel K. Pettersson <joelkp@tuta.io>
2025-01-02 12:48:44 +0100

    Implement PD phase offset '.p' subparameter.
    Allow shifting or rotating how to apply each
    phase distortion effect using '.p'. Using it
    allows e.g. using the '.x' PD for a sawtooth
    shape by treating a sine like a cosine.
    
    These give very similar results:
     - "W.x0.99.p1/4 p1/4"
     - "R.x0.99.p0/4 mf"
    
    This subparameter supports sweep, modulation
    unlike the main phase parameter 'p'. Its use
    is well-behaved no matter how it's done. Now
    "wiggly" PD is easily done with a modulator.
    
    2025-01-02: Implement PD `.p` for `W` & `R`.
    2025-01-27: Correct PD synth `.p` direction, was flipped;
        concerns `R` only; and, fix mis-scaled `c.p` & `d.p`.

--- sau/parser.c
+++ sau/parser.c
@@ -1579,15 +1579,20 @@ static uint8_t parse_par_pdset(sauParser *restrict o,
 		for (uint32_t i = 0; i < SAU_PPD_TYPES; ++i) {
 			init_range(o, &(*pdset)[i].v);
 			init_range(o, &(*pdset)[i].f);
+			init_range(o, &(*pdset)[i].p);
 		}
 	}
 	sauPDSet *p = &(*pdset)[pdset_id];
-	sauRange *range_v = &p->v, *range_f = &p->f;
+	sauRange *range_v = &p->v, *range_f = &p->f, *range_p = &p->p;
 	uint8_t c;
 	switch ((c = parse_par_modranges(o, NULL, &range_v, false, 0, mod))) {
 	case 'f':
 		parse_par_modranges(o, scan_note_const, &range_f,
 				false, 0, mod+4);
+		break;
+	case 'p':
+		parse_par_modranges(o, scan_cyclepos_const, &range_p,
+				false, 0, mod+8);
 		break;
 	default:
 		return c;
@@ -2247,6 +2252,7 @@ static inline void time_pdset(sauPDSet *restrict p,
 	for (uint32_t i = 0; i < SAU_PPD_TYPES; ++i) {
 		time_range(&p[i].v, default_time_ms);
 		time_range(&p[i].f, default_time_ms);
+		time_range(&p[i].p, default_time_ms);
 	}
 }
 
